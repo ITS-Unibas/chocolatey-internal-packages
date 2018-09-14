9b9ee1a33b12d4586672e2e070cc9267273f1952
max.burgert@unibas.ch <max.burgert@unibas.ch>
2018-09-14 11:32:04 +0200

    Added thunderbird. This package does not require any helper scripts.

---
+++ automatic/thunderbird/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿
+$ErrorActionPreference = 'Stop';
+ 
+
+$packageName= 'thunderbird'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = ''
+  
+  silentArgs   = '/S'
+  validExitCodes= @(0) 
+
+  softwareName  = 'Mozilla Thunderbird*'
+  checksum      = ''
+  checksumType  = 'md5'
+}
+
+&(Join-path $PSScriptRoot InitialScript.ps1)
+Install-ChocolateyPackage @packageArgs
+&(Join-path $PSScriptRoot FinalScript.ps1)
