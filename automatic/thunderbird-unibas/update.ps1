abf880a2a2d4b3e36d4991842d8030abce71cd16
MaxBurgert <max.burgert@unibas.ch>
2019-01-17 13:23:31 +0100

    Update update.ps1

--- automatic/thunderbird-unibas/update.ps1
+++ automatic/thunderbird-unibas/update.ps1
@@ -1,5 +1,4 @@
 ﻿import-module au
-. $PSScriptRoot\..\_scripts\all.ps1
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $releases = 'https://www.thunderbird.net/en-US/thunderbird/all/'
