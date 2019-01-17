b2b70237076872cc0f8a97a58ba96d8541b5373d
Chocolatey <chocolatey@realdimensions.net>
2019-01-17 13:36:05 +0000

    AU: 2 updated - rstudio.unibas thunderbird.unibas [skip ci] https://gist.github.com/dddd183fae34b7f07585422765dee87b/a5b8adc7d64a13f84a9dcdfda356d0198d1a5fbe

--- automatic/rstudio.unibas/tools/chocolateyinstall.ps1
+++ automatic/rstudio.unibas/tools/chocolateyinstall.ps1
@@ -7,12 +7,12 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://download1.rstudio.org/RStudio-1.1.456.exe'
+  url           = 'https://download1.rstudio.org/RStudio-1.1.463.exe'
   silentArgs   = '/S'
   validExitCodes= @(0)
  
   softwareName  = 'rstudio*'
-  checksum      = '24ca3fe0dad8187aabd4bfbb9dc2b5ad'
+  checksum      = '58b3d796d8cf96fb8580c62f46ab64d4'
   checksumType  = 'md5'
 }
 
