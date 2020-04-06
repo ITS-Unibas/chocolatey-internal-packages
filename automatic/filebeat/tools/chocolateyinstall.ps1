3c11d1ca808e9215f0dcf8dbde84a72981a1e0ee
Maximilian Burgert <max.burgert@unibas.ch>
2020-04-06 16:15:43 +0200

    Adding filebeat

---
+++ automatic/filebeat/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'filebeat'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64 = ''
+
+$folder = if (Get-ProcessorBits 64) { [io.path]::GetFileNameWithoutExtension($url64) } else { [io.path]::GetFileNameWithoutExtension($url) }
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url            = $url64
+  url64bit       = $url64
+  checksum64     = ''
+  checksumType64 = 'sha512'
+  checksum       = $checksum
+  checksumType   = $checksumType64
+  specificFolder = $folder
+}
+
+Install-ChocolateyPackage @packageArgs
