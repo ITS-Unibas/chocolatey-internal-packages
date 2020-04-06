1c49eefd6afc58b27463e9c263071ff4089e9427
Maximilian Burgert <max.burgert@unibas.ch>
2020-04-06 12:33:37 +0200

    Resetting repo to fully work

---
+++ automatic/rstudio-automatic/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+ 
+$packageName= 'rstudio'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = 'https://download1.rstudio.org/desktop/windows/RStudio-1.2.5033.exe'
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+ 
+  softwareName  = 'rstudio*'
+  checksum      = 'ccc7cd9069a7dd688c40865f9ec6467a'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
