39463511052dd118b408f41f362a79027e986279
Maximilian Burgert <max.burgert@unibas.ch>
2020-04-06 15:11:32 +0200

    Adding zoom

---
+++ automatic/zoom/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'zoom'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'msi'
+  url            = 'url_here'
+  silentArgs     = '/quiet /norestart'
+  validExitCodes = @(0)
+  softwareName   = 'zoom*'
+  checksum       = 'checksum_here'
+  checksumType   = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/zoom/update.ps1
@@ -0,0 +1,34 @@
+import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$download_page_url = 'https://zoom.us/download#client_4meeting'
+$url_part1 = 'https://zoom.us/client/'
+$url_part2 = '/ZoomInstallerFull.msi'
+
+function global:au_BeforeUpdate() {
+  Get-RemoteFiles -Purge -FileNameBase 'ZoomInstallerFull'
+  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'md5'
+}
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+function global:au_GetLatest {
+  $homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $download_page_url
+
+  # Get Version
+  $homepage_content -match '(Version \d+.\d+.\d (\(.\d+.\d+\)))' | Out-Null
+  $recodeversion = $matches[1] -replace "Version ", ""
+  $version = $recodeversion.Substring(0, 4) + $recodeversion.Substring(7, 10)
+  Write-Host $version
+  $url = $url_part1 + $version + $url_part2
+
+  $Latest = @{ URL = $url; Version = $version }
+  return $Latest
+}
+
+update -ChecksumFor none
