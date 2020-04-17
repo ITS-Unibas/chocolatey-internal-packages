f4e476b07e8cacd958b8e3c1a998342c761fb17a
Maximilian Burgert <max.burgert@unibas.ch>
2020-04-17 11:00:30 +0200

    Adding firefoxesr

---
+++ automatic/firefoxesr/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName   = 'firefoxesr'
+  softwareName  = 'Mozilla Firefox*'
+  fileType      = 'EXE'
+  unzipLocation = $toolsDir
+  url           = ''
+  checksum      = ''
+  checksumType  = 'sha512'
+  silentArgs    = "/S"
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/firefoxesr/update.ps1
@@ -0,0 +1,33 @@
+import-module AU
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$releasesESR = 'https://www.mozilla.org/en-US/firefox/organizations/notes/'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL64)`'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releasesESR
+  $download_page.Content -match 'Version \d+.\d+.\d+'
+  $version = $matches[0] -replace "Version ", ""
+
+  $url = "https://ftp.mozilla.org/pub/firefox/releases/$($version)esr/win64/en-US/Firefox%20Setup%20$($version)esr.exe"
+  Write-Host $url
+
+  $ExecutableName = "firefox-${Version}esr"
+  $allChecksums = Invoke-WebRequest -UseBasicParsing -Uri "https://releases.mozilla.org/pub/firefox/releases/${Version}esr/SHA512SUMS"
+  $allChecksums -match "(\w*)  update\/win64\/en-US\/$ExecutableName"
+  $checksum = $matches[1]
+
+  $Latest = @{ URL64 = $url; Version = $version; Checksum = $checksum }
+  return $Latest
+}
+
+update -ChecksumFor none -NoCheckChocoVersion -Verbose
