254adaa0ec5ccaea4b1496110b7cc791a2e2d4c4
Maximilian Burgert <max.burgert@unibas.ch>
2020-04-22 14:54:12 +0200

    Adding wireshark

---
+++ automatic/wireshark/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  file           = "$toolsDir\Wireshark-win32-3.2.3.exe"
+  file64         = "$toolsDir\Wireshark-win64-3.2.3.exe"
+  softwareName   = 'Wireshark*'
+  silentArgs     = '/S /quicklaunchicon=no'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
---
+++ automatic/wireshark/update.ps1
@@ -0,0 +1,58 @@
+Import-Module au
+
+$version_page = 'https://www.wireshark.org/#download'
+$releases64 = 'https://www.wireshark.org/download/win64/all-versions/'
+$releases32 = 'https://www.wireshark.org/download/win32/all-versions/'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+  $version = GetVersion
+
+  $r64 = '[Ww]ire[Ss]hark-win64-[\d\.]+\.exe$'
+  $url64 = GetDownloadLink -downloadlinks $releases64 -pattern $r64 -version $version
+
+  $r32 = '[Ww]ire[Ss]hark-win32-[\d\.]+\.exe$'
+  $url32 = GetDownloadLink -downloadlinks $releases32 -pattern $r32 -version $version
+
+  return @{
+    URL32    = $url32
+    URL64    = $url64
+    Version  = $version
+    FileType = 'exe'
+  }
+}
+
+function global:au_SearchReplace {
+  return @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$version_page>"
+      "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
+      "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL64)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+      "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
+    }
+  }
+}
+
+function GetVersion {
+  $version_page = Invoke-WebRequest -Uri $version_page
+  $re64 = '(http[s]?|[s]?)(:\/\/)([^\s,]+)\/win64\/[Ww]ire[Ss]hark-win64-[\d\.]+\.exe$'
+  $v = $version_page.links | Where-Object href -match $re64 | Select-Object -First 1 -expand href
+  $v -split '-|.exe' | Select-Object -Last 1 -Skip 1
+}
+
+function GetDownloadLink([string] $downloadlinks, [string] $pattern, [string] $version) {
+  $page = Invoke-WebRequest -Uri $downloadlinks
+  $link = $page.links | Where-Object { ($_.innerHTML -like '*' + $version + '*') -And ($_.innerHTML -match $pattern) } | Select-Object -first 1 -expand href
+  $page.BaseResponse.ResponseUri.ToString() + $link
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+  update -ChecksumFor None -NoCheckChocoVersion
+}
