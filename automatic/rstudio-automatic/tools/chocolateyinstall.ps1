f021e6f8010739f98f4ad3977ed32a46f67d16be
Chocolatey <chocolatey@realdimensions.net>
2020-05-23 00:01:43 +0000

    AU: 1 updated - rstudio-automatic [skip ci] https://gist.github.com/9679c1d6d10b8dd4989856f51ec43a5f/49549db62833d6ec0389e84362cae873f0cab6a0

--- automatic/rstudio-automatic/tools/chocolateyinstall.ps1
+++ automatic/rstudio-automatic/tools/chocolateyinstall.ps1
@@ -7,12 +7,12 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://download1.rstudio.org/desktop/windows/RStudio-1.2.5042.exe'
+  url           = 'https://download1.rstudio.org/desktop/windows/RStudio-1.3.959.exe'
   silentArgs   = '/S'
   validExitCodes= @(0)
  
   softwareName  = 'rstudio*'
-  checksum      = '29acbe410ea81c4223ce5485f8986ec6'
+  checksum      = '9d4f67ad2c409df475134d92bc504b59'
   checksumType  = 'md5'
 }
 
