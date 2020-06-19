04879d9c4d5f5fc4a8bf806c05f5e07e3d2e6959
Chocolatey <chocolatey@realdimensions.net>
2020-06-19 00:01:25 +0000

    AU: 1 updated - filebeat [skip ci] https://gist.github.com/524e9928dce8e3f8760a5bbe985eac05/c4d284b38b2f5626f14edc4d608dc412e973c14a

--- automatic/filebeat/tools/chocolateyinstall.ps1
+++ automatic/filebeat/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'filebeat'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.7.1-windows-x86_64.msi'
+$url64 = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.8.0-windows-x86_64.msi'
 
 $folder = if (Get-ProcessorBits 64) { [io.path]::GetFileNameWithoutExtension($url64) } else { [io.path]::GetFileNameWithoutExtension($url) }
 
@@ -12,7 +12,7 @@ $packageArgs = @{
   unzipLocation  = $toolsDir
   url            = $url64
   url64bit       = $url64
-  checksum64     = '002a8bb0b862be5505fa1085ade09360cee4a7fcaec074569196179aa7a48eb80c507a9d6d8b449ba1ea72797f8714225a05b597c55198eb9b1c5f32532e8e0c'
+  checksum64     = 'c00680d145e39ff0b4e12a0540776ed8c78115440792a4736036e77af7c8e90bdd9efc410b6fae7581242010c90797f91abb3754e8eb6aca305c2f542d66f528'
   checksumType64 = 'sha512'
   checksum       = $checksum
   checksumType   = $checksumType64
