663f61de95e35bb8e6b38896e02da922b3b44bbc
Chocolatey <chocolatey@realdimensions.net>
2020-06-30 00:05:03 +0000

    AU: 1 updated - zoom [skip ci] https://gist.github.com/27046b0b4432e4acb9080136c722a14d/74cde36b0f1a76ca874b4a9191ea509a8d208c4f

--- automatic/zoom/tools/chocolateyinstall.ps1
+++ automatic/zoom/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'zoom'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://zoom.us/client/5.1.27830.0612/ZoomInstallerFull.msi'
+  url            = 'https://zoom.us/client/5.1.28546.0628/ZoomInstallerFull.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'zoom*'
-  checksum       = '2d070b46cbf01bfb217b2157ee97cbea'
+  checksum       = 'ba7872c004f0e5c3cde2b97e9936b9c2'
   checksumType   = 'md5'
 }
 
