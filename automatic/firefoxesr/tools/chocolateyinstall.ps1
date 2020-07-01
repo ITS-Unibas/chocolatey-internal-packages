43634d683511f3ed9b4cc728f3fbbe4e85cd3096
Chocolatey <chocolatey@realdimensions.net>
2020-07-01 00:01:34 +0000

    AU: 1 updated - firefoxesr [skip ci] https://gist.github.com/50ac4800e2ce63d4ac39d19c793e422f/af057d9db320e59513fff0de7ada9f5a5f5bf2ed

--- automatic/firefoxesr/tools/chocolateyinstall.ps1
+++ automatic/firefoxesr/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   softwareName  = 'Mozilla Firefox*'
   fileType      = 'EXE'
   unzipLocation = $toolsDir
-  url           = 'https://ftp.mozilla.org/pub/firefox/releases/68.9.0esr/win64/en-US/Firefox%20Setup%2068.9.0esr.msi'
-  checksum      = 'b1046e2949861784901329d3b2b9a93f5f6286fffaef182d352c2a69eb59301a4562a3915ce81a78c79f04bdd6b09842c6c495cdc9049b24ab9fe6f75887e8a1'
+  url           = 'https://ftp.mozilla.org/pub/firefox/releases/68.10.0esr/win64/en-US/Firefox%20Setup%2068.10.0esr.msi'
+  checksum      = 'c4d5564807e493cc7c6861d6675e885e225d3c194f4fbbbed08d7545d91afbc5b44376694614cf466bcd88d3b746f9c503e4b5a2e018c4b8a613dc38200c9167'
   checksumType  = 'sha512'
   silentArgs    = "/S"
 }
