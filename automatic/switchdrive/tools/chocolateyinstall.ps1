f96a53d5c891804958adaf27c8ca6b11080d6f23
Maximilian Burgert <max.burgert@unibas.ch>
2020-07-06 16:13:09 +0200

    Adding switchdrive

---
+++ automatic/switchdrive/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'switchdrive'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'msi'
+  url            = 'https://download.to.switch.ch/59.exe'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+
+  softwareName   = 'SWITCHdrive*'
+  checksum       = 'dd4f6f4ddcf09494b5134d90bc40bb59'
+  checksumType   = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
