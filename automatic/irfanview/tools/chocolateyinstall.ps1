5688e45e2e56d418e1048422e3cadbf1cb62b0c3
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-08-04 08:39:36 +0200

    removed checksum

--- automatic/irfanview/tools/chocolateyinstall.ps1
+++ automatic/irfanview/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $packageArgs = @{
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
   url            = 'https://zoom.us/client/5.1.28656.0709/ZoomInstallerFull.msi'
-  silentArgs     = '/silent /group=1 /alluusers=1'
+  silentArgs     = '/silent /group=1 /allusers=1'
   validExitCodes = @(0)
   softwareName   = 'irfanview*'
-  checksum       = 'f2209071959286d268a8171510d900d3'
+  checksum       = ''
   checksumType   = 'md5'
 }
 
