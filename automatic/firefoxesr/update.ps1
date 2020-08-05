4c2aa1f49e947617707ec972dada19a2aad94132
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-08-05 16:29:18 +0200

    firefox changed it's release site, so only the last stable version found. this should fix, but should be double checked if 78.2.0 was released

--- automatic/firefoxesr/update.ps1
+++ automatic/firefoxesr/update.ps1
@@ -2,7 +2,7 @@ import-module AU
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$releasesESR = 'https://www.mozilla.org/en-US/firefox/organizations/notes/'
+$releasesESR = 'https://www.mozilla.org/en-US/firefox/all/#product-desktop-esr'
 
 function global:au_SearchReplace {
   @{
@@ -15,14 +15,17 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releasesESR
-  $download_page.Content -match 'Version \d+.\d+.\d+'
-  $version = $matches[0] -replace "Version ", ""
+
+
+  $version = Select-String "(\d+)\.(\d+)\.(\d+)esr" -InputObject $download_page -AllMatches | ForEach-Object {$_.matches}`
+                      | Select-Object -ExpandProperty Value | Sort-Object | Get-Unique | ForEach-Object {[version]($_.Replace('esr', ''))} `
+                      | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum
 
   $url = "https://ftp.mozilla.org/pub/firefox/releases/$($version)esr/win64/en-US/Firefox%20Setup%20$($version)esr.msi"
 
   $ExecutableName = "Firefox Setup ${Version}esr.msi"
   $allChecksums = Invoke-WebRequest -UseBasicParsing -Uri "https://releases.mozilla.org/pub/firefox/releases/${Version}esr/SHA512SUMS"
-  $allChecksums -match "(\w*)  win64\/en-US\/$ExecutableName"
+  $allChecksums -match "(\w*)  win64\/en-US\/$ExecutableName" | Out-Null
   $checksum = $matches[1]
 
   $Latest = @{ URL64 = $url; Version = $version; Checksum = $checksum }
