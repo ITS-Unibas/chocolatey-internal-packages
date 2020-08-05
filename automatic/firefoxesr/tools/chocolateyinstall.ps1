5f591d20ca8be210b6408ee0cb958bfede317ea4
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-08-05 16:35:43 +0200

    filetype was EXE changed it to MSI

--- automatic/firefoxesr/tools/chocolateyinstall.ps1
+++ automatic/firefoxesr/tools/chocolateyinstall.ps1
@@ -6,12 +6,13 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName   = 'firefoxesr'
   softwareName  = 'Mozilla Firefox*'
-  fileType      = 'EXE'
+  fileType      = 'MSI'
   unzipLocation = $toolsDir
   url           = 'https://ftp.mozilla.org/pub/firefox/releases/78.1.0esr/win64/en-US/Firefox%20Setup%2078.1.0esr.msi'
   checksum      = 'a68012a0adbdd917512f2f7cee60880a7325a6fc566033509b862828200ed08c9a63c34edfb4eb4feba535c122862a803f14458dd18a2dcf0bd5f78498f14eba'
   checksumType  = 'sha512'
-  silentArgs    = "/S"
+  silentArgs    = '/quiet /norestart'
+  validExitCodes = @(0, 3010)
 }
 
 Install-ChocolateyPackage @packageArgs
