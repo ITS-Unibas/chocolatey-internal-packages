9372ebb0b7f772418b5449e698c86b30662cf88c
Chocolatey <chocolatey@realdimensions.net>
2020-08-10 15:53:32 +0000

    AU: 1 updated - vcredist140-x86 [skip ci] https://gist.github.com/d5d2a2b9185838c9c925939ba060e9b8/9c23a07fceff8dd1fbdd5b68a885f274a2d14580

--- automatic/vcredist140-x86/tools/chocolateyinstall.ps1
+++ automatic/vcredist140-x86/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'vcredist140-x86'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
-  url            = ''
+  url            = 'https://download.visualstudio.microsoft.com/download/pr/9fe82b83-f3a1-43f5-8f25-ebe24529854c/B4D433E2F66B30B478C0D080CCD5217CA2A963C16E90CAF10B1E0592B7D8D519/VC_redist.x86.exe'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0, 1638, 3010)
   softwareName   = 'Microsoft Visual C++ 2015-2019 Redistributable (x86)*'
-  checksum       = ''
+  checksum       = 'b4d433e2f66b30b478c0d080ccd5217ca2a963c16e90caf10b1e0592b7d8d519'
   checksumType   = 'sha256'
 }
 
