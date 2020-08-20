a24bbe1c09e56b2d7743fd9d8d53cfc91bd2e63c
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-08-20 12:29:25 +0200

    vcredist 2010 x86

---
+++ automatic/vcredist10-x86/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'vcredist10-x86'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = 'https://download.microsoft.com/download/C/6/D/C6D0FD4E-9E53-4897-9B91-836EBA2AACD3/vcredist_x86.exe'
+  silentArgs     = '/quiet /norestart'
+  validExitCodes = @(0, 1638, 3010)
+  softwareName   = 'Microsoft Visual C++ 2010 Redistributable (x86)*'
+  checksum       = '66b797b3b4f99488f53c2b676610dfe9868984c779536891a8d8f73ee214bc4b'
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
