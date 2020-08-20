16c595d39b437e1c3ff215ec9d55320cfe5ab96d
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-08-20 14:11:17 +0200

    vcredist 2012 x86

---
+++ automatic/vcredist12-x86/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'vcredist12-x86'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = 'https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe'
+  silentArgs     = '/quiet /norestart'
+  validExitCodes = @(0, 1638, 3010)
+  softwareName   = 'Microsoft Visual C++ 2012 Redistributable (x64)*'
+  checksum       = ''
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/vcredist12-x86/update.ps1
@@ -0,0 +1,43 @@
+import-module au
+Import-Module Wormies-AU-Helpers
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
+Import-Module "$PSScriptRoot\..\..\scripts/au_extensions.psm1"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$Release = 'https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+
+function GetResultInformation([string]$url) {
+  $url = Get-RedirectedUrl $url
+  $dest = "$env:TEMP\vcredist_x64.exe"
+
+  Get-WebFile $url $dest | Out-Null
+  $checksumType = 'sha256'
+  $version = Get-Version (Get-Item $dest | % { $_.VersionInfo.ProductVersion })
+  $checksum = Get-FileHash $dest -Algorithm $checksumType | % { $_.Hash.ToLowerInvariant() }
+
+  return @{
+    URL            = $url
+    Version          = if ($version.Version.Revision -eq '0') { $version.ToString(3) } else { $version.ToString() }
+    VersionThreePart = $version.ToString(3)
+    Checksum       = $checksum
+    ChecksumType  = $checksumType
+  }
+}
+
+function global:au_GetLatest {
+  Update-OnETagChanged -execUrl $Release `
+    -OnEtagChanged {
+    GetResultInformation $Release
+  } -OnUpdated { @{ URL = $Release}}
+}
+
+update -ChecksumFor none
