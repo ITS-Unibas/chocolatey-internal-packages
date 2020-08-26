7fbdadf8bb3c9b0fb4025adaecc8cd422cb84923
Chocolatey <chocolatey@realdimensions.net>
2020-08-26 10:56:18 +0000

    AU: 1 updated - powershell [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/dcbe58713c1be3c1fe60dc037d21d631f92868ac

--- automatic/powershell/tools/chocolateyinstall.ps1
+++ automatic/powershell/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'powershell'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = ''
+  url            = 'https://github.com/PowerShell/PowerShell/releases/download/v7.0.3/PowerShell-7.0.3-win-x64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'PowerShell*'
-  checksum       = ''
+  checksum       = 'AD3B4A868D1B7E47A1048E1EB20F7F782D9B95D5066D79A25D02CCC4DD14E79F'
   checksumType   = 'sha256'
 }
 
