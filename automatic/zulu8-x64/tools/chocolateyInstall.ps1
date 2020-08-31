e18640b2e3349d7a5da91b70c032680b949120e8
Maximilian Burgert <max.burgert@unibas.ch>
2020-08-31 14:14:33 +0200

    Triggering update

--- automatic/zulu8-x64/tools/chocolateyInstall.ps1
+++ automatic/zulu8-x64/tools/chocolateyInstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-$urljre = 'https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=8&bundle_type=jre&features=&ext=zip&os=windows&arch=x86&hw_bitness=64'
-$urljdkfx = 'https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=8&bundle_type=jdk&features=fx&ext=zip&os=windows&arch=x86&hw_bitness=64'
-$urljrefx = 'https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=8&bundle_type=jre&features=fx&ext=zip&os=windows&arch=x86&hw_bitness=64'
+$urljre = 't1'
+$urljdkfx = 't2'
+$urljrefx = 't3'
 
 
 Get-ChocolateyWebFile -PackageName 'zulu8-x64' -FileFullPath "$toolsDir\JRE\zulu8-x64-jre.zip" -Url $urljre
@@ -14,8 +14,8 @@ Get-ChocolateyWebFile -PackageName 'zulu8-x64' -FileFullPath "$toolsDir\FX\JRE\z
 $packageArgs = @{
     packageName    = 'zulu8-x64'
     fileType       = 'msi'
-    url64bit       = 'https://cdn.azul.com/zulu/bin/zulu8.48.0.53-ca-jdk8.0.265-win_x64.msi'
-    checksum64     = 'b291a35afc72bfc942d4ca6dd639a1500f4fd1fec5bc38db986d6f7535b1a419'
+    url64bit       = 't4'
+    checksum64     = 'check'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
     validExitCodes = @(0)
