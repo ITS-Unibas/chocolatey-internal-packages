ab3e214af97ed1bd3615e729b68c64de1c1a54a5
Chocolatey <chocolatey@realdimensions.net>
2020-09-02 00:02:23 +0000

    AU: 2 updated - zoom zulu8-x86 [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/322282e7ca5c7839cfffb66b141c818692b57533

--- automatic/zoom/tools/chocolateyinstall.ps1
+++ automatic/zoom/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'zoom'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://zoom.us/client/5.2.44052.0816/ZoomInstallerFull.msi'
+  url            = 'https://zoom.us/client/5.2.45108.0831/ZoomInstallerFull.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'zoom*'
-  checksum       = '632451af241901d6ceb80757012e3706'
+  checksum       = '7b9db6d34955ca8f6027b531443a3204'
   checksumType   = 'md5'
 }
 
