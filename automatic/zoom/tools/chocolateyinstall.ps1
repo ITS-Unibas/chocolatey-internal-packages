dfd312b8c1a8f6ef4763a6a3af1c74805676b259
Chocolatey <chocolatey@realdimensions.net>
2020-09-08 10:06:59 +0000

    AU: 2 updated - atlassian-companion zoom [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/33243c838b953579d6c6e43fda53e3de20891c12

--- automatic/zoom/tools/chocolateyinstall.ps1
+++ automatic/zoom/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'zoom'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://zoom.us/client/5.2.45108.0831/ZoomInstallerFull.msi'
+  url            = 'https://zoom.us/client/5.2.45120.0906/ZoomInstallerFull.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'zoom*'
-  checksum       = '7b9db6d34955ca8f6027b531443a3204'
+  checksum       = '0c6bfa393751f2a24209d8bbd89535b3'
   checksumType   = 'md5'
 }
 
