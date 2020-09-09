d9853e0e3e8de630e3be6264313cbb95fc02d82f
Maximilian Burgert <max.burgert@unibas.ch>
2020-09-09 08:59:55 +0200

    Adding unibas-audacity

---
+++ automatic/unibas-audacity/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName = 'unibas-audacity'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = ''
+  #OTHERS
+  # Uncomment matching EXE type (sorted by most to least common)
+  #silentArgs   = '/S'           # NSIS
+  silentArgs     = '/VERYSILENT' # Inno Setup
+  #silentArgs   = '/s'           # InstallShield
+  #silentArgs   = '/s /v"/qn"' # InstallShield with MSI
+  #silentArgs   = '/s'           # Wise InstallMaster
+  #silentArgs   = '-s'           # Squirrel
+  #silentArgs   = '-q'           # Install4j
+  #silentArgs   = '-s -u'        # Ghost
+  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
+  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
+  #       https://chocolatey.org/packages/autohotkey.portable
+  validExitCodes = @(0) #please insert other valid exit codes here
+
+  # optional, highly recommended
+  softwareName   = 'Audacity' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum       = ''
+  checksumType   = 'sha256' #default is md5, can also be sha1
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/unibas-audacity/update.ps1
@@ -0,0 +1,36 @@
+﻿Import-Module AU
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$releases = 'https://www.fosshub.com/Audacity.html'
+
+function global:au_BeforeUpdate() {
+  Get-RemoteFiles -Purge -FileNameBase $Latest.PackageName
+  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'sha256'
+}
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $installer_exe = $download_page.Links | Where-Object href -match 'audacity-win-.*.exe$' | Select-Object -First 1 -expand href
+  if ($installer_exe) {
+    $version = $installer_exe -split '-|.exe' | Select-Object -Skip 2 -First 1
+  }
+
+  if ($version) {
+    $url = $releases + "?dwl=audacity-win-" + $version + ".exe"
+  }
+
+  @{
+    URL    = $url
+    Version  = $version
+    FileType = 'exe'
+  }
+}
+
+update -ChecksumFor none -NoCheckChocoVersion -Force
