605e0b9de0ea023a72cff13cafea8dadaf402a68
Maximilian Burgert <max.burgert@unibas.ch>
2020-09-15 13:02:08 +0200

    Adding autohotkey

---
+++ automatic/autohotkey/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿
+$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'autohotkey'
+  fileType       = 'exe'
+  url            = ''
+  silentArgs     = "/S /D=C:\Program Files\AutoHotkey"
+  softwareName   = 'AutoHotkey*'
+  checksum       = ''
+  checksumType   = 'sha256' #default is md5, can also be sha1
+  validExitCodes = @(0, 1223)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+$installLocation = Get-AppInstallLocation $packageArgs.softwareName
+$packageName = $packageArgs.softwareName
+if ($installLocation) {
+  $installName = 'AutoHotkey'
+  Write-Host "$packageName installed to '$installLocation'"
+  Register-Application "$installLocation\$installName.exe"
+  Write-Host "$packageName registered as $installName"
+}
+else { Write-Warning "Can't find $packageName install location" }
---
+++ automatic/autohotkey/update.ps1
@@ -0,0 +1,28 @@
+﻿Import-Module AU
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$releases = 'https://autohotkey.com/download/1.1'
+
+function global:au_BeforeUpdate() {
+  Get-RemoteFiles -Purge -FileNameBase 'autohotkey'
+  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'sha256'
+}
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+function global:au_GetLatest {
+  $version = Invoke-WebRequest -Uri "$releases\version.txt" -UseBasicParsing | ForEach-Object Content
+  $url = "https://github.com/Lexikos/AutoHotkey_L/releases/download/v${version}/AutoHotkey_${version}_setup.exe"
+  @{
+    Version  = $version
+    URL      = $url
+    FileName = $url -split '/' | Select-Object -Last 1
+  }
+}
+
+update -ChecksumFor none -NoCheckChocoVersion
