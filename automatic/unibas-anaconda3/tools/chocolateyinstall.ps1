0817449febc5b27cb58d74b1e628d0897535d148
Maximilian Burgert <max.burgert@unibas.ch>
2020-09-17 09:35:10 +0200

    Adding unibas-anaconda3

---
+++ automatic/unibas-anaconda3/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'Anaconda3'
+  fileType       = 'EXE'
+  url            = 'url'
+  checksum       = 'checksum'
+  checksumType   = 'sha256'
+  silentArgs     = "/S /InstallationType=AllUsers /RegisterPython=1 /AddToPath=1 /D=C:\Program Files\Anaconda3"
+  validExitCodes = @(0)
+}
+
+Write-Warning 'The Anaconda3 installation can take a long time (up to 30 minutes).'
+Write-Warning 'Please be patient and let it finish.'
+Write-Warning 'If you want to verify the install is running, you can watch the installer process in Task Manager'
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/unibas-anaconda3/update.ps1
@@ -0,0 +1,40 @@
+﻿Import-Module AU
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate() {
+  Get-RemoteFiles -Purge -FileNameBase 'unibas-anaconda3'
+  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'sha256'
+}
+function global:au_GetLatest {
+  $ArchiveURL = 'https://repo.continuum.io/archive/'
+  $List = Invoke-WebRequest -Uri $ArchiveURL
+
+  $version = "1.0"
+
+  $List.links | Where-Object { $_.href -match "Anaconda3.*Windows" } |
+  ForEach-Object {
+    $v = ($_.href.split('-')[1])
+    if ([version]$v -gt [version]$version) {
+      $version = $v
+    }
+  }
+
+  $URL64 = "https://repo.continuum.io/archive/Anaconda3-$version-Windows-x86_64.exe"
+
+  return @{
+    Version = $Version
+    URL     = $URL64
+  }
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+
+Update -ChecksumFor none -NoCheckChocoVersion
