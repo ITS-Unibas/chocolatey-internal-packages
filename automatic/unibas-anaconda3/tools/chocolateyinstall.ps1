8a30a50834f70c6131901c66e6993589c735b115
Chocolatey <chocolatey@realdimensions.net>
2020-09-17 07:37:04 +0000

    AU: 1 updated - unibas-anaconda3 [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/01ab8b0e2c82d32be6d2edd4d3845bc11dd12f9d

--- automatic/unibas-anaconda3/tools/chocolateyinstall.ps1
+++ automatic/unibas-anaconda3/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Anaconda3'
   fileType       = 'EXE'
-  url            = 'url'
-  checksum       = 'checksum'
+  url            = 'https://repo.continuum.io/archive/Anaconda3-2020.07-Windows-x86_64.exe'
+  checksum       = '66acb9bdf7d2d5925df8762311a85ad72f57dfd340447bf00636d35a28454244'
   checksumType   = 'sha256'
   silentArgs     = "/S /InstallationType=AllUsers /RegisterPython=1 /AddToPath=1 /D=C:\Program Files\Anaconda3"
   validExitCodes = @(0)
