fc4d4bcac2998980418a1841927c3fdce1bca5e2
Chocolatey <chocolatey@realdimensions.net>
2020-09-22 08:39:44 +0000

    AU: 1 updated - unibas-teamviewer [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/ed94bc476d211ab4a4207031b4d6be048d202bc3

--- automatic/unibas-teamviewer/tools/chocolateyinstall.ps1
+++ automatic/unibas-teamviewer/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'unibas-teamviewer'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
-  url            = ''
+  url            = 'https://dl.teamviewer.com/download/version_15x/Jmp-91/TeamViewer_Setup.exe'
   silentArgs     = '/S /norestart'
   validExitCodes = @(0, 1638, 3010)
   softwareName   = 'TeamViewer*'
-  checksum       = ''
+  checksum       = '441c21970a08ae66b331719c295a5f397fd486859fef20382251da57fafe9541'
   checksumType   = 'sha256'
 }
 
