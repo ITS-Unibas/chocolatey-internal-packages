bce13f41b46993ba25664f4c3d6a4afa68689cab
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-09-23 17:23:20 +0200

    xming-fonts

---
+++ automatic/xming-fonts/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName = 'xming-fonts'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = ''
+  silentArgs     = "/VERYSILENT /ALLUSERS /NOCANCEL /NORESTART /LOG=`"$($env:SWP)\Install_$($packageName).$($env:chocolateyPackageVersion).log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes = @(0, 3010, 1641)
+
+  softwareName   = 'Xming-fonts*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum       = ''
+  checksumType   = 'sha256' #default is md5, can also be sha1
+}
+
+Install-ChocolateyPackage @packageArgs
