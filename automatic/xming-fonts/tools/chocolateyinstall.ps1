1737fdb5a81aaed5b35fe356a54f74c4c4a6aca3
Chocolatey <chocolatey@realdimensions.net>
2020-09-23 15:26:01 +0000

    AU: 1 updated - xming-fonts [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/b320537c7483aed91b167dd0e4a7784908ab23cd

--- automatic/xming-fonts/tools/chocolateyinstall.ps1
+++ automatic/xming-fonts/tools/chocolateyinstall.ps1
@@ -9,12 +9,12 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = ''
+  url            = 'https://sourceforge.net/projects/xming/files/Xming-fonts/7.7.0.10/Xming-fonts-7-7-0-10-setup.exe/download'
   silentArgs     = "/VERYSILENT /ALLUSERS /NOCANCEL /NORESTART /LOG=`"$($env:SWP)\Install_$($packageName).$($env:chocolateyPackageVersion).log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0, 3010, 1641)
 
   softwareName   = 'Xming-fonts*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = ''
+  checksum       = 'ACF5507D51C680A5BAED212334412CA97BBFA67D7E161B6347FC9194EC22D9B1'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
