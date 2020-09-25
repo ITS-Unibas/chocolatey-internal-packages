b622ff5073fd787f6a4b56ce3a2817c060cfd6ea
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-09-25 12:19:36 +0200

    added vmware tools which automatically updates other than the github one

---
+++ automatic/unibas-vmware-tools/update.ps1
@@ -0,0 +1,35 @@
+﻿Import-Module AU
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$releaseurl = "https://packages.vmware.com/tools/releases/latest/windows/x64/"
+
+function global:au_GetLatest {
+  $Version = "0.0"
+  $content = Get-Content "$PSScriptRoot\tools\chocolateyInstall.ps1"
+  $content -match "(\s*checksum\s*=\s*)('.*')"
+  $OldChecksum = $Matches[2].Replace("'", "")
+  $html_content = Invoke-WebRequest -Uri $releaseurl
+  $file = $html_content.Links | Where-Object href -like "*.exe" | Select-Object -ExpandProperty href
+  $url = $releaseurl + $file
+  $checksum = Get-RemoteChecksum -Url $url -Algorithm 'sha256'
+  if($checksum -ne $OldChecksum) {
+    Invoke-WebRequest $url -OutFile 'vmware-tools.exe'
+    $Version = Get-Item 'vmware-tools.exe' | Select-Object -ExpandProperty VersionInfo | Select-Object -ExpandProperty ProductVersion
+  }
+  return @{
+    Version = $Version
+    URL     = $url
+    Checksum = $checksum
+  }
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+
+Update -ChecksumFor none -NoCheckChocoVersion
