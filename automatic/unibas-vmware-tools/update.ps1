f9115ab8c47c1aa1e1ca57c262f940d027291a58
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-09-25 15:31:28 +0200

    wrong char was shipped inside the script

--- automatic/unibas-vmware-tools/update.ps1
+++ automatic/unibas-vmware-tools/update.ps1
@@ -7,7 +7,7 @@ function global:au_GetLatest {
   $Version = "0.0"
   $content = Get-Content ".\tools\chocolateyInstall.ps1"
   $OldChecksum = ''
-  if($content -match "(?i)(^\s*checksum\s*=\s*)('.*')") {¨
+  if($content -match "(?i)(^\s*checksum\s*=\s*)('.*')") {
     Write-Verbose $Matches
     if($Matches -and $Matches[2]) {
       $OldChecksum = $Matches[2].Replace("'", "")
