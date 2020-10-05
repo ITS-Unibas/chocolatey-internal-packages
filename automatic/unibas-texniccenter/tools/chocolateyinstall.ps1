53635b3a1058ceea8f4c0ca1a15e72182942ee64
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-10-05 17:06:08 +0200

    texniccenter

---
+++ automatic/unibas-texniccenter/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'TexnicCenter*'
+  fileType       = 'EXE'
+  url            = ''
+  checksum       = ''
+  checksumType   = 'sha256'
+  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  validExitCodes = @(0)
+}
+
+
+Install-ChocolateyPackage @packageArgs
