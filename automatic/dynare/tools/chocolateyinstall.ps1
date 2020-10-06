5bc2e220c902a8217c9738b25a0950592834c1a1
Chocolatey <chocolatey@realdimensions.net>
2020-10-06 12:14:28 +0000

    AU: 1 updated - dynare [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/68f81ba4f630fa35f5477ceff4fbe930832da718

--- automatic/dynare/tools/chocolateyinstall.ps1
+++ automatic/dynare/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = ''
+  url           = 'https://www.dynare.org/release/windows/dynare-4.6.2-win.exe'
   #file         = $fileLocation
   #OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
@@ -27,7 +27,7 @@ $packageArgs = @{
 
   # optional, highly recommended
   softwareName  = 'Dynare*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = ''
+  checksum      = 'b8c042c30f429b8e3f554053779fc9448dc53d0788877731e3831e76265163a9'
   checksumType  = 'sha256' #default is md5, can also be sha1
 }
 
