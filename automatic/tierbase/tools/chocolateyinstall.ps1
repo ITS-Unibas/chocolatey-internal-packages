e62a405fbecd97d210a187061e4c549d43fd2058
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-10-07 17:34:02 +0200

    Tierbase (#19)
    * tierbase installer and updater
    
    * tierbase uninstaller

---
+++ automatic/tierbase/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'tierbase'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = ''
+  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($LogPath)$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
+  validExitCodes = @(0)
+  softwareName   = '4D v*'
+  checksum       = ''
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
