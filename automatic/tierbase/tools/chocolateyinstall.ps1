aadaca649a9622315ed234521bf81daa00fbd6b0
Chocolatey <chocolatey@realdimensions.net>
2020-10-07 15:37:38 +0000

    AU: 1 updated - tierbase [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/245b24c92bca303d77fe787553b88b14cb89f986

--- automatic/tierbase/tools/chocolateyinstall.ps1
+++ automatic/tierbase/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'tierbase'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
-  url            = ''
+  url            = 'https://www.uni-heidelberg.de/md/ibf/datenbanken/4d_v14.6_windows_32-bit.exe'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($LogPath)$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
   softwareName   = '4D v*'
-  checksum       = ''
+  checksum       = '132dc0414466a644581b5ffe58efac6c95e778e3cb745dfdb39cf8ce6002d05d'
   checksumType   = 'sha256'
 }
 
