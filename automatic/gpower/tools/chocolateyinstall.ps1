6e784f9d8c9f8ccab450b86b75aa9fb85633e6df
Chocolatey <chocolatey@realdimensions.net>
2020-10-13 06:55:55 +0000

    AU: 1 updated - gpower [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/86061085eeb65da81931b9d48f71b08555733eb9

--- automatic/gpower/tools/chocolateyinstall.ps1
+++ automatic/gpower/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = ''
+  url           = 'https://www.psychologie.hhu.de//fileadmin/redaktion/Fakultaeten/Mathematisch-Naturwissenschaftliche_Fakultaet/Psychologie/AAP/gpower/GPowerWin_3.1.9.7.zip'
   silentArgs    = '/S'
   softwareName  = 'gpower*'
-  checksum      = ''
+  checksum      = '653615497c763863bb10a06d605ba0073cf5f9f035bc6246e0dd21405a39bc6d'
   checksumType  = 'sha256'
 }
 
