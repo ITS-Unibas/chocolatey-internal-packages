1c918c1ffdf6bd55e1e6ddecf7c824caecb0a919
Chocolatey <chocolatey@realdimensions.net>
2020-10-13 07:15:11 +0000

    AU: 1 updated - unibas-greenshot [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/b6cbd718b4bd6737cb27704fe067ebcb5d6af6fd

--- automatic/unibas-greenshot/tools/chocolateyinstall.ps1
+++ automatic/unibas-greenshot/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = ''
+  url           = 'https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.10.6/Greenshot-INSTALLER-1.2.10.6-RELEASE.exe'
   #OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
   silentArgs    = '/S'           # NSIS
@@ -25,7 +25,7 @@ $packageArgs = @{
 
   # optional, highly recommended
   softwareName  = 'Greenshot*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = ''
+  checksum      = '1687311b4e7a3720be20490e8ed6cc772a32336a7bed8896e475b8ec616c6b81'
   checksumType  = 'sha256' #default is md5, can also be sha1
 }
 
