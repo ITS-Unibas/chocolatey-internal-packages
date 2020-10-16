07c0670e8e59e636c34e147eeea1c29463a93046
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-16 12:56:50 +0200

    Adds irfanview

---
+++ automatic/unibas-irfanview/update.ps1
@@ -0,0 +1,29 @@
+﻿Import-Module AU
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$releases = 'https://www.irfanview.com/'
+
+function global:au_BeforeUpdate() {
+  Get-RemoteFiles -Purge -FileNameBase 'unibas-irfanview'
+  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'sha256'
+}
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1`'$($Latest.URL)`'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex = 'x64_setup.exe$'
+  $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -match '(?:iview)(\d*)'
+  $version = $Matches[0].replace('iview', '')
+  $version = $version.insert(1, ".")
+  Write-Host $version
+  return @{ Version = $version; URL = $url }
+}
+
+update -ChecksumFor none -NoCheckChocoVersion
