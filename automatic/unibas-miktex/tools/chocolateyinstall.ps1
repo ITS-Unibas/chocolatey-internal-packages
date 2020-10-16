35c5cef84d0d1004a09e7d5e579895a65311786e
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-16 08:43:48 +0200

    pushing update manually

--- automatic/unibas-miktex/tools/chocolateyinstall.ps1
+++ automatic/unibas-miktex/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = ''
+  url64bit       = 'https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-20.10-x64.exe'
   silentArgs     = '--no-registry --shared --unattended --local-package-repository=C:\Temp\MikTex\'
   softwareName   = 'miktex*'
-  checksum64     = ''
+  checksum64     = 'cb9434d9ba0cedb480500f9bdfe437cd08fb57583704cabab5697e53bfba610b'
   checksumType64 = 'sha256'
 }
 
