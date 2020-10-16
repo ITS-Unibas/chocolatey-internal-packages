60abfe743c36240593f991173f55483f568a275a
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-16 08:25:30 +0200

    Adds miktex

---
+++ automatic/unibas-miktex/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'unibas-miktex'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url64bit       = ''
+  silentArgs     = '--no-registry --shared --unattended --local-package-repository=C:\Temp\MikTex\'
+  softwareName   = 'miktex*'
+  checksum64     = ''
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
