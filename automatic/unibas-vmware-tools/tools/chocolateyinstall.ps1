440f519739d679a84f3272830c866da15fdbdf63
Chocolatey <chocolatey@realdimensions.net>
2020-10-16 09:56:07 +0000

    AU: 3 updated - camtasia snagit unibas-vmware-tools [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/5240719fb887d66f6c2078da973a8f4b6a1d06a1

--- automatic/unibas-vmware-tools/tools/chocolateyinstall.ps1
+++ automatic/unibas-vmware-tools/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'VMWare Tools*'
   fileType       = 'EXE'
-  url            = 'https://packages.vmware.com/tools/releases/latest/windows/x64/VMware-tools-11.1.5-16724464-x86_64.exe'
-  checksum       = '7919592ed91712205969848f0b9d8c278abaf3df6f31cea16731f17fcd7f12ea'
+  url            = 'https://packages.vmware.com/tools/releases/latest/windows/x64/VMware-tools-11.2.0-16938113-x86_64.exe'
+  checksum       = '12e5190abcd12c301a1bf04cdadae471b590a349d5c45bddd2cae1b39f258da5'
   checksumType   = 'sha256'
   silentArgs     = "/S /v `"/qn /l*v `"$env:SWP\Install_vmware-tools_$env:ChocolateyPackageVersion.log`" REBOOT=R`""
   validExitCodes = @(0, 3010)
