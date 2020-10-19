7727bfa9bb2908ec24a01905f7d81ef5136738cf
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-10-19 16:50:36 +0200

    python3

---
+++ automatic/unibas-python3/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# Old Camtasia versions can be found at https://www.techsmith.com/download/oldversions
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url     = 'https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe'
+
+  checksum    = 'fd2e2c6612d43bb6b213b72fc53f07d73d99059fa72c96e44bde12e7815073ae'
+  checksumType= 'SHA256'
+
+  silentArgs    = "/quiet"
+  softwareName = "Python*"
+  validExitCodes= @(0)
+}
+
+
+
+Install-ChocolateyPackage @packageArgs
