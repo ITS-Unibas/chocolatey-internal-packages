0dcf2ce5872542666a06516238080c4e98316c1b
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-20 09:54:52 +0200

    Adds jabref

---
+++ automatic/unibas-jabref/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'unibas-jabref'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  url64bit       = ''
+  silentArgs     = "/quiet /qn /norestart"
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'unibas-jabref*'
+  checksum64     = ''
+  checksumType64 = 'sha256' #default is checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
