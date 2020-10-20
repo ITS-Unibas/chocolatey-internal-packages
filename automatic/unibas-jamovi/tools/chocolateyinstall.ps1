7397922ce1e20deb74d832991d28430120df4d08
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-20 10:18:49 +0200

    Adds jamovi

---
+++ automatic/unibas-jamovi/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'unibas-jamovi'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url64bit       = ''
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'unibas-jamovi*'
+  checksum64     = ''
+  checksumType64 = 'sha256' #default is checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
