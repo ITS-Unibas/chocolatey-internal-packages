ce56bf2d2a90d033c76e9b06a4c0189fa30aeea1
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-23 14:32:23 +0200

    Adds mnova

---
+++ automatic/mnova/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'mnova'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  url64bit       = ''
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'mnova*'
+  checksum64     = ''
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/mnova/update.ps1
@@ -0,0 +1,30 @@
+﻿Import-Module AU
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$releases = 'https://mestrelab.com/download/mnova/'
+
+function global:au_BeforeUpdate() {
+  Get-RemoteFiles -Purge -FileNameBase 'mnova'
+  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'sha256'
+}
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1`'$($Latest.URL)`'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex = '.msi$'
+  $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $url = "https://mestrelab.com$url"
+  $arr = $url -split '-|_.*.msi'
+  $version = $arr[1]
+  $Options = @{ Headers = @{Referer = $releases}}
+  $options.Headers.Add("User-Agent", [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox)
+  return @{ Version = $version; URL = $url; Options = $Options }
+}
+
+update -ChecksumFor none
