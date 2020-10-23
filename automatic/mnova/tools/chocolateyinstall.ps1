2e9ee5df2eabc0f8d25f967fd09e38901c8c7854
Chocolatey <chocolatey@realdimensions.net>
2020-10-23 12:35:28 +0000

    AU: 1 updated - mnova [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/103d56b95f36b9a218c98f34b9a7a396ee19b2a6

--- automatic/mnova/tools/chocolateyinstall.ps1
+++ automatic/mnova/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url64bit       = ''
+  url64bit       = 'https://mestrelab.com/downloads/mnova/win/msi/MestReNova-14.2.0-26256_x64.msi'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'mnova*'
-  checksum64     = ''
+  checksum64     = '1f417322434bb0467911553964b04e3067e106b58fab0d52fcd37c1b0a43331c'
   checksumType64 = 'sha256'
 }
 
