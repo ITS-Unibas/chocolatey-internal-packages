a314d3ba75531d130cbe2a3783d652702ef174d2
Chocolatey <chocolatey@realdimensions.net>
2020-10-26 13:54:27 +0000

    AU: 1 updated - unibas-visioviewer2016 [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/58134f3c214714364c25e3b90637f0e270a419e4

--- automatic/unibas-visioviewer2016/tools/chocolateyinstall.ps1
+++ automatic/unibas-visioviewer2016/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'unibas-visioviewer2016'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
-  url            = ''
+  url            = 'https://download.microsoft.com/download/D/B/7/DB790874-4414-417F-ADF6-348B29572B9F/visioviewer_4339-1001_x64_en-us.exe'
   silentArgs     = '/quiet /passive /norestart'
   validExitCodes = @(0, 1638, 3010)
   softwareName   = 'Microsoft Visio Viewer'
-  checksum       = ''
+  checksum       = 'b8ff91c1d37284ef4ec8380045a48e915cfe75536c700d5a8bbe229f1f1fafa1'
   checksumType   = 'sha256'
 }
 
