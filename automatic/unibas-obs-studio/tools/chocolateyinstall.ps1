7c2c6ba7a58a9f61847a9d5d7fd9dadf7b7990ea
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-26 14:41:55 +0100

    Adds unibas-obs-studio.nuspec

---
+++ automatic/unibas-obs-studio/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$file64Location = Get-Item $toolsDir\*_x64.exe
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  fileType     = 'EXE'
+  file64       = $file64Location
+  softwareName = 'OBS Studio'
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item $packageArgs.file64 -Force -ea 0
---
+++ automatic/unibas-obs-studio/update.ps1
@@ -0,0 +1,33 @@
+Import-Module au
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$releases = 'https://github.com/obsproject/obs-studio/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(^\s*x64:).*"         = "`${1} $($Latest.URL64)"
+            "(?i)(^\s*checksum64\:).*" = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+function global:au_BeforeUpdate() {
+    Get-RemoteFiles -Purge
+}
+
+function global:au_GetLatest {
+    $cache_page = (Invoke-WebRequest $releases -UseBasicParsing).Links.href
+    $download_page = $cache_page | Select-String '/tag/' | Select-Object -First 1
+    $Matches = $null
+    $download_page -match '\d+\.\d+\.\d+'
+    $version = $Matches[0]
+    # Filenames omit ending '0' in version string; just scrape again.
+    $url64 = 'https://github.com' + ($cache_page | Select-String 'x64.exe$' | Select-Object -First 1 )
+
+    return @{ Version = $version; URL64 = $url64; }
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+    # run the update only if script is not sourced
+    Update-Package -checksumfor none -NoCheckChocoVersion
+}
