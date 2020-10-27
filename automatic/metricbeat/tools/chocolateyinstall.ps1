23b9bb3788759d28a55c137d63036bca2b9fb552
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-27 08:45:53 +0100

    Adding beats

---
+++ automatic/metricbeat/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'metricbeat'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64 = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.9.3-windows-x86_64.msi'
+
+$packageArgs = @{
+  packageName    = $packageName
+  softwareName   = "Beats metricbeat"
+  fileType       = 'msi'
+  unzipLocation  = $toolsDir
+  url64bit       = $url64
+  checksum64     = '95f9cad6904ba9f7e6908c8587268826872339a0f62a849615caf449ae820a81f02ef1f7cee7699b7c4b49a11d8d6a54ddddee0cfdcb6dbaba91097b825a4a90'
+  checksumType64 = 'sha512'
+  checksumType   = $checksumType64
+  silentArgs     = '/qn /norestart'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/packetbeat/update.ps1
@@ -0,0 +1,48 @@
+import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$url = 'https://www.elastic.co/downloads/beats/packetbeat'
+$packageName = 'packetbeat'
+
+function global:au_SearchReplace {
+  @{
+    'tools\chocolateyInstall.ps1'   = @{
+      "(^\s*[$]packageName\s*=\s*)(['""].*['""])" = "`$1'$($packageName)'"
+      "(^\s*[$]url64\s*=\s*)(['""].*['""])"       = "`$1'$($Latest.URL64)'"
+      "(^\s*[$]*checksum64\s*=\s*)('.*')"         = "`$1'$($Latest.Checksum64)'"
+      "(^\s*[$]*checksumType64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64Type)'"
+    };
+    'tools\chocolateyUnInstall.ps1' = @{
+      "(^\s*[$]packageName\s*=\s*)(['""].*['""])" = "`$1'$($packageName)'"
+    };
+    "$($packageName).nuspec"        = @{
+      "(<projectSourceUrl.*?)(\d+\.\d+)(.*?projectSourceUrl>)" = "`${1}$([regex]::match($Latest.Version, "\d+\.\d+").Value)`${3}"
+      "(<docsUrl.*?)(\d+\.\d+)(.*?docsUrl>)"                   = "`${1}$([regex]::match($Latest.Version, "\d+\.\d+").Value)`${3}"
+    };
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = invoke-webrequest $url -UseBasicParsing -DisableKeepAlive
+
+  $links = $download_page.Links | Where-Object { $_.href -imatch "msi" -and $_.href -notmatch "alpha|beta|-rc\d-" }
+
+  $url64 = ($links | Where-Object { $_.href -imatch "x86_64.msi$" }).href
+
+  $reVersion = "-(\d+.\d+.\d+)-"
+  $arr = $url64 -split $reVersion
+  $version = $arr[1]
+
+  $x = ($links | Where-Object { $_.href -imatch "x86_64.msi.sha" -and $_.href -match "msi\.(\w+)$" }).href | Select-Object -first 1
+  $checksum64Type = $Matches[1]
+  $checkSum64 = (Invoke-WebRequest ($x) -UseBasicParsing -DisableKeepAlive)
+  $checkSum64 = [System.Text.Encoding]::Default.GetString($checkSum64.Content)
+  $checkSum64 = ($checkSum64 | Out-String) -imatch "^\S+"
+  $checkSum64 = $Matches[0]
+
+  $Latest = @{ URL64 = $url64; Version = $version; Checksum64 = $checkSum64; Checksum64Type = $checksum64Type; }
+  return $Latest
+}
+
+update -ChecksumFor none -NoCheckChocoVersion
