5efc9f981bc6de8e9d3643e06310255330915acf
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-29 09:33:59 +0100

    adds google earth

---
+++ automatic/unibas-googleearth/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'unibas-googleeath'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = ''
+  silentArgs     = '/S OMAHA=1'
+  validExitCodes = @(0)
+  softwareName   = 'Google Earth*'
+  checksum       = ''
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
