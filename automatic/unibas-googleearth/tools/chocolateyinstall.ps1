cd7897d34f61cd5d4cffea590b2414d09368b582
Chocolatey <chocolatey@realdimensions.net>
2020-10-29 08:40:02 +0000

    AU: 1 updated - unibas-googleearth [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/6ed6cb1675d16d16517b9841d7ef4d4c5739cc43

--- automatic/unibas-googleearth/tools/chocolateyinstall.ps1
+++ automatic/unibas-googleearth/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = ''
+  url            = 'https://dl.google.com/dl/earth/client/advanced/current/googleearthprowin-7.3.3-x64.exe'
   silentArgs     = '/S OMAHA=1'
   validExitCodes = @(0)
   softwareName   = 'Google Earth*'
-  checksum       = ''
+  checksum       = 'fe2711c7cb75455b52d7e4a6700be296fd567af607d5d43bfa8ba7451bb7c2b7'
   checksumType   = 'sha256'
 }
 
