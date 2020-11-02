f7f3d659de9c5a2bb132f6d9b7b1eda92309541e
Chocolatey <chocolatey@realdimensions.net>
2020-11-02 12:04:35 +0000

    AU: 1 updated - unibas-miktex [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/7da0f2f269b96f69994ea7d72a3a254a0335975d

--- automatic/unibas-miktex/tools/chocolateyinstall.ps1
+++ automatic/unibas-miktex/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = 'https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-20.10-x64.exe'
+  url64bit       = 'https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-20.11-x64.exe'
   silentArgs     = '--no-registry --shared --unattended --local-package-repository=C:\Temp\MikTex\'
   softwareName   = 'miktex*'
-  checksum64     = 'cb9434d9ba0cedb480500f9bdfe437cd08fb57583704cabab5697e53bfba610b'
+  checksum64     = 'a77f8d0052d6be81e71fc53b21320cb691b5a9c33378707d899930efbb350487'
   checksumType64 = 'sha256'
 }
 
