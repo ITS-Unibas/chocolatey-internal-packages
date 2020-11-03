58e129790cdfa20609f1a0fa4aeb4141cc43406d
Chocolatey <chocolatey@realdimensions.net>
2020-11-03 10:48:29 +0000

    AU: 2 updated - msedge unibas-infrarecorder [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/20d0dcfe6b6e28c14ab2e1635e25c72cb312c19b

--- automatic/unibas-infrarecorder/tools/chocolateyinstall.ps1
+++ automatic/unibas-infrarecorder/tools/chocolateyinstall.ps1
@@ -9,12 +9,12 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url            = ''
+  url            = 'https://sourceforge.net/projects/infrarecorder/files/InfraRecorder/0.53/ir053_x64.msi/download'
   silentArgs     = '/quiet /norestart' # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0, 3010, 1641)
 
   softwareName   = 'InfraRecorder*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = ''
+  checksum       = '332704763605252EF9E88D2A8CE6D076719F0ACB307DB231069483AE5CA407FF'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
