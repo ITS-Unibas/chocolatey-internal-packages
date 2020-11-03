fe6747075fdd37209379ae671af5b3054941c5e8
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-11-03 11:45:48 +0100

    infrarecorder

---
+++ automatic/unibas-infrarecorder/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName = 'unibas-infrarecorder'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  url            = ''
+  silentArgs     = '/quiet /norestart' # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes = @(0, 3010, 1641)
+
+  softwareName   = 'InfraRecorder*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum       = ''
+  checksumType   = 'sha256' #default is md5, can also be sha1
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/unibas-infrarecorder/update.ps1
@@ -0,0 +1,32 @@
+﻿Import-Module AU
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$releases = 'https://sourceforge.net/projects/infrarecorder/files/InfraRecorder/'
+
+function global:au_BeforeUpdate() {
+  $file = Invoke-WebRequest -UseBasicParsing -Uri $Latest.URL -OutFile "infrarecorder.exe" -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox
+  $Latest.Checksum = Get-FileHash "$PSScriptRoot\infrarecorder.exe" -Algorithm 'sha256' | Select-Object -ExpandProperty Hash
+}
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $version = "0.0"
+  $download_page.Links | Where-Object href -match "projects\/infrarecorder\/files\/InfraRecorder\/(\d+\.\d+\.\d+|\d+\.\d+)" | ForEach-Object {
+    if([version]$Matches[1] -gt [version]$version) {
+      $version = $Matches[1]
+    }
+  }
+  $downloadLink = Invoke-WebRequest "$releases/$version"
+  $url = $downloadLink.Links | Where-Object href -match ".*msi/.*download" | Select-Object -ExpandProperty href
+
+  return @{ Version = $version; URL = $url }
+}
+
+update -ChecksumFor none -NoCheckChocoVersion
