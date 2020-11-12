1b3577701e7fa979015dd1777688868f14d1e85b
Chocolatey <chocolatey@realdimensions.net>
2020-11-12 16:02:42 +0000

    AU: 1 updated - unibas-veracrypt [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/f05adce129f0dac3574c9d6236901f858303fc6d

--- automatic/unibas-veracrypt/tools/chocolateyinstall.ps1
+++ automatic/unibas-veracrypt/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'unibas-veracrypt'
   fileType       = 'msi'
-  url            = ''
+  url            = 'https://www.kooijman.biz/files/VeraCrypt123HF2.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'VeraCrypt*'
-  checksum       = ''
+  checksum       = 'ADA3334B3443BFE3A65B72D4FB21B734DA113A2C44D272A8416DBBBBDC2F15A4'
   checksumType   = 'sha256'
 }
 
