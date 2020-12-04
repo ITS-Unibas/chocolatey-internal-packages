a0883b94c775ed23c25d3b58c536174f831e8089
Philip Allen Stokes - stokes-adm <stokes-adm@unibas.ch>
2020-12-04 14:54:15 +0100

    start-acrobat-reader

---
+++ automatic/unibas-acrobat-reader/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'Anaconda3'
+  fileType       = 'EXE'
+  url            = 'https://repo.continuum.io/archive/Anaconda3-2020.11-Windows-x86_64.exe'
+  checksum       = 'aa523115daf31c431bb392faf75e70d35ada935e36dc7b1dd8902baee240bcc1'
+  checksumType   = 'sha256'
+  silentArgs     = "/S /InstallationType=AllUsers /RegisterPython=1 /AddToPath=1 /D=C:\Program Files\Anaconda3"
+  validExitCodes = @(0)
+}
+
+Write-Warning 'The Anaconda3 installation can take a long time (up to 30 minutes).'
+Write-Warning 'Please be patient and let it finish.'
+Write-Warning 'If you want to verify the install is running, you can watch the installer process in Task Manager'
+
+Install-ChocolateyPackage @packageArgs
