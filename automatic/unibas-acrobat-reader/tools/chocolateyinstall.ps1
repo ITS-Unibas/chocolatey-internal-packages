a1d13db7b9c3995969991ec3f4903e142a2174cd
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-12-07 10:10:35 +0100

    Acrobat reader (#22)
    * start-acrobat-reader
    
    * finished reader
    
    Co-authored-by: Philip Allen Stokes - stokes-adm <stokes-adm@unibas.ch>

---
+++ automatic/unibas-acrobat-reader/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'Adobe Acrobat DC Reader*'
+  fileType       = 'EXE'
+  url            = ''
+  checksum       = ''
+  checksumType   = 'sha256'
+  silentArgs     = "/sAll /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES DISABLEDESKTOPSHORTCUT=1 DISABLE_ARM_SERVICE_INSTALL=1 UPDATE_MODE=0 /L*v `"$env:TEMP\$env:chocolateyPackageName.$env:chocolateyPackageVersion.Install.log`""
+  validExitCodes = @(0, 1000, 1101)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/unibas-acrobat-reader/update.ps1
@@ -0,0 +1,50 @@
+﻿import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+#Thanks to https://stackoverflow.com/questions/20259251/powershell-script-to-check-the-status-of-a-url
+function Get-UrlStatusCode([string] $Url)
+{
+    try
+    {
+        (Invoke-WebRequest -Method HEAD -Uri $Url -UseBasicParsing -DisableKeepAlive).StatusCode
+    }
+    catch [Net.WebException]
+    {
+        [int]$_.Exception.Response.StatusCode
+    }
+}
+function global:au_GetLatest {
+   # Discover the latest release version
+   $VersionURL  = 'https://helpx.adobe.com/acrobat/release-note/release-notes-acrobat-reader.html'
+   $download_page = Invoke-WebRequest -Uri $VersionURL #-UseBasicParsing -DisableKeepAlive
+   $Version = "0.0"
+   $download_page | Select-String -Pattern "((\d+)\.(\d+)\.(\d+))" -AllMatches | ForEach-Object Matches | ForEach-Object {
+      if ([version]$_.Value -gt [version]$Version -and (Get-UrlStatusCode "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/$($_.Value.Replace('.',''))/AcroRdrDC$($_.Value.Replace('.',''))_MUI.exe") -ne '404') {
+         $Version = $_.Value
+      }
+   }
+   $VersionWOPoints = $Version.Replace('.','')
+
+   return  @{
+       Version = $Version
+       URL     = "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/$VersionWOPoints/AcroRdrDC$($VersionWOPoints)_MUI.exe"
+   }
+}
+
+function global:au_SearchReplace {
+   @{
+     ".\tools\chocolateyInstall.ps1" = @{
+       "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
+       "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+     }
+   }
+ }
+
+function global:au_BeforeUpdate() {
+   $Latest.Checksum = Get-RemoteChecksum $Latest.URL
+}
+
+Update-Package -NoCheckChocoVersion
+
+
