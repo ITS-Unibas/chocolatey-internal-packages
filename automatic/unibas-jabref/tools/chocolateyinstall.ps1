1208680193f7b9a5ca3418dac7c0c886dc0294b2
Chocolatey <chocolatey@realdimensions.net>
2020-12-25 00:04:25 +0000

    AU: 1 updated - unibas-jabref [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/097923c2b90134a14883cb5b5a4f0f7e163945c5

--- automatic/unibas-jabref/tools/chocolateyinstall.ps1
+++ automatic/unibas-jabref/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url64bit       = 'https://github.com//JabRef/jabref/releases/download/v5.1/JabRef-5.1.msi'
+  url64bit       = 'https://github.com//JabRef/jabref/releases/download/v5.2/JabRef-5.2.msi'
   silentArgs     = "/quiet /qn /norestart"
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'unibas-jabref*'
-  checksum64     = '1e74d351a2ac6362416754b2781d9061ef1c156edfb16b478a45c3dbcbde8c8d'
+  checksum64     = 'f3523a0c82473b79fc1021ed4b5d444018a5150771ffa579d3d4faaaf2aa66d0'
   checksumType64 = 'sha256' #default is checksumType
 }
 
