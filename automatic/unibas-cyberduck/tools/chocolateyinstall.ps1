73760875ab67fc4d22ed363ff75b6f42801490b5
Chocolatey <chocolatey@realdimensions.net>
2021-01-15 12:05:25 +0000

    AU: 1 updated - unibas-cyberduck [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/68c793b5a7c8a201ee67a304895d53ebea0a3acb

--- automatic/unibas-cyberduck/tools/chocolateyinstall.ps1
+++ automatic/unibas-cyberduck/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://update.cyberduck.io/windows/Cyberduck-Installer-7.8.0.34081.exe'
+  url            = 'https://update.cyberduck.io/windows/Cyberduck-Installer-7.8.1.34119.exe'
   #file         = $fileLocation
 
   #MSI
@@ -32,7 +32,7 @@ $packageArgs = @{
 
   # optional, highly recommended
   softwareName   = 'Cyberduck' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = 'fc11e4fa512663813cd2102558e60f439a1017424a83841714907dbf062c0bea'
+  checksum       = 'b95ed8abf05bfe5101160b9edb8e9f982d70179ac6c02abdfd2653ee832ca2e7'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
