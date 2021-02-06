a19a07ed5150b5867c9c5a33e952db681230fe70
Chocolatey <chocolatey@realdimensions.net>
2021-02-06 00:04:26 +0000

    AU: 2 updated - firefoxesr msedge [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/20884a0a1017779688e2ccbee242c043ff7bdd13

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/7b4e86db-a89d-4f34-9e12-f218b875b303/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/e2d06b69-9e44-45e1-bdf5-b3b827fe06b2/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = '5FC512514800E9503856036795879F86DB4E797148C3589B794884BB59718A1D'
+  checksum       = 'B6616258484997E8AB77EFCE5C313EDEFD1F056159ACA70156122414C0BD2E60'
   checksumType   = 'sha256'
 }
 
