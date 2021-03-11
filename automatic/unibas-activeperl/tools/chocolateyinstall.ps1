ca403333f4483c409dbdabecc07af7d784e25a4b
Philip Allen Stokes - stokes-adm <stokes-adm@unibas.ch>
2021-03-11 13:47:23 +0100

    activeperl

---
+++ automatic/unibas-activeperl/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'unibas-activeperl'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  url            = ''
+  silentArgs     = 'INSTALLDIR="C:\Program Files\Perl64" /q /l*v `"$($env:SWP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`"'
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'unibas-activeperl*'
+  checksum       = ''
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
