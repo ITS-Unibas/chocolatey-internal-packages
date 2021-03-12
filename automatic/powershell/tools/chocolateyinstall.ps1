e328fe6a608e98facbe1cd339429efd6c8558bbc
Chocolatey <chocolatey@realdimensions.net>
2021-03-12 00:04:47 +0000

    AU: 1 updated - powershell [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/b47f4d06ce20461ba0a40070c8a6f8c34f2934ac

--- automatic/powershell/tools/chocolateyinstall.ps1
+++ automatic/powershell/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'powershell'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://github.com/PowerShell/PowerShell/releases/download/v7.1.2/PowerShell-7.1.2-win-x64.msi'
+  url            = 'https://github.com/PowerShell/PowerShell/releases/download/v7.1.3/PowerShell-7.1.3-win-x64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'PowerShell*'
-  checksum       = '8381D72E31415B533FCFD349E3AF1A62EFA8C793202A260C1C724D10C25BB49D'
+  checksum       = '459642D8B6D69F643794DF8394F28191F43E5ED35472899C4F0D8424F6D1317C'
   checksumType   = 'sha256'
 }
 
