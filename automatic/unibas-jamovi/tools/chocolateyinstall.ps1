8b97bb534ac3e2bb6908541e640e8604ab44fb7f
Chocolatey <chocolatey@realdimensions.net>
2021-04-17 12:03:27 +0000

    AU: 1 updated - unibas-jamovi [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/6e3090354b7d075fc64ba533d557dbed95f1ceb0

--- automatic/unibas-jamovi/tools/chocolateyinstall.ps1
+++ automatic/unibas-jamovi/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = 'https://jamovi.org//downloads/jamovi-1.6.15.0-win64.exe'
+  url64bit       = 'https://jamovi.org//downloads/jamovi-1.6.23.0-win64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'unibas-jamovi*'
-  checksum64     = 'ece58a12b584c63f6e5c21fa6aad4fca237850e6ae6b1a829777b3b20ab4c89a'
+  checksum64     = '59eda2aec3ef929aca5a28d164d64a2487a707533f0609cb7a0f7705b1652e85'
   checksumType64 = 'sha256' #default is checksumType
 }
 
