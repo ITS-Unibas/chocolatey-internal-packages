79511e69972468a6b19577cd6c91479f535022d1
MaxBurgert <max.burgert@unibas.ch>
2021-04-26 08:33:02 +0200

    Update chocolateyinstall.ps1

--- automatic/zoom/tools/chocolateyinstall.ps1
+++ automatic/zoom/tools/chocolateyinstall.ps1
@@ -1,15 +1,23 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
+$checksum = 'dasdasd'
+$checksum64 = 'asdad'
+
+$url = 'https://cdn.zoom.us/prod/5.6.1.617/ZoomInstallerFull.msi'
+$url64 = 'https://cdn.zoom.us/prod/5.6.1.617/x64/ZoomInstallerFull.msi'
 
 $packageArgs = @{
   packageName    = 'zoom'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://zoom.us/client/5.5.13142.0301/ZoomInstallerFull.msi'
+  url            = $url
+  url64          = $url64
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'zoom*'
-  checksum       = '97d4f8e3ab287a38b49f93b7a0f9f246'
-  checksumType   = 'md5'
+  checksum       = $checksum
+  checksum64     = $checksum64
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
