09c8c81361e422a777c04a7fd1b38a1ad75f0aa7
Chocolatey <chocolatey@realdimensions.net>
2021-06-02 00:04:49 +0000

    AU: 3 updated - firefoxesr rstudio-automatic unibas-obs-studio [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/300531d7ddf90a29783c8b72fbfb82e49ed4dc62

--- automatic/rstudio-automatic/tools/chocolateyinstall.ps1
+++ automatic/rstudio-automatic/tools/chocolateyinstall.ps1
@@ -7,12 +7,12 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://download1.rstudio.org/desktop/windows/RStudio-1.4.1106.exe'
+  url           = 'https://download1.rstudio.org/desktop/windows/RStudio-1.4.1717.exe'
   silentArgs   = '/S'
   validExitCodes= @(0)
  
   softwareName  = 'rstudio*'
-  checksum      = '64c821269730f380ebd2ac842451016c'
+  checksum      = 'c5b3efc7f08e0de6daa1658ed86a2ced'
   checksumType  = 'md5'
 }
 
