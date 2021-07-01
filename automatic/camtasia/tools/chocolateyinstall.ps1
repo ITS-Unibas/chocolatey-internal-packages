36f51893275530d015139d1fff9db3ef1f744aa7
Chocolatey <chocolatey@realdimensions.net>
2021-07-01 00:04:50 +0000

    AU: 1 updated - camtasia [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/7a76692fb258454fb037eb88408094060fb3c383

--- automatic/camtasia/tools/chocolateyinstall.ps1
+++ automatic/camtasia/tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Old Camtasia versions can be found at https://www.techsmith.com/download/oldversions
-$url64      = 'https://download.techsmith.com/camtasiastudio/releases/2104/camtasia.msi'
+$url64      = 'https://download.techsmith.com/camtasiastudio/releases/2105/camtasia.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
   url64bit      = $url64
 
-  checksum64    = '19307649101842fb0d82286fb1fd90788539c8cfa7a82710eb3aade7a56b09f0'
+  checksum64    = '21de2080258bee12d63a5111991fe4e313879ec7782bd80245c813831e87f224'
   checksumType64= 'SHA256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
