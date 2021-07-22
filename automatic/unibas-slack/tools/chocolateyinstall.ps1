96b29907012a90ed1efcea31c1607e6b97e7495e
uwe_molnar <76432776+uwemolnar@users.noreply.github.com>
2021-07-22 17:14:46 +0200

    Update chocolateyinstall.ps1
    correcting variables

--- automatic/unibas-slack/tools/chocolateyinstall.ps1
+++ automatic/unibas-slack/tools/chocolateyinstall.ps1
@@ -2,17 +2,23 @@
 
 $packageName = 'unibas-slack'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32          = 'https://downloads.slack-edge.com/releases/windows/4.18.0/prod/ia32/slack-standalone-4.18.0.0.msi'
+$checksum32     = '3cad47d22b22d5460becc95b7f75700191e715cc05921646dd11e938d12b6f90'
+$checksumType32 = 'sha256'
+$url64          = 'https://downloads.slack-edge.com/releases/windows/4.18.0/prod/x64/slack-standalone-4.18.0.0.msi'
+$checksum64     = '7821699675bc67b63d1c7f2eb08dfc41ce3359d4d3cb37742ddb74090073eb1c'
+$checksumType64 = 'sha256'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'msi'
-  url           = 'https://downloads.slack-edge.com/releases/windows/4.18.0/prod/ia32/slack-standalone-4.18.0.0.msi'
-  checksum      = '3cad47d22b22d5460becc95b7f75700191e715cc05921646dd11e938d12b6f90'
-  checksumType  = 'sha256'
-  url64bit      = 'https://downloads.slack-edge.com/releases/windows/4.18.0/prod/x64/slack-standalone-4.18.0.0.msi'
-  checksum64    = '7821699675bc67b63d1c7f2eb08dfc41ce3359d4d3cb37742ddb74090073eb1c'
-  checksumType64= 'sha256'
+  url           = $url32
+  checksum      = $checksum32
+  checksumType  = $checksumType32
+  url64bit      = $url64
+  checksum64    = $checksum64
+  checksumType64= $checksumType64
   softwareName  = 'unibas-slack*'
   silentArgs    = "/qn /norestart"
   validExitCodes= @(0)
