b9f7e7ff01fa5c42a9fb8a4ee331b14b9d06d9c4
Chocolatey <chocolatey@realdimensions.net>
2021-08-02 07:30:32 +0000

    AU: 1 updated - nis-elements-viewer [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/5db28a3e62f99d48eb6e360f7804aa4706db695c

--- automatic/nis-elements-viewer/tools/chocolateyinstall.ps1
+++ automatic/nis-elements-viewer/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = ''
+  url            = 'https://d33b8x22mym97j.cloudfront.net/phase5/files/downloads/NIS_Viewer_5.21.00_b1483_64bit.zip'
   silentArgs     = "/s /sms /f1C:\temp\setup.iss /f2c:\temp\install.txt" # s: silent, sms: wait for process, f1: import setup.iss-file for silent install, f2: install-log
   softwareName   = 'NIS-Elements viewer*' 
-  checksum       = ''
+  checksum       = '82dfc2dd6419c4c4ffc9eb81ea4b58c2296528fa3a0c5ec5c3505402afbcf989'
   checksumType   = 'sha256'
 }
 
