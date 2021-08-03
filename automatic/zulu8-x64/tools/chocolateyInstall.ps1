981057e4bf792a41fe9217e081b684114c0ae6e9
Chocolatey <chocolatey@realdimensions.net>
2021-08-03 00:22:28 +0000

    AU: 1 updated - zulu8-x64 [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/86a70082ac83e418b8363b9e5cfc5c3b5579f33d

--- automatic/zulu8-x64/tools/chocolateyInstall.ps1
+++ automatic/zulu8-x64/tools/chocolateyInstall.ps1
@@ -2,13 +2,13 @@
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-$urljre = 'https://cdn.azul.com/zulu/bin/zulu8.56.0.21-ca-jre8.0.302-win_x64.zip'
-$urljdkfx = 'https://cdn.azul.com/zulu/bin/zulu8.54.0.21-ca-fx-jdk8.0.292-win_x64.zip'
-$urljrefx = 'https://cdn.azul.com/zulu/bin/zulu8.54.0.21-ca-fx-jre8.0.292-win_x64.zip'
+$urljre = 'https://cdn.azul.com/zulu/bin/zulu8.56.0.23-ca-fx-jre8.0.302-win_x64.zip'
+$urljdkfx = 'https://cdn.azul.com/zulu/bin/zulu8.56.0.23-ca-fx-jdk8.0.302-win_x64.zip'
+$urljrefx = 'https://cdn.azul.com/zulu/bin/zulu8.56.0.23-ca-fx-jre8.0.302-win_x64.zip'
 
-$checksumurljre = '4885567e6ca047e437b55c42919c20979bbe02cca2a3b66916e84b2b3f5df35c'
-$checksumurljdkfx = '84efdbd536497f5f21ef4412e2045b637255d135be873ea05184f1f7924c3828'
-$checksumurljrefx = '1158de7458620fbc0ceaddc4b754aedcbd869a1ce097f4b3ddb7a4b27a66c1ce'
+$checksumurljre = '89c714f23e1c03f96552b39a9bf94da2025333cb11f094659191f49c9082dfb5'
+$checksumurljdkfx = '9f6ac415dfcb615078b19cb0304be020b10c1b160e900166bbc993dafade494a'
+$checksumurljrefx = '89c714f23e1c03f96552b39a9bf94da2025333cb11f094659191f49c9082dfb5'
 
 
 Get-ChocolateyWebFile -PackageName 'zulu8-x64' -FileFullPath "$toolsDir\JRE\zulu8-x64-jre.zip" -Url $urljre -Checksum $checksumurljre -ChecksumType 'sha256'
@@ -18,8 +18,8 @@ Get-ChocolateyWebFile -PackageName 'zulu8-x64' -FileFullPath "$toolsDir\FX\JRE\z
 $packageArgs = @{
     packageName    = 'zulu8-x64'
     fileType       = 'msi'
-    url64bit       = 'https://cdn.azul.com/zulu/bin/zulu8.56.0.21-ca-jdk8.0.302-win_x64.msi'
-    checksum64     = '89b39bc320ef09ee688c6884722ad6f3e1719c462ccfe8a4435b2f50d7ffccb4'
+    url64bit       = 'https://cdn.azul.com/zulu/bin/zulu8.56.0.23-ca-fx-jdk8.0.302-win_x64.msi'
+    checksum64     = 'f35809cbf8ef4454fe923bc3785c2755faeaea3dc0b43cfb91c8f2c0573e149f'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
     validExitCodes = @(0)
