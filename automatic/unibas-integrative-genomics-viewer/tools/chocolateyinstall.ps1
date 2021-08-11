264222f82a38b9c5fc36f82c4dba7d81114e9166
Chocolatey <chocolatey@realdimensions.net>
2021-08-11 16:33:45 +0000

    AU: 1 updated - unibas-integrative-genomics-viewer [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/6e9a97db7e190ee0fe207a7662f6b9afe1ae569f

--- automatic/unibas-integrative-genomics-viewer/tools/chocolateyinstall.ps1
+++ automatic/unibas-integrative-genomics-viewer/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = ''
+  url            = 'https://data.broadinstitute.org/igv/projects/downloads/2.10/IGV_Win_2.10.2-installer.exe'
   silentArgs     = "/S"
   softwareName   = 'Integrative Genomics Viewer*'
-  checksum       = ''
+  checksum       = 'f7a52b8e8aec1f6a874d6d8974af5ceb36f9e5628e99a32e8fdacfd2f0e43662'
   checksumType   = 'sha256'
 }
 
