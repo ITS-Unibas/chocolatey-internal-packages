2284610002a8b36abce4b513291129cffe75cd63
Uwe Molnar <uwe.molnar@unibas.ch>
2021-08-11 16:21:19 +0200

    New SW IGV

---
+++ automatic/unibas-integrative-genomics-viewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'unibas-integrative-genomics-viewer'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = ''
+  silentArgs     = "/S"
+  softwareName   = 'Integrative Genomics Viewer*'
+  checksum       = ''
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
