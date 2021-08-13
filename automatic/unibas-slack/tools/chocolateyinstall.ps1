2fd31245aa421cacec6238e232532452963dbbd7
uwe_molnar <76432776+uwemolnar@users.noreply.github.com>
2021-08-13 14:26:42 +0200

    Update chocolateyinstall.ps1

--- automatic/unibas-slack/tools/chocolateyinstall.ps1
+++ automatic/unibas-slack/tools/chocolateyinstall.ps1
@@ -2,9 +2,6 @@
 
 $packageName = 'unibas-slack'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://downloads.slack-edge.com/releases/windows/4.19.0/prod/ia32/slack-standalone-4.19.0.0.msi'
-$checksum32     = ''
-$checksumType32 = ''
 $url64          = 'https://downloads.slack-edge.com/releases/windows/4.19.0/prod/x64/slack-standalone-4.19.0.0.msi'
 $checksum64     = ''
 $checksumType64 = ''
@@ -13,9 +10,6 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'msi'
-  url           = $url32
-  checksum      = $checksum32
-  checksumType  = $checksumType32
   url64bit      = $url64
   checksum64    = $checksum64
   checksumType64= $checksumType64
