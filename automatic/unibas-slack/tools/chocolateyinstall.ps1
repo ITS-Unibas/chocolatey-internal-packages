1fd9bbaf61ffe08ae6dfe887dea0b79e1099d40d
Maximilian Burgert <max.burgert@unibas.ch>
2021-08-17 15:10:02 +0200

    Reset

--- automatic/unibas-slack/tools/chocolateyinstall.ps1
+++ automatic/unibas-slack/tools/chocolateyinstall.ps1
@@ -1,21 +1,26 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'unibas-slack'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://downloads.slack-edge.com/releases/windows/4.19.0/prod/x64/slack-standalone-4.19.0.0.msi'
-$checksum64     = ''
-$checksumType64 = ''
+$url32 = 'https://downloads.slack-edge.com/releases/windows/4.19.0/prod/ia32/slack-standalone-4.19.0.0.msi'
+$checksum32 = 'FFFA5269DF5CC5C3C7B8C16F0CF2CC07DD9A471A8AE4CD93372821F6306FB349'
+$checksumType32 = 'sha256'
+$url64 = 'https://downloads.slack-edge.com/releases/windows/4.19.0/prod/x64/slack-standalone-4.19.0.0.msi'
+$checksum64 = 'DC3D22E4A698D99608D64B1EE4B3F24CF270937149580FFC2DE6DAC5E6C42E61'
+$checksumType64 = 'sha256'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'msi'
-  url64bit      = $url64
-  checksum64    = $checksum64
-  checksumType64= $checksumType64
-  softwareName  = 'unibas-slack*'
-  silentArgs    = "/qn /norestart"
-  validExitCodes= @(0)
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'msi'
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = $checksumType32
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = $checksumType64
+  softwareName   = 'Slack*'
+  silentArgs     = "/qn /norestart"
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
--- automatic/unibas-slack/update.ps1
+++ automatic/unibas-slack/update.ps1
@@ -1,14 +1,15 @@
-Import-Module AU
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Import-Module au
 
 $releases = 'https://slack.com/intl/en-nl/downloads/windows'
 
 function global:au_SearchReplace {
-  @{
+    @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
             "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
     }
@@ -17,6 +18,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
+    $url32 = Get-RedirectedUrl 'https://slack.com/ssb/download-win-msi'
     $url64 = Get-RedirectedUrl 'https://slack.com/ssb/download-win64-msi'
 
     $re = "Version (.+\d)</span>"
@@ -24,9 +26,10 @@ function global:au_GetLatest {
     $version = ([regex]::Match($download_page.RawContent, $re)).Captures.Groups[1].value
 
     return @{
-        URL64 = $url64
+        URL32   = $url32
+        URL64   = $url64
         Version = $version
     }
 }
 
-update -NoCheckChocoVersion
+update
