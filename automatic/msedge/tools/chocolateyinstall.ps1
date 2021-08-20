f5cd2682324326ee5bf38659f96948f56018a0d3
Chocolatey <chocolatey@realdimensions.net>
2021-08-20 00:23:49 +0000

    AU: 1 updated - msedge [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/627a9597be9053e0047905e59b580226187a83c6

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/354a3197-be46-4cad-b0cf-49b608c57a5b/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/017f6599-46bb-4178-98cf-8d841a0fb014/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = '6E8C8D6B8B48142CA743C29B2B62C04A59F92CEC138C02B2066EC96EE4AD656D'
+  checksum       = '246D6FFDD873AFF96392FEA520EB0F9AE47FF5C7875B63FE0E87627230517C8B'
   checksumType   = 'sha256'
 }
 
