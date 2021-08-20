b6c9429e5a6ccbbb4bdc75823d1093aed357d905
Chocolatey <chocolatey@realdimensions.net>
2021-08-20 12:23:18 +0000

    AU: 1 updated - zoom [skip ci] https://gist.github.com/96b4efbc9647eab3f4ae8eee2e90207e/710fe718bb8d72de48e115c452a541433e92dcbd

--- automatic/zoom/tools/chocolateyinstall.ps1
+++ automatic/zoom/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
-$checksum = '9d3f0f1f7821fb07ea753a83a1dac240e248e1abc622f8218f710d8ba5f0068e'
-$checksum64 = 'aec26bfbe031bc4129e45ba950e9d540b987d14e81efce0a8f3372d2eb07e7c5'
+$checksum = 'a5156e3d5b14fd449654a64cda2b02a38f5bacdfaa22e06fe1c1a248fbe78faa'
+$checksum64 = '8b0bf9fc13934bfabcd30ba0f1d6005988084c8312f7c25bdf5bceb903cb5c56'
 
-$url = 'https://cdn.zoom.us/prod/5.7.5.1020/ZoomInstallerFull.msi'
-$url64 = 'https://cdn.zoom.us/prod/5.7.5.1020/x64/ZoomInstallerFull.msi'
+$url = 'https://cdn.zoom.us/prod/5.7.6.1055/ZoomInstallerFull.msi'
+$url64 = 'https://cdn.zoom.us/prod/5.7.6.1055/x64/ZoomInstallerFull.msi'
 
 $packageArgs = @{
   packageName    = 'zoom'
