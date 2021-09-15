bf52ac9ec2f7a11928b00ce9b85a64caa1a096b5
ITS-Unibas <ITS-Unibas+github@gmail.com>
2021-09-15 14:05:32 +0000

    AU: 1 updated - unibas-eclipse [skip ci]

--- automatic/unibas-eclipse/tools/chocolateyinstall.ps1
+++ automatic/unibas-eclipse/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2021-06/R/eclipse-jee-2021-06-R-win32-x86_64.zip&r=1'
+  url64bit       = 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2021-09/R/eclipse-jee-2021-09-R-win32-x86_64.zip&r=1'
   #OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
   #silentArgs   = '/S'           # NSIS
@@ -25,7 +25,7 @@ $packageArgs = @{
 
   # optional, highly recommended
   softwareName   = 'Eclipse*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum64     = 'b0c490cc5331c21cafaa4d921df941fcc3a44f0a364d2231c9a7924797cb7a75'
+  checksum64     = 'e44376f49ce2cfe9fff18cb4fb80310577aa1424f64a9725b1382989a030809f'
   checksumType64 = 'sha256' #default is checksumType
 }
 
