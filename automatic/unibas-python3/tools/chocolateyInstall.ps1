6c48b33b6d660c3d0a55f1572e640547260ef48a
ITS-Unibas <ITS-Unibas+github@gmail.com>
2021-10-05 05:00:47 +0000

    AU: 2 updated - unibas-obs-studio unibas-python3 [skip ci]

--- automatic/unibas-python3/tools/chocolateyInstall.ps1
+++ automatic/unibas-python3/tools/chocolateyInstall.ps1
@@ -6,9 +6,9 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url     = 'https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe'
+  url     = 'https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe'
 
-  checksum    = '3f63f4c77a6da13f9bae715eb6644a48bd5f900601e0f1f233862e8ca1a96dd8'
+  checksum    = 'cb580eb7dc55f9198e650f016645023e8b2224cf7d033857d12880b46c5c94ef'
   checksumType= 'SHA256'
 
   silentArgs    = "/quiet"
