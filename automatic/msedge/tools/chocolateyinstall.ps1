190a0ebf66e2490a2e288df4dbcfff577901d971
ITS-Unibas <ITS-Unibas+github@gmail.com>
2021-10-12 04:36:23 +0000

    AU: 1 updated - msedge [skip ci]

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/eb583337-beb3-4772-a587-ec9c055bb398/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/de8e5069-b343-4a53-9b65-e331ea357853/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = '48229F430BD28D630B18E5D8D9C6651FBD97A9B64995C004EB3C719DC66A5D69'
+  checksum       = 'E4EFFD753C19C34170F99723F3F3E8C5C86B49358D267921E3E8747A289D12A9'
   checksumType   = 'sha256'
 }
 
