054f34f0f6c160cbf2a7e08a84e51e3c1b4d082d
ITS-Unibas <ITS-Unibas+github@gmail.com>
2021-10-15 04:52:41 +0000

    AU: 2 updated - msedge powershell [skip ci]

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/de8e5069-b343-4a53-9b65-e331ea357853/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/d50edaf7-3482-4add-80f3-4c208b971c0e/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = 'E4EFFD753C19C34170F99723F3F3E8C5C86B49358D267921E3E8747A289D12A9'
+  checksum       = 'E3960F2DEBCE3BA0D7221710FE27972A84B5E30F2B9AA789BB5DD83D06AB67AB'
   checksumType   = 'sha256'
 }
 
--- automatic/powershell/tools/chocolateyinstall.ps1
+++ automatic/powershell/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'powershell'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/PowerShell-7.1.4-win-x64.msi'
+  url            = 'https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/PowerShell-7.1.5-win-x64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'PowerShell*'
-  checksum       = '9190F005ADCC59F1D2CFF21B8D4FBBA70D72B8B4B567D845B33508A9C388A7A2'
+  checksum       = '17924469091452658680BFBC582BBE5CADE52242F035880CC29D2A763D16D011'
   checksumType   = 'sha256'
 }
 
