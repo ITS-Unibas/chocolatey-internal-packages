661c16f72c58e048d4026f88308d532cc14f350c
uwe_molnar <76432776+uwemolnar@users.noreply.github.com>
2021-10-22 16:50:22 +0200

    Update chocolateyinstall.ps1

--- automatic/unibas-lenovo-thin-installer/tools/chocolateyinstall.ps1
+++ automatic/unibas-lenovo-thin-installer/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
   url            = 'https://download.lenovo.com/pccbbs/thinkvantage_en/lenovo_thininstaller_1.3.0031.exe'
-  silentArgs     = "/VERYSILENT /NORESTART /LOG='C:\ProgramData\LANDESK\Log\Install_ThinInstaller.log'" 
+  silentArgs     = '/VERYSILENT /NORESTART /LOG="C:\ProgramData\LANDESK\Log\Install_ThinInstaller.log"'
   softwareName   = 'unibas-lenovo-thin-installer*'
   checksum       = '26ef9d03f8fe5b5969f88478fdd454ffbcb50ad1a614b54c921ca0183463afd8'
   checksumType   = 'sha256'
