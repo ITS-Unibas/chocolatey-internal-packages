836ed6aaed574b07c2c408a2bf1de9bcaae9d557
Uwe Molnar <uwe.molnar@unibas.ch>
2021-10-22 14:08:10 +0200

    new software lenovo-thininstaller

---
+++ automatic/unibas-lenovo-thin-installer/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'unibas-lenovo-thin-installer'
+
+$extractionPath = "C:\Program Files (x86)\Lenovo\ThinInstaller\"
+
+if (Test-Path $extractionPath){
+	Remove-Item -Path $extractionPath -Recurse -Force
+} else {
+	Write-Host "Lenovo-ThinInstaller NOT found under: $extractionPath. Software seems to be removed by other means!" -ForegroundColor Cyan
+}
