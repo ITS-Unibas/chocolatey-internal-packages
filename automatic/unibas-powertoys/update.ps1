c81b27ce120c830fb1aea3e37465cbd0e8d283c5
russi0001 <nikita.russi@unibas.ch>
2021-11-05 08:23:52 +0100

    Anpassungen nuspec und update.ps1

--- automatic/unibas-powertoys/update.ps1
+++ automatic/unibas-powertoys/update.ps1
@@ -10,12 +10,12 @@ function global:au_GetLatest {
 
     $re  = "PowerToysSetup(.+)?.exe"
     $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
-
+    $arr = $url -split '-|.exe'
     $version = (($download_page.Links | Where-Object href -Match "releases/tag" | Select-Object -First 1 -ExpandProperty href) -Split "/" | Select-Object -Last 1) -Replace "v"
     $url = 'https://github.com' + $url
 
     return @{
-        URL64 = $url
+        URL = $url
         Version = $version
         RemoteVersion  = $version
         FileType = 'exe'
@@ -30,7 +30,7 @@ function global:au_SearchReplace {
     }
     ".\legal\VERIFICATION.txt" = @{
       "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL64)>"
+      "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL)>"
       "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType64)"
       "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
     }
