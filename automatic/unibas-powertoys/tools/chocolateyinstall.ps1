72a75859ec8298a91a91c3e7714f09667cfa4fb5
russi0001 <nikita.russi@unibas.ch>
2021-11-05 08:47:32 +0100

    Install

--- automatic/unibas-powertoys/tools/chocolateyinstall.ps1
+++ automatic/unibas-powertoys/tools/chocolateyinstall.ps1
@@ -12,17 +12,17 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
-  fileType       = 'EXE'
+  fileType       = 'exe'
   url            = ''
   url64bit       = ''
   #file         = $fileLocation
 
   #MSI
  #silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes = @(0)
+  validExitCodes = @(0,1641,3010)
   #OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
-  silentArgs   = '--silent'           # NSIS
+  silentArgs   =  "--silent --skip_dotnet_install"         # NSIS
   #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   #silentArgs   = '/s'           # InstallShield
   #silentArgs   = '/s /v"/qn"' # InstallShield with MSI
