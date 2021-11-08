6c6b1f656910da1c6bedb1b823e8c7adb5062413
ITS-Unibas <ITS-Unibas+github@gmail.com>
2021-11-08 17:01:51 +0000

    AU: 1 updated - powershell [skip ci]

--- automatic/powershell/tools/chocolateyinstall.ps1
+++ automatic/powershell/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'powershell'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/PowerShell-7.1.5-win-x64.msi'
+  url            = 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/PowerShell-7.2.0-win-x64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'PowerShell*'
-  checksum       = '17924469091452658680BFBC582BBE5CADE52242F035880CC29D2A763D16D011'
+  checksum       = 'BD5BDDF2FAC4626479C4F381BDB30A0DC6A9516BE2DE9FC726F33C65F56FE9D4'
   checksumType   = 'sha256'
 }
 
