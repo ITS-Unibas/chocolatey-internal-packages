63449d6d7acd297547cda0946a2e1c376b63a116
ITS-Unibas <ITS-Unibas+github@gmail.com>
2021-11-08 05:45:03 +0000

    AU: 1 updated - unibas-lenovo-thin-installer [skip ci]

--- automatic/unibas-lenovo-thin-installer/tools/chocolateyinstall.ps1
+++ automatic/unibas-lenovo-thin-installer/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://download.lenovo.com/pccbbs/thinkvantage_en/lenovo_thininstaller_1.3.0031.exe'
+  url            = 'https://download.lenovo.com/pccbbs/thinkvantage_en/lenovo_thininstaller_1.3.0038.exe'
   silentArgs     = '/VERYSILENT /NORESTART /LOG="C:\ProgramData\LANDESK\Log\Install_ThinInstaller.log"'
   softwareName   = 'unibas-lenovo-thin-installer*'
-  checksum       = '26ef9d03f8fe5b5969f88478fdd454ffbcb50ad1a614b54c921ca0183463afd8'
+  checksum       = '950400a56f9083feb46e2bcf51c2bb0eadfb7d8dbb8122c39fa3aea29e191e65'
   checksumType   = 'sha256'
 }
 
