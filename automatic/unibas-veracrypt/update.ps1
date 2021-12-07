361d1a2463d2bc7386a6f0fda546b1d9922bcc66
Uwe Molnar - molnar0002-adm <molnar0002-adm@unibas.ch>
2021-12-07 16:56:26 +0100

    made veracryp new (msi)

--- automatic/unibas-veracrypt/update.ps1
+++ automatic/unibas-veracrypt/update.ps1
@@ -1,12 +1,8 @@
 Import-Module AU
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 $releases = 'https://www.veracrypt.fr/en/Downloads.html'
 
-function global:au_BeforeUpdate() {
-  Get-RemoteFiles -Purge -FileNameBase 'unibas-veracrypt'
-  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'sha256'
-}
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
@@ -17,12 +13,12 @@ function global:au_SearchReplace {
 }
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex = '.exe$'
+  $regex = '.msi$'
   $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
-  $arr = $url -split '%20'
-  $version = $arr[2].replace('-Update','.')
-  $version = $version.replace('.exe','')
-  $url = $url.replace('&#43;','+')
+  
+  $arr = $url -split '/'
+  $version = $arr[5]
+ 
   return @{ Version = $version; URL = $url }
 }
 
