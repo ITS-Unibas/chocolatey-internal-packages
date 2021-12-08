7181b42ab89bb995b417c7ee734550bb6facb98f
ITS-Unibas <ITS-Unibas+github@gmail.com>
2021-12-08 08:58:15 +0000

    AU: 1 updated - unibas-veracrypt [skip ci]

--- automatic/unibas-veracrypt/tools/chocolateyinstall.ps1
+++ automatic/unibas-veracrypt/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName    = 'unibas-veracrypt'
   fileType       = 'msi'
-  url            = ''
+  url            = 'https://launchpad.net/veracrypt/trunk/1.25.4/+download/VeraCrypt_Setup_x64_1.25.4.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'VeraCrypt*'
