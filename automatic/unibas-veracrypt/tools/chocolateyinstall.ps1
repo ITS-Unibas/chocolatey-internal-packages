539edc4ce48614eaf0b6d05614b093b7f7ce44ff
uwe_molnar <76432776+uwemolnar@users.noreply.github.com>
2021-12-08 10:21:54 +0100

    Update chocolateyinstall.ps1
    adds install-parameters for silent install

--- automatic/unibas-veracrypt/tools/chocolateyinstall.ps1
+++ automatic/unibas-veracrypt/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = 'unibas-veracrypt'
   fileType       = 'msi'
   url            = 'https://launchpad.net/veracrypt/trunk/1.25.4/+download/VeraCrypt_Setup_x64_1.25.4.msi'
-  silentArgs     = '/quiet /norestart'
+  silentArgs     = '/qb /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ACCEPTLICENSE=YES'
   validExitCodes = @(0)
   softwareName   = 'VeraCrypt*'
   checksum       = ''
