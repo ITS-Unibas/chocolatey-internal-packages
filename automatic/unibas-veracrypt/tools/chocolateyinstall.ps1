bc483e8d381f757cf30df22c16d3c6b13c285b60
uwe_molnar <76432776+uwemolnar@users.noreply.github.com>
2021-12-08 09:26:38 +0100

    Update chocolateyinstall.ps1

--- automatic/unibas-veracrypt/tools/chocolateyinstall.ps1
+++ automatic/unibas-veracrypt/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'unibas-veracrypt'
   fileType       = 'msi'
-  url            = 'https://launchpad.net/veracrypt/trunk/1.25.4/+download/VeraCrypt_Setup_1.25.4.exe'
+  url            = ''
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'VeraCrypt*'
-  checksum       = '94efbf75cbd4b526f4030b746ae3d4bbb8741b6629603568478fd10e6930a3a5'
+  checksum       = ''
   checksumType   = 'sha256'
 }
 
