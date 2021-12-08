f02c9d110f57d1599ac19e200ce7b96195393a19
ITS-Unibas <ITS-Unibas+github@gmail.com>
2021-12-08 10:03:47 +0000

    AU: 2 updated - unibas-miktex unibas-veracrypt [skip ci]

--- automatic/unibas-miktex/tools/chocolateyinstall.ps1
+++ automatic/unibas-miktex/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = 'https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-21.8-x64.exe'
+  url64bit       = 'https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-21.12-x64.exe'
   silentArgs     = '--no-registry --shared --unattended --local-package-repository=C:\Temp\MikTex\'
   softwareName   = 'miktex*'
-  checksum64     = '0dfeba2bfcf8492c22661710e0d2c4fa7fb0ebd34f9830048cec03f8fbc9efad'
+  checksum64     = '4fdb55be91eb8f4687fdf3890f36aed1c3f545cf0258d050f2e11fc3a4ba2b56'
   checksumType64 = 'sha256'
 }
 
