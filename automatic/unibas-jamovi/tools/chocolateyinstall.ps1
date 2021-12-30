e3daed5ada5fb4c8753f95dd1384d3e3b5b08e0d
ITS-Unibas <ITS-Unibas+github@gmail.com>
2021-12-30 05:00:39 +0000

    AU: 1 updated - unibas-jamovi [skip ci]

--- automatic/unibas-jamovi/tools/chocolateyinstall.ps1
+++ automatic/unibas-jamovi/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = 'https://jamovi.org//downloads/jamovi-1.6.23.0-win64.exe'
+  url64bit       = 'https://jamovi.org//downloads/jamovi-2.2.5.0-win64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'unibas-jamovi*'
-  checksum64     = '59eda2aec3ef929aca5a28d164d64a2487a707533f0609cb7a0f7705b1652e85'
+  checksum64     = '4ef1b091f41d8f63803b74717017eec3431cc845abab62a4f23c19a0d41307a7'
   checksumType64 = 'sha256' #default is checksumType
 }
 
