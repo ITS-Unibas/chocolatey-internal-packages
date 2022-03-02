34dd086b6e43e42a15b6d11ce61aca1828ef253e
uwe_molnar <76432776+uwemolnar@users.noreply.github.com>
2022-03-02 16:38:04 +0100

    version not needed here

--- automatic/unibas-powertoys/tools/chocolateyinstall.ps1
+++ automatic/unibas-powertoys/tools/chocolateyinstall.ps1
@@ -14,7 +14,6 @@ $packageArgs = @{
   unzipLocation  = $toolsDir
   fileType       = 'exe'
   url            = 'https://github.com/microsoft/PowerToys/releases/download/v0.49.1/PowerToysSetup-0.49.1-x64.exe'
-  version        =
   #file         = $fileLocation
 
   #MSI
