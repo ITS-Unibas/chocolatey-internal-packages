e3cbe5b2d44ea121fc4e0508037c24d635a63a87
ITS-Unibas <ITS-Unibas+github@gmail.com>
2022-03-23 17:14:06 +0000

    AU: 1 updated - rstudio-automatic [skip ci]

--- automatic/rstudio-automatic/tools/chocolateyinstall.ps1
+++ automatic/rstudio-automatic/tools/chocolateyinstall.ps1
@@ -7,12 +7,12 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://download1.rstudio.org/desktop/windows/RStudio-2022.02.0-443.exe'
+  url           = 'https://download1.rstudio.org/desktop/windows/RStudio-2022.02.1-461.exe'
   silentArgs   = '/S'
   validExitCodes= @(0)
  
   softwareName  = 'rstudio*'
-  checksum      = 'e308f15d8852df5c8ad4c261660b74b0'
+  checksum      = '591dfa70c5679e35da394e3344367343'
   checksumType  = 'md5'
 }
 
