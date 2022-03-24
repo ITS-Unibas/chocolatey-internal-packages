d6950cd4e7beaa06e5e65eb40080581e22ca2c56
uwe_molnar <76432776+uwemolnar@users.noreply.github.com>
2022-03-24 11:25:15 +0100

    Update chocolateyinstall.ps1

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/d0dc21e6-4d66-42ba-bef3-0f24d9afc835/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/83788acd-5a05-4a45-975b-b1338831a73c/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = '0CFEC4C2B6262A5AE0814428C2B034C6A4B049897D2A6EA13D49C18F62D36B0D'
+  checksum       = '123538efe88960620a539558bf304b0b4169310484862a0e50b45aaac22ea1df'
   checksumType   = 'sha256'
 }
 
