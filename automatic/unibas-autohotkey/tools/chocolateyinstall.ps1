1949dc05a1170fe32bdc2dc1bf70920084813d81
ITS-Unibas <ITS-Unibas+github@gmail.com>
2022-05-08 04:57:32 +0000

    AU: 1 updated - unibas-autohotkey [skip ci]

--- automatic/unibas-autohotkey/tools/chocolateyinstall.ps1
+++ automatic/unibas-autohotkey/tools/chocolateyinstall.ps1
@@ -6,10 +6,10 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'unibas-autohotkey'
   fileType       = 'exe'
-  url            = 'https://github.com/Lexikos/AutoHotkey_L/releases/download/v1.1.34.00/AutoHotkey_1.1.34.00_setup.exe'
+  url            = 'https://github.com/Lexikos/AutoHotkey_L/releases/download/v1.1.34.01/AutoHotkey_1.1.34.01_setup.exe'
   silentArgs     = "/S /D=C:\Program Files\AutoHotkey"
   softwareName   = 'AutoHotkey*'
-  checksum       = '25ce0fa078c6603a909bb391c1cb4eb891554b29ad275beea47042962576f4ff'
+  checksum       = 'f33996eaa68e8a7a5f5a6156b44af666049769bd021979a6ffb9abb29b45ea2b'
   checksumType   = 'sha256' #default is md5, can also be sha1
   validExitCodes = @(0, 1223)
 }
