47e174cb46a2ab051d9bb5be051b7d7613e93c47
ITS-Unibas <ITS-Unibas+github@gmail.com>
2022-05-28 05:09:00 +0000

    AU: 1 updated - unibas-freecad [skip ci]

--- automatic/unibas-freecad/tools/chocolateyinstall.ps1
+++ automatic/unibas-freecad/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://github.com/FreeCAD/FreeCAD/releases/download/0.19.2/FreeCAD-0.19.2.7b5e18a-WIN-x64-installer1.exe'
+  url            = 'https://github.com/FreeCAD/FreeCAD/releases/download/0.20beta1/FreeCAD-0.20.0beta-WIN-x64-installer-4.exe'
   silentArgs     = '/S'
   softwareName   = 'FreeCAD*'
-  checksum       = '5ed4d2609f5010a8f18af40f29b127ab5e0923dd5aa4b022f8ce35bbcfb54f44'
+  checksum       = 'ca8c72fae8e5fc1addf801ca5a3c424cace308ca3843c9d408a1e0577fb7fa2d'
   checksumType   = 'sha256'
   validExitCodes = @(0) 
 }
