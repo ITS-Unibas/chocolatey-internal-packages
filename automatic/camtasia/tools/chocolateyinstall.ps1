f574e21fc1c4e15730686ecb22a06403fefc7b2c
ITS-Unibas <ITS-Unibas+github@gmail.com>
2022-09-28 13:29:36 +0000

    AU: 1 updated - camtasia [skip ci]

--- automatic/camtasia/tools/chocolateyinstall.ps1
+++ automatic/camtasia/tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Old Camtasia versions can be found at https://www.techsmith.com/download/oldversions
-$url64      = 'https://download.techsmith.com/camtasiastudio/releases/2211/camtasia.msi'
+$url64      = 'https://download.techsmith.com/camtasiastudio/releases/2220/camtasia.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
   url64bit      = $url64
 
-  checksum64    = '84e99ccbaec49e13fc47c6f99d139d106ba7ef0ca0139aff284e16a970a21c71'
+  checksum64    = '3222c333883a86fed917cb99d8a680d82a3f8fbff7487711883fbe9382f40887'
   checksumType64= 'SHA256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
