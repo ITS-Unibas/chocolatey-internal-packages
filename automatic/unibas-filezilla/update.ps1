5c7d926c979e8ebd9e65b81e1574e7c78e3cabd8
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2022-11-02 15:31:22 +0100

    Update update.ps1

--- automatic/unibas-filezilla/update.ps1
+++ automatic/unibas-filezilla/update.ps1
@@ -1,31 +1,31 @@
-﻿import-module AU
+import-module AU
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 $releases = 'https://filezilla-project.org/download.php?show_all=1'
 
-
-
 function global:au_BeforeUpdate() {
   Get-RemoteFiles -Purge -FileNameBase 'unibas-filezilla' #name for override 
-  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'sha256'
 }
 
 function global:au_SearchReplace {
-  @{
+  return @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL64)`'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum64)`'"
     }
   }
 }
 
-
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex = "win64\-setup\.exe"
-  $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
-  $version = $url -split "_" | ? { $_ -match '^\d+\.[\d\.]+$' } | select -first 1
-  return @{ Version = $version; URL = $url }
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing -UserAgent "Chocolatey"
+  $url64 = $download_page.Links | ? href -match "win64\-setup\.exe" | select -first 1 -expand href
+  $version = Get-Version $url64
+
+  @{
+      Version  = $version
+      URL64    = $url64
+      FileType = "exe"
+  }
 }
 
 update -ChecksumFor none -NoCheckChocoVersion
