c2fa65cc0c71a7a413cca8bbc31637fa1c091641
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2022-11-02 12:21:48 +0100

    Update update.ps1

--- automatic/wireshark/update.ps1
+++ automatic/wireshark/update.ps1
@@ -1,47 +1,37 @@
+$ErrorActionPreference = 'Stop'
 Import-Module au
 
-$version_page = 'https://www.wireshark.org/#download'
-$releases64 = 'https://www.wireshark.org/download/win64/all-versions/'
-
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
-
-function global:au_GetLatest {
-  $version = GetVersion
-
-  $r64 = '[Ww]ire[Ss]hark-win64-[\d\.]+\.exe$'
-  $url64 = GetDownloadLink -downloadlinks $releases64 -pattern $r64 -version $version
-
-  return @{
-    URL64    = $url64
-    Version  = $version
-    FileType = 'exe'
-  }
-}
+# Thanks to mkevenaar (https://github.com/mkevenaar/chocolatey-packages/blob/master/automatic/wireshark/update.ps1)
+$version_page = 'https://www.wireshark.org/download.html'
+# $releases64 = 'https://www.wireshark.org/download/win64/all-versions/'
 
 function global:au_SearchReplace {
   return @{
     ".\tools\chocolateyInstall.ps1" = @{
       "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName64)`""
     }
-    ".\legal\VERIFICATION.txt"      = @{
-      "(?i)(listed on\s*)\<.*\>" = "`${1}<$version_page>"
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$($version_page)>"
       "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL64)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType64)"
       "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
     }
   }
 }
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
-function GetVersion {
-  $version_page = Invoke-WebRequest -Uri $version_page -UseBasicParsing
-  $re64 = '(http[s]?|[s]?)(:\/\/)([^\s,]+)\/win64\/[Ww]ire[Ss]hark-win64-[\d\.]+\.exe$'
-  $v = $version_page.links | Where-Object href -match $re64 | Select-Object -First 1 -expand href
-  $v -split '-|.exe' | Select-Object -Last 1 -Skip 1
-}
+function global:au_GetLatest {
+  $version_page = Invoke-WebRequest -Uri $version_page
 
-function GetDownloadLink([string] $downloadlinks, [string] $pattern, [string] $version) {
-  $page = Invoke-WebRequest -Uri $downloadlinks -UseBasicParsing
-  $link = $page.links | Where-Object { ($_.href -like '*' + $version + '*') -And ($_.href -match $pattern) } | Select-Object -first 1 -expand href
-  $downloadlinks + $link
+  $re64  = '(http[s]?|[s]?)(:\/\/)([^\s,]+)\/win64\/[Ww]ire[Ss]hark-win64-[\d\.]+\.exe$'
+  $url64 = $version_page.links | Where-Object href -match $re64 | Select-Object -First 1 -expand href
+  $version = $url64 -split '-|.exe' | Select-Object -Last 1 -Skip 1
+
+  return @{
+      URL64 = $url64
+      Version = $version
+      FileType = 'exe'
+  }
 }
 
 update -ChecksumFor None -NoCheckChocoVersion
