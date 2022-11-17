a683e0e469acf604ae4ab0a7d915d89bcb4c24d9
ITS-Unibas <ITS-Unibas+github@gmail.com>
2022-11-17 14:46:33 +0000

    updated chocolatey-community-packages

--- automatic/unibas-filezilla/tools/chocolateyinstall.ps1
+++ automatic/unibas-filezilla/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   fileType      = 'EXE'
   silentArgs    = '/S'
   validExitCodes= @(0)
-  url           = ''
-  checksum      = ''
+  url           = 'https://dl1.cdn.filezilla-project.org/client/FileZilla_3.62.1_win64-setup.exe?h=SequiixlAcCtT34s2XiAYA&x=1668699465'
+  checksum      = '95ECA7FD49E2C332121332A54B866216F7FDFA6D5C12BB16ECDFC443D87EE79F'
   checksumType  = 'sha256'
 }
  
