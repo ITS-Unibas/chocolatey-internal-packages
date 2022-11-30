da2e49198d09fa5ee63fb60b2defda2e5cdb0f94
ITS-Unibas <ITS-Unibas+github@gmail.com>
2022-11-30 04:51:56 +0000

    updated chocolatey-community-packages

--- automatic/unibas-vmware-tools/tools/chocolateyinstall.ps1
+++ automatic/unibas-vmware-tools/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'VMWare Tools*'
   fileType       = 'EXE'
-  url            = 'https://packages.vmware.com/tools/releases/latest/windows/x64/VMware-tools-12.1.0-20219665-x86_64.exe'
-  checksum       = '520a0ab010311d09e37ef5a461fb42565f8415792fd8c026aa9264d3359129ce'
+  url            = 'https://packages.vmware.com/tools/releases/latest/windows/x64/VMware-tools-12.1.5-20735119-x86_64.exe'
+  checksum       = '2c1d6ef7ae17e57a5ae48d38951b1fe2f2f1eb7c7cdd05e3ab785c78b562e958'
   checksumType   = 'sha256'
   silentArgs     = "/S /v `"/qn /l*v `"$env:SWP\Install_vmware-tools_$env:ChocolateyPackageVersion.log`" REBOOT=R`""
   validExitCodes = @(0, 3010)
