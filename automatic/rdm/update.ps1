1839d56f4938b49547a5361a808d4c8859744494
nirus <89071803+russi0001@users.noreply.github.com>
2023-01-17 07:40:01 +0100

    Update update.ps1

--- automatic/rdm/update.ps1
+++ automatic/rdm/update.ps1
@@ -1,34 +1,83 @@
 
-
 #import-module au
 
-Import-Module AU
+Import-Module au
 
-$releases = 'https://devolutions.net/remote-desktop-manager/home/previousversions'
+$releases = 'https://devolutions.net/remote-desktop-manager/home/download'
+$softwareName= "Remote Desktop Manager"
 
 function global:au_BeforeUpdate() {
-    Invoke-WebRequest -UseBasicParsing -Uri $Latest.URL -OutFile "rdm.exe" -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox
-    $Latest.Checksum = Get-FileHash "$PSScriptRoot\rdm.exe" -Algorithm 'sha256' | Select-Object -ExpandProperty Hash
+    $Latest.Checksum = Get-Checksum -version $Latest.Version -checksumType $Latest.ChecksumType
 }
+
 
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
+           "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
             "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
             "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
         }
+   
+     ".\tools\chocolateyUninstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
     }
+     }
 }
 
+Function GetVersion($InputString) {
+    $regExResult = $InputString | Select-String -Pattern 'Version 202\d.\d.\d\d.\d' 
+    $regExResult.Matches[0].Value
+}
+
+#import-module au
+
+Import-Module au
+
+$releases = 'https://devolutions.net/remote-desktop-manager/home/download'
+$softwareName= "Remote Desktop Manager"
+
+function global:au_BeforeUpdate() {
+    $Latest.Checksum = Get-Checksum -version $Latest.Version -checksumType $Latest.ChecksumType
+}
+
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+           "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+        }
+   
+     ".\tools\chocolateyUninstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+    }
+     }
+}
+
+Function GetVersion($InputString) {
+    $regExResult = $InputString | Select-String -Pattern 'Version 202\d.\d.\d\d.\d' 
+    $regExResult.Matches[0].Value
+}
+
+
+
+
+
+
 function global:au_GetLatest {
-    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-    $regex = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.\d\d\d\d.\d.\d\d.\d.exe'
-    $url = ($page.links | Where-Object href -Match $regex | Select-Object -First 1).href
-
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    #get Versionnumber
+    $urltext = $download_page.ToString()
+    $version = GetVersion -InputString $urltext
+    $version_number = $version -replace "Version "
+    $url = "https://cdn.devolutions.net/download/Setup.RemoteDesktopManager."
+    $url+=$version_number
+    $url+=".exe"
     return @{
         URL   = $url
-        Version = $matches.version
+        Version = $version_number
     }
 }
 
