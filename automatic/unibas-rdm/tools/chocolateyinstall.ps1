6721b207cca9a7873ff9ad73c370d92abefe1440
nirus <89071803+russi0001@users.noreply.github.com>
2023-01-17 07:43:15 +0100

    Update chocolateyinstall.ps1

--- automatic/unibas-rdm/tools/chocolateyinstall.ps1
+++ automatic/unibas-rdm/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
   
 $packageArgs = @{
-  packageName   = 'unibas-rdm'
+  packageName   = 'rdm'
   softwareName  = 'remote desktop manager*'
   fileType      = 'EXE'
   silentArgs    = '/S'
   validExitCodes= @(0)
-  url           = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.2022.3.28.0.exe'
-  checksum      = '88ec4f26fe4926f6bc7742fd7723486bfc90a4f376bb6f5937b64662553fcbce'
+  url           = ''
+  checksum      = ''
   checksumType  = 'sha256'
 }
  
