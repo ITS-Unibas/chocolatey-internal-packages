531750856765dbe9742bbfa0e6980c6c3e50ea1b
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-03-03 16:58:19 +0000

    updated chocolatey-community-packages

--- automatic/unibas-cryptomator/tools/chocolateyinstall.ps1
+++ automatic/unibas-cryptomator/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
-  url            = 'https://github.com/cryptomator/cryptomator/releases/download/1.7.0/Cryptomator-1.7.0-x64.exe'
+  url            = 'https://github.com/cryptomator/cryptomator/releases/download/1.7.1/Cryptomator-1.7.1-x64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0) 
   softwareName   = 'Cryptomator'
-  checksum       = 'ec4abd4e50ead7e8d4ac357329457d25163ae6ca1d2bb04796e7f25a198c78e8'
+  checksum       = 'da51534999516fdc3f478a39df8f722791b3b39d5e3901f4e2e4b80c2b788e29'
   checksumType   = 'sha256'
 }
 
