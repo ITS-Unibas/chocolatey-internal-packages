2e401f784e600bbb5b46187080b405d9fd51805d
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-03-16 14:03:07 +0000

    updated chocolatey-community-packages

--- automatic/citavi/tools/chocolateyinstall.ps1
+++ automatic/citavi/tools/chocolateyinstall.ps1
@@ -13,7 +13,7 @@ $packageArgs = @{
 
   # optional, highly recommended
   softwareName   = 'Citavi*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum64     = '43fe08694b1fc40ccd6928b2552517d2c12442bcb431cc90639d5491c9d4865c'
+  checksum64     = 'a269b83ece226908705818bdc3d7e29f864e637280f620a58fd302af1ede7eda'
   checksumType64 = 'sha256' #default is md5, can also be sha1
 }
 
