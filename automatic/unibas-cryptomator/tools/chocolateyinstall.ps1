2b56ee80a6b8f54f4bc2cfadd73338e844e633a7
stokes-unibas <75485474+stokes-unibas@users.noreply.github.com>
2023-03-20 10:51:05 +0100

    Update chocolateyinstall.ps1

--- automatic/unibas-cryptomator/tools/chocolateyinstall.ps1
+++ automatic/unibas-cryptomator/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
 $packageName = 'unibas-cryptomator'
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $packageName
-  fileType       = 'EXE'
-  url            = 'https://github.com/cryptomator/cryptomator/releases/download/1.7.3/Cryptomator-1.7.3-x64.exe'
-  silentArgs     = '/S'
+  fileType       = 'MSI'
+  url            = ''
+  silentArgs     = '/qn /norestart'
   validExitCodes = @(0) 
   softwareName   = 'Cryptomator'
-  checksum       = '3d2f0ffdc83a480b267167ac8fe4b8e022232604f1970a514d2d2cd57a5a894c'
+  checksum       = ''
   checksumType   = 'sha256'
 }
 
