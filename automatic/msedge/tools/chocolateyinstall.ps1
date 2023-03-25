e0c5e52465ecbf170ac2be1f7e42682737140431
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-03-25 04:47:58 +0000

    updated chocolatey-community-packages

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/04705fe5-a0d7-4c0e-9e35-567443bc5d7f/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/7a7cd97d-7926-42cb-a900-b09c911bcc76/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = 'C3777F4FDBFA6207B2B4885C0E5985C8602805D93F526FE1B0DAEDE4ABF8CD25'
+  checksum       = 'B6779C6812BAA14F1862F8A28C8FD4BC7399D54D1526872AA359A564E12D34E4'
   checksumType   = 'sha256'
 }
 
--- automatic/unibas-brightauthor-connected/tools/chocolateyinstall.ps1
+++ automatic/unibas-brightauthor-connected/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://www.brightsignnetwork.com/download/ba-connected-builds/ba-connected-prod/BA connected Setup 1.8.3.exe'
+  url            = 'https://www.brightsignnetwork.com/download/ba-connected-builds/ba-connected-prod/BA connected Setup 1.10.0.exe'
   silentArgs     = "/S"
   softwareName   = 'unibas-brightauthor-connected'
-  checksum       = '973a640ebd6f91a3587e730759fbcbc4de08e1bb9859598e660c3a137cdaf8df'
+  checksum       = '2caefea3ba54d4220081a49e69b7ad76de57d7b05beaa5a7f3cd0dfe43e6b5cc'
   checksumType   = 'sha256' 
 }
 
