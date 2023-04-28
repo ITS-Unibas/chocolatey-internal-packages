3b0ba78065cd698a09627d3baef35daf41f2da18
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-04-28 10:36:45 +0000

    updated chocolatey-community-packages

--- automatic/unibas-audacity/tools/chocolateyinstall.ps1
+++ automatic/unibas-audacity/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://github.com/audacity/audacity/releases/download/Audacity-3.3.0/audacity-win-3.3.0-x64.exe'
+  url            = 'https://github.com/audacity/audacity/releases/download/Audacity-3.3.1/audacity-win-3.3.1-x64.exe'
   #OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
   #silentArgs   = '/S'           # NSIS
@@ -27,7 +27,7 @@ $packageArgs = @{
 
   # optional, highly recommended
   softwareName   = 'Audacity' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = '6d6153ff44859541fdf9cec487a525a4b0215b2f991af415bf623eff36dbd541'
+  checksum       = '296552cff05905339069e3750697a2418e44eb0953ae0602eb2417ac67e9f701'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
