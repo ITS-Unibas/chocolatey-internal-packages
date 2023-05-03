a1e2a86bc56df964e6377c1e0baad5c37d4b536a
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2023-05-03 15:31:44 +0200

    Update chocolateyinstall.ps1

--- automatic/unibas-filezilla/tools/chocolateyinstall.ps1
+++ automatic/unibas-filezilla/tools/chocolateyinstall.ps1
@@ -2,6 +2,7 @@
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
+  softwareName   = 'FileZilla*'
   url            = 'https://dl3.cdn.filezilla-project.org/client/FileZilla_3.64.0_win64-setup.exe?h=hg2sj1COks-ZJg5ysya0cg&x=1683122966'
   checksum       = '0d8f5dbe4a1f05ddacda6493733bed57fc54310c90bc6fdc764fb95a92441b40'
   checksumType   = 'sha256'
