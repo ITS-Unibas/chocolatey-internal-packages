4028685e11b80ce8a05432ed1b2fd6bbb52c672b
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-05-12 15:48:12 +0000

    updated chocolatey-community-packages

--- automatic/rstudio-automatic/tools/chocolateyinstall.ps1
+++ automatic/rstudio-automatic/tools/chocolateyinstall.ps1
@@ -7,12 +7,12 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://download1.rstudio.org/electron/windows/RStudio-2023.03.0-386.exe'
+  url           = 'https://download1.rstudio.org/electron/windows/RStudio-2023.03.1-446.exe'
   silentArgs   = '/S'
   validExitCodes= @(0)
  
   softwareName  = 'rstudio*'
-  checksum      = '6331f8cfa88ce06e60e847facaf90e39'
+  checksum      = '1fa4dc0bb6ebac5d22bb062be97b3a6e'
   checksumType  = 'md5'
 }
 
