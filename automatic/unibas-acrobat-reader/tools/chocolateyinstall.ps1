7ba377bc040b5a2177a8ccbb509913e3b69bab8f
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-07-11 08:47:43 +0000

    updated chocolatey-community-packages

--- automatic/unibas-acrobat-reader/tools/chocolateyinstall.ps1
+++ automatic/unibas-acrobat-reader/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Adobe Acrobat DC Reader*'
   fileType       = 'EXE'
-  url            = 'https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2300320215/AcroRdrDC2300320215_MUI.exe'
-  checksum       = '153ffeb7bc7bf0962cceae7c72cc130c239a59b31cef22bd70d81e7c87af8c42'
+  url            = 'https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2300320244/AcroRdrDC2300320244_MUI.exe'
+  checksum       = '4333bd63b7309d44429d2c6203ca7e3d855f0a062c72889d6418a8e08cb0fdec'
   checksumType   = 'sha256'
   silentArgs     = "/sAll /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES DISABLEDESKTOPSHORTCUT=1 DISABLE_ARM_SERVICE_INSTALL=1 UPDATE_MODE=0 /L*v `"$env:TEMP\$env:chocolateyPackageName.$env:chocolateyPackageVersion.Install.log`""
   validExitCodes = @(0, 1000, 1101)
