5b3c86bf5348b41072e1747962642774ef6b314d
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2023-08-04 11:37:21 +0200

    updated download-page and search-string

--- automatic/unibas-brightauthor-connected/update.ps1
+++ automatic/unibas-brightauthor-connected/update.ps1
@@ -1,7 +1,7 @@
 ﻿Import-Module AU
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$releases = 'https://www.brightsign.biz/digital-signage-products/software/brightauthorconnected'
+$releases = 'https://www.brightsign.biz/resources/software-downloads/'
 
 function global:au_BeforeUpdate() {
   Get-RemoteFiles -Purge -FileNameBase 'unibas-brightauthor-connected'
@@ -18,7 +18,8 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
   $regex = '.exe$'
-  $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $title = '.*connected.*'
+  $url = $download_page.links | Where-Object {$_.title -match $title -and $_.href -match $regex} | Select-Object -First 1 -expand href
   $arr = $url -split '/'
   $arr = $arr[$arr.length-1] # the last item is the name of the installer-exe (e.g. "BA connected Setup 1.6.44.exe")
   $arr = $arr -split ' ' # extracts the version with exe
