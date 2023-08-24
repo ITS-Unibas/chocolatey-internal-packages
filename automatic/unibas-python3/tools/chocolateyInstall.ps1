23e2cb1268e9c9e21b2521349f125cde18493478
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-08-24 17:39:31 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-python3/tools/chocolateyInstall.ps1
+++ automatic/unibas-python3/tools/chocolateyInstall.ps1
@@ -6,9 +6,9 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url     = 'https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe'
+  url     = 'https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe'
 
-  checksum    = '47be821c0f1825d90fc40f83a3ee3d3a691a3e16c8e21ac0cd56371362aaad50'
+  checksum    = '1bb46f65bb6f71b295801c8ff596bb5b69fa4c0645541db5f3d3bac33aa6eade'
   checksumType= 'SHA256'
 
   silentArgs    = "/quiet"
