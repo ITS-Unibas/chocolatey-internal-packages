40a63a9720d287b7536e899e2e76e5db8650587f
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-10-02 04:31:44 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-veracrypt/tools/chocolateyinstall.ps1
+++ automatic/unibas-veracrypt/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'unibas-veracrypt'
   fileType       = 'msi'
-  url            = 'https://launchpad.net/veracrypt/trunk/1.25.9/+download/VeraCrypt_Setup_x64_1.25.9.msi'
+  url            = 'https://launchpad.net/veracrypt/trunk/1.26.7/+download/VeraCrypt_Setup_x64_1.26.7.msi'
   silentArgs     = '/qb /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ACCEPTLICENSE=YES'
   validExitCodes = @(0)
   softwareName   = 'VeraCrypt*'
-  checksum       = '206d31c2ed1a2b0390d4dfa12e74a9b9dae88658d1f9bfa1bb433b23e2b24fd8'
+  checksum       = 'c802dcdda02e9a7a3f52645bf90285d3055c76038dc760131192cc23327ca8f8'
   checksumType   = 'sha256'
 }
 
