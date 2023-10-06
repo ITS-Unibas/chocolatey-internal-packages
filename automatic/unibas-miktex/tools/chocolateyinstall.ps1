70bc819db4cfe3cd73d80e31a5594ee194ba921d
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-10-06 09:26:44 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-miktex/tools/chocolateyinstall.ps1
+++ automatic/unibas-miktex/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = 'https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-23.9-x64.exe'
+  url64bit       = 'https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-23.10-x64.exe'
   silentArgs     = '--no-registry --shared --unattended --local-package-repository=C:\Temp\MikTex\'
   softwareName   = 'miktex*'
-  checksum64     = '37f013b14f2e2e176f4d45058428fffb22bfb44c6c37a7751e2f5b70c02e478a'
+  checksum64     = 'c07f61ab9e21ab107064ea50a3ea6998f921b85c4e1a459bbd95c93e5aaba0ac'
   checksumType64 = 'sha256'
 }
 
