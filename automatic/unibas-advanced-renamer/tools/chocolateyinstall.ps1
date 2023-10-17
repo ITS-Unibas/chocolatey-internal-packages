328012e02c052d0e67be05405d9f0784529dc4a5
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2023-10-17 09:05:43 +0200

    Create chocolateyinstall.ps1

---
+++ automatic/unibas-advanced-renamer/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'unibas-advanced-renamer'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = ''
+  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART"
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'unibas-advanced-renamer*'
+  checksum       = ''
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
