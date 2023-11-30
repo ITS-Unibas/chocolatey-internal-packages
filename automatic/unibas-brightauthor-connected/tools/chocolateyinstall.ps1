4b5d72c897a2c51b23a985d66002766e89980db3
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-11-30 17:29:54 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-brightauthor-connected/tools/chocolateyinstall.ps1
+++ automatic/unibas-brightauthor-connected/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://www.brightsignnetwork.com/download/ba-connected-builds/ba-connected-prod/BA connected Setup 1.19.4.exe'
+  url            = 'https://www.brightsignnetwork.com/download/ba-connected-builds/ba-connected-prod/BA connected Setup 1.26.3.exe'
   silentArgs     = "/S"
   softwareName   = 'unibas-brightauthor-connected'
-  checksum       = '10e35d76f5dc2609c9937d734c4448aea874a1f794165ab42181526a29e01717'
+  checksum       = 'a1245bc1b428dc2ad760ac87a8605c85cc053e0b4345b27f85b7c69036674b24'
   checksumType   = 'sha256' 
 }
 
