1cbf45b905858e5fdfa9de44f22fdc5fe6e56d26
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-12-21 04:30:45 +0000

    updated chocolatey-internal-packages

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/8ec28e1e-d2ae-4d26-b1e6-324aa5318db1/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/5620865e-aa89-4c07-a212-9b30efe0effc/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = '32E2FEA574B96B50DE3DBF1EB817A8643E2A0E07D6DEDE974262BE71E14613E9'
+  checksum       = '8A0F9F3DB94FCE541348A5E34A3714FFA0666D6B01A61572022B4D5E1FAEA403'
   checksumType   = 'sha256'
 }
 
--- automatic/unibas-filezilla/tools/chocolateyinstall.ps1
+++ automatic/unibas-filezilla/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'FileZilla*'
-  url            = 'https://dl2.cdn.filezilla-project.org/client/FileZilla_3.66.1_win64-setup.exe?h=B0T0v3CsSyudMtOheaKnfQ&x=1699018888'
-  checksum       = '72dd2d9247d78fae564056770c53f96acacf6e5129d5666c16cd80c300bf849a'
+  url            = 'https://dl1.cdn.filezilla-project.org/client/FileZilla_3.66.4_win64-setup.exe?h=bF43FjFB-3WTeggf4rIABA&x=1703136487'
+  checksum       = '35ea6fac8fcb1c9826647c345a3152e1e7e8903d81b5df1032d6670c1f564b84'
   checksumType   = 'sha256'
   fileType       = 'EXE'
   silentArgs     = '/S'
