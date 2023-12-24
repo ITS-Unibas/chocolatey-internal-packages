dd0a6c9bb737971bbb4514469abef32021008781
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-12-24 14:27:11 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-jabref/tools/chocolateyinstall.ps1
+++ automatic/unibas-jabref/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url64bit       = 'https://github.com/JabRef/jabref/releases/download/v5.11/JabRef-5.11.msi'
+  url64bit       = 'https://github.com/JabRef/jabref/releases/download/v5.12/JabRef-5.12.msi'
   silentArgs     = "/quiet /qn /norestart"
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'unibas-jabref*'
-  checksum64     = 'd1a179fe8b7137e1ff9ea5ad226fb5cd1da620185c4d7df25782f9fe9ca14305'
+  checksum64     = 'ee783c44839854991919749f801da404b884ffb65a5dadb91a7c9591e4ec030a'
   checksumType64 = 'sha256' #default is checksumType
 }
 
