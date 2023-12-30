c8b627e1888dc243924b8ef56a35d0950f66e839
ITS-Unibas <ITS-Unibas+github@gmail.com>
2023-12-30 14:26:50 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-mobaxterm/tools/chocolateyinstall.ps1
+++ automatic/unibas-mobaxterm/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageName = 'unibas-mobaxterm'
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'MSI'
-  url            = 'https://download.mobatek.net/2352023111832715/MobaXterm_Installer_v23.5.zip'
+  url            = 'https://download.mobatek.net/2362023122033030/MobaXterm_Installer_v23.6.zip'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0)
   softwareName   = 'unibas-mobaxterm*'
-  checksum       = '715f1db3d326bce409893477c71f0c66e9786e25a7d58d3271c3977605f02308'
+  checksum       = 'b1ccdd8a6596cf723acd7c6636387447c735feaf28a480c836fd25df953544d9'
   checksumType   = 'sha256' 
 }
 
