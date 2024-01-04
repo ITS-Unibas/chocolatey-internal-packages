bb43c1de4d29e965339f83b5d608006067a0f22d
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2024-01-04 14:49:26 +0100

    Update chocolateyinstall.ps1

--- automatic/unibas-mobaxterm/tools/chocolateyinstall.ps1
+++ automatic/unibas-mobaxterm/tools/chocolateyinstall.ps1
@@ -1,16 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'unibas-mobaxterm'
-
 $packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'MSI'
+  packageName    = 'unibas-mobaxterm'
   url            = 'https://download.mobatek.net/2362023122033030/MobaXterm_Installer_v23.6.zip'
-  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes = @(0)
+  unzipLocation  = $toolsDir
   softwareName   = 'unibas-mobaxterm*'
   checksum       = '6770bb1538143f530441e9de17583d99e5cb529ae54340f84de1f313f4081927'
   checksumType   = 'sha256' 
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs
