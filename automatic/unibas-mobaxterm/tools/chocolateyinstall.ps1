7383428523ac1045df8f32faaf1ee334e451bfd9
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2024-01-04 13:11:47 +0100

    Update chocolateyinstall.ps1

--- automatic/unibas-mobaxterm/tools/chocolateyinstall.ps1
+++ automatic/unibas-mobaxterm/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0)
   softwareName   = 'unibas-mobaxterm*'
-  checksum       = 'b1ccdd8a6596cf723acd7c6636387447c735feaf28a480c836fd25df953544d9'
+  checksum       = '6770bb1538143f530441e9de17583d99e5cb529ae54340f84de1f313f4081927'
   checksumType   = 'sha256' 
 }
 
