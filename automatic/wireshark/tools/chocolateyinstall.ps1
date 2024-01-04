2ea5ca86cd20d470b0a2874a0c075717a37f2a47
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-01-04 04:30:25 +0000

    updated chocolatey-internal-packages

--- automatic/wireshark/tools/chocolateyinstall.ps1
+++ automatic/wireshark/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file64         = "$toolsDir\Wireshark-win64-4.0.11.exe"
+  file64         = "$toolsDir\Wireshark-win64-4.0.12.exe"
   softwareName   = 'Wireshark*'
   silentArgs     = '/S /quicklaunchicon=no'
   validExitCodes = @(0)
