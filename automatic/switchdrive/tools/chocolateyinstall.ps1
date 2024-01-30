8acb6a7ade15250fc4030cca23a193b7c3b607cb
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-01-30 12:36:47 +0000

    AU: 1 updated - switchdrive [skip ci]

--- automatic/switchdrive/tools/chocolateyinstall.ps1
+++ automatic/switchdrive/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'msi'
-  url            = ''
+  url            = 'https://drive.switch.ch/index.php/s/z3SK7JuXtG0fk5C/download'
   silentArgs     = 'MSIRESTARTMANAGERCONTROL=Disable ALLUSERS=1 REBOOT=ReallySuppress /qn'
   validExitCodes = @(0)
   softwareName   = 'SWITCHdrive*'
-  checksum       = ''
+  checksum       = '0fc3fd850d564fa50ca54a565f194eaa9f591cc56735edc5fe2f72545722c52c'
   checksumType   = 'sha256'
 }
 
