29133bb49b5278401f06586d9c0d02e510590d72
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-01-31 04:38:14 +0000

    AU: 2 updated - rstudio-automatic zoom [skip ci]

--- automatic/rstudio-automatic/tools/chocolateyinstall.ps1
+++ automatic/rstudio-automatic/tools/chocolateyinstall.ps1
@@ -7,12 +7,12 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://download1.rstudio.org/electron/windows/RStudio-2023.12.0-369.exe'
+  url           = 'https://download1.rstudio.org/electron/windows/RStudio-2023.12.1-402.exe'
   silentArgs   = '/S'
   validExitCodes= @(0)
  
   softwareName  = 'rstudio*'
-  checksum      = '3acb7c398845b83edbadad24052ada80'
+  checksum      = 'b7ae97a5f6bcbbecc8bb190c06afcb77'
   checksumType  = 'md5'
 }
 
