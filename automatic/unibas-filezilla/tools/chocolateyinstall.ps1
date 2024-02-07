f9bc1042d58234aade67b3ec9ca2a6775b170eab
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-02-07 11:25:55 +0000

    AU: 1 updated - unibas-filezilla [skip ci]

--- automatic/unibas-filezilla/tools/chocolateyinstall.ps1
+++ automatic/unibas-filezilla/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'FileZilla*'
-  url            = 'https://dl1.cdn.filezilla-project.org/client/FileZilla_3.66.4_win64-setup.exe?h=bF43FjFB-3WTeggf4rIABA&x=1703136487'
-  checksum       = '35ea6fac8fcb1c9826647c345a3152e1e7e8903d81b5df1032d6670c1f564b84'
+  url            = 'https://dl4.cdn.filezilla-project.org/client/FileZilla_3.66.5_win64-setup.exe?h=txy4k3OEnJovU5YZnSyA_Q&x=1707308593'
+  checksum       = 'b5dc1c1a5d7112d19f225d14b0ba7e704994d619e6b2767be341cbeca885d933'
   checksumType   = 'sha256'
   fileType       = 'EXE'
   silentArgs     = '/S'
