f1dbf6e6faf65d7bd94fc4f86b7518a651ecf91f
goodv00 <151019880+goodv00@users.noreply.github.com>
2024-03-07 16:37:35 +0100

    Add files via upload

---
+++ automatic/paraview/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName 	= 'paraview'
+$url            = ''
+$checksum       = ''
+$checksumType   = ''
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'MSI'
+  url            = $url
+  softwareName   = 'paraview*'
+  checksum       = $checksum
+  checksumType   = $checksumType
+  silentArgs    = "/quiet /qn /norestart"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/paraview/update.ps1
@@ -0,0 +1,31 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://www.paraview.org/files/'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL64)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum64)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType64)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$folder = ((Invoke-WebRequest -Uri $releases).Links | Where-Object {$_ -match 'v[0-9].'} | Select-Object -Last 1).href
+	$file = (((Invoke-WebRequest -Uri "$releases$folder" ).Links | Where-Object {$_ -match ".msi"} | Select-Object -Last 1).href).replace('.0&','&')
+	$url = "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=$folder&type=binary&os=Windows&downloadFile=$file"
+	$version=$folder.replace('v','').replace('/','')
+	$checksumType = 'sha256'
+	$checksum = Get-RemoteChecksum($url,$checksumType)
+
+	$Latest = @{ 
+		URL64 = $url; 
+		Version = $version
+	}
+	return $Latest
+}
+
+update -NoCheckChocoVersion
