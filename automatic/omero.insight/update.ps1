5124379dcd512f60ad1aa34b3351928060271813
goodv00 <151019880+goodv00@users.noreply.github.com>
2024-03-07 15:37:34 +0100

    Add files via upload
    Committed omero.insight source for automatic updates

---
+++ automatic/omero.insight/update.ps1
@@ -0,0 +1,26 @@
+import-module au
+$releases = 'https://www.openmicroscopy.org/omero/downloads/'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"     = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url     = $download.links | ? href -match ".msi" | % href
+    $version = $($url -split '/' | select -Last 1 -Skip 1) -split 'v' | select -Last 1
+    
+
+    @{
+        Version      = $version
+        URL          = $url
+    }
+}
+
+update
