b5d0acfd4b94c352c792e50bb5552e8794c54256
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-03-07 13:36:36 +0000

    AU: 1 updated - mnova [skip ci]

--- automatic/mnova/tools/chocolateyinstall.ps1
+++ automatic/mnova/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url64bit       = 'https://mestrelab.com/downloads/mnova/win/msi/MestReNova-15.0.0-34764_x64.msi'
+  url64bit       = 'https://mestrelab.com/downloads/mnova/win/msi/MestReNova-15.0.1-35756_x64.msi'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'mnova*'
-  checksum64     = 'a06d776892ff7f5aede4deedd42dfc6d4b47b7a2125c2e56d7d4898508f4f143'
+  checksum64     = '4d7b188120ac81a47a0b9c5fcaaad43dc80c1f43e4abd23f91a9f5c78ee4fe82'
   checksumType64 = 'sha256'
 }
 
