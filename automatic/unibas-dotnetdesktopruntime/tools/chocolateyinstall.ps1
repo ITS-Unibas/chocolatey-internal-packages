0d2cf4d663b89bc2ad789db6bbfd80dc2930a254
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-03-18 14:33:19 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
+++ automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
@@ -8,13 +8,13 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  url            = ''
+  url            = 'https://download.visualstudio.microsoft.com/download/pr/9e753d68-7701-4ddf-b358-79d64e776945/2a58564c6d0779a7b443a692c520782f/dotnet-sdk-8.0.203-win-x64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
  
   # optional, highly recommended
   softwareName   = 'unibas-dotnetdesktopruntime' 
-  checksum       = ''
+  checksum       = '1d691580c6639dc96a4c8908dfecf9e3ca000201a5cd9e2e56541bbd14b8f5c9'
   checksumType   = 'sha256'
 }
  
