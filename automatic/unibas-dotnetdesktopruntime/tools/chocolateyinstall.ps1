cd66596e12e3e9ffc3e97faf55c264cb83c34004
stokes-unibas <75485474+stokes-unibas@users.noreply.github.com>
2024-03-18 14:47:42 +0100

    Update chocolateyinstall.ps1

--- automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
+++ automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
@@ -8,13 +8,13 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  url            = 'https://download.visualstudio.microsoft.com/download/pr/0c804185-7f86-4167-8703-8365d4939d72/02935dd20c741d36acb2c4eb2f2d5a21/windowsdesktop-runtime-9.0.0-preview.1.24081.3-win-x64.exe'
+  url            = ''
   silentArgs     = '/S'
   validExitCodes = @(0)
  
   # optional, highly recommended
   softwareName   = 'unibas-dotnetdesktopruntime' 
-  checksum       = 'a1f4fabe710c4edb012ca6578e4c3ccfed49e4af12c0f5dbb7b50b2f1c69cbf1'
+  checksum       = ''
   checksumType   = 'sha256'
 }
  
