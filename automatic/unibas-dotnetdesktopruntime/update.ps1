7dc7bf918d94b27320c6937434c3f0ef8cf6a02a
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2024-03-18 15:02:52 +0100

    Update and rename _update.ps1 to update.ps1

---
+++ automatic/unibas-dotnetdesktopruntime/update.ps1
@@ -0,0 +1,33 @@
+Import-Module chocolatey-au
+
+$releases = 'https://dotnet.microsoft.com/en-us/download'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
+            "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.packageName)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version_part_download_page = $download_page.Links | ? href -match "windows-x64-installer"
+    $domain  = $releases -split '(?<=//.+)/' | select -First 1
+    $version_download_page = $domain + $version_part_download_page
+    
+    $download_page = Invoke-WebRequest -Uri $version_download_page -UseBasicParsing
+    $url = $download_page.Links.href | ? {($_ -match 'win-x64.exe$') | select -First 1
+    $version = ($url -split("/"))[-1] -replace "dotnet-sdk-", "" -replace "-win-x64.exe", ""
+
+    @{
+		URL		= $url
+		Version	= $version
+    }
+}
+
+update -ChecksumFor 64 -NoCheckChocoVersion
