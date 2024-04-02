a1aa53c42edb72bb2c311f62958dc89aa3e5b8ba
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2024-04-02 14:30:59 +0200

    Update update.ps1

--- automatic/camtasia/update.ps1
+++ automatic/camtasia/update.ps1
@@ -1,51 +1,6 @@
-import-module chocolatey-au -Force
+import-module chocolatey-au
 
-#. $PSScriptRoot\..\..\scripts\all.ps1
-
-$releases    = 'https://www.videohelp.com/software/Camtasia-Studio/version-history'
-
-# Taken from https://gist.github.com/jstangroome/913062
-function Get-MsiProductVersion {
-
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [ValidateScript( {$_ | Test-Path -PathType Leaf})]
-        [string]
-        $Path
-    )
-
-    function Get-Property ($Object, $PropertyName, [object[]]$ArgumentList) {
-        return $Object.GetType().InvokeMember($PropertyName, 'Public, Instance, GetProperty', $null, $Object, $ArgumentList)
-    }
-
-    function Invoke-Method ($Object, $MethodName, $ArgumentList) {
-        return $Object.GetType().InvokeMember($MethodName, 'Public, Instance, InvokeMethod', $null, $Object, $ArgumentList)
-    }
-
-    $ErrorActionPreference = 'Stop'
-    Set-StrictMode -Version Latest
-
-    #http://msdn.microsoft.com/en-us/library/aa369432(v=vs.85).aspx
-    $msiOpenDatabaseModeReadOnly = 0
-    $Installer = New-Object -ComObject WindowsInstaller.Installer
-
-    $Database = Invoke-Method $Installer OpenDatabase  @($Path, $msiOpenDatabaseModeReadOnly)
-
-    $View = Invoke-Method $Database OpenView  @("SELECT Value FROM Property WHERE Property='ProductVersion'")
-
-    $null = Invoke-Method $View Execute
-
-    $Record = Invoke-Method $View Fetch
-    if ($Record) {
-        Write-Output (Get-Property $Record StringData 1)
-    }
-
-    $null = Invoke-Method $View Close @()
-    Remove-Variable -Name Record, View, Database, Installer
-
-}
-
+$releases    = 'https://download.techsmith.com/update/camtasiastudio/enu/manifest.xml?utm_source=product&utm_medium=cs&utm_campaign=cw22&ipc_item_name=cs&ipc_platform=windows'
 
 function global:au_SearchReplace {
     @{
@@ -57,64 +12,25 @@ function global:au_SearchReplace {
     }
 }
 
-function global:au_BeforeUpdate() {
-    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
-    $Latest.ChecksumType64 = 'SHA256'
-}
-
-# function global:au_AfterUpdate {
-#     Set-DescriptionFromReadme -SkipFirst 2
-# }
-
 function global:au_GetLatest {
-    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regexUrl = ": Camtasia (?<version>[\d\.]+)"
+    $response = Invoke-RestMethod -Uri $releases -Method 'Get' -ContentType 'text/xml' -UseBasicParsing
+    $response -match '\<Application Name="Camtasia Studio" Version="(?<version>[\d\.]+)"' | Out-Null
+    $version = $matches.version
+    
+    if (-not [string]::IsNullOrEmpty($version)) {
+        $downloadUrl = ("https://download.techsmith.com/camtasiastudio/releases/{0}/camtasia.msi" -f $version.Replace('.', ''))
 
-    $page.content -match $regexUrl
-
-    # we need to do a couple of things here to get the url version needed to download:
-    # if 4 have a patch in the semantic versioning we need to add two zeroes so we can produce fix versions if needed
-    # if the version starts with '20' we need to remove that - Camtastaa advertises the version as '2018' but tags it's installers as '18'
-    # remove all the dots from the string version number
-    $urlVersion = $matches.version
-
-    # remove the trailing '20' if it's there
-    if ($urlVersion.StartsWith("20")) {
-        $urlVersion = $urlVersion.SubString(2)
+        # check the URL is valid - this will throw if it's not
+        Invoke-WebRequest -Uri $downloadUrl -UseBasicParsing -Method HEAD
     }
     else {
-        # something isn't right if it doesn't start with '20' so throw an exception and we can come here to fix it
-        throw "Camtasia version '$urlVersion' does not start with '20' so they may have changed their version numbering."
-    }
-
-    # remove the dots from the version string
-    $urlVersion = $urlVersion.Replace('.', '')
-
-    # now we can construct what should be the url
-    $url = "https://download.techsmith.com/camtasiastudio/releases/$urlVersion/camtasia.msi"
-    $tempFile = New-TemporaryFile
-    Invoke-WebRequest -Uri $url -OutFile $tempFile -UseBasicParsing
-    $version = Get-MsiProductVersion -Path $tempFile
-
-    if ([version]$version.major -gt 100) {
-        throw "We downloaded a version of Camtasia and it's version number is '$version' - anything with a major version > 100 throws this exception. Something is not right and needs fixing."
-    }
-    else {
-        # the version needs to have a '20' added to the start of it.
-        $version = "20" + $version
-    }
-
-    # check if we have a revision number nad if so append 00 to it for use as a fix version
-    # see https://github.com/chocolatey/choco/wiki/CreatePackages#package-fix-version-notation
-    if (([version]$version).revision -ne -1) {
-        # we have a revision number - add the 00
-        $version += "00"
+        return
     }
 
     return @{
-        URL64        = $url
-        Version      = $version
+        URL64        = $downloadUrl
+        Version      = '20' + $version
     }
 }
 
-update -ChecksumFor none -NoCheckChocoVersion
+Update-Package -ChecksumFor 64
