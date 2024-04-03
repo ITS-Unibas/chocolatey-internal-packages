aaa87c5f5e35bf295fe02ddef4012c9736ca6261
stokes-unibas <75485474+stokes-unibas@users.noreply.github.com>
2024-04-03 08:24:26 +0200

    Create update.ps1

---
+++ automatic/keepass.install/update.ps1
@@ -0,0 +1,31 @@
+#import-module chocolatey-au
+
+$releases = 'https://keepass.info/download.html'
+
+function global:au_SearchReplace {
+    @{
+    }
+}
+
+function global:au_BeforeUpdate() {
+    Invoke-WebRequest -Uri $Latest.Url32 -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox `
+        -UseBasicParsing -OutFile "tools\KeePass-Setup.exe"
+}
+
+function global:au_AfterUpdate {
+    Set-DescriptionFromReadme -SkipFirst 2
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $regex = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/(?<version>[\d\.]+)/KeePass-[\d\.]+-Setup.exe/download'
+    $url = ($page.links | Where-Object href -Match $regex | Select-Object -First 1).href
+
+    return @{
+        URL32   = $url
+        Version = ConvertTo-VersionNumber -Version ([version]$matches.version) -Part 3
+    }
+}
+
+Update-Package -ChecksumFor None
