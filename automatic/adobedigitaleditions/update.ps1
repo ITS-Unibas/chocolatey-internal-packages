560c847051761fdb097142b1667eed732830b171
stokes-unibas <75485474+stokes-unibas@users.noreply.github.com>
2024-04-03 08:19:38 +0200

    Create update.ps1

---
+++ automatic/adobedigitaleditions/update.ps1
@@ -0,0 +1,38 @@
+#import-module chocolatey-au
+
+$releases    = 'https://www.adobe.com/solutions/ebook/digital-editions/download.html'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*url\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate() {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+    $Latest.ChecksumType32 = 'SHA256'
+}
+
+function global:au_AfterUpdate { 
+    Set-DescriptionFromReadme -SkipFirst 2 
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $regexVer  = "Adobe Digital Editions (.*) Installers"
+    $regexUrl = "ADE_.*_installer.exe"
+    if ($page.content -match $regexVer) { $version = $matches[1] }
+    $url = $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href
+
+    return @{
+        URL32        = $url
+        Version      = $version
+    }
+}
+
+update -ChecksumFor none
