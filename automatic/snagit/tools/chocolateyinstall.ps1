1fb93f36502d0c4a8738afc03d27de2a874e9cc7
stokes-unibas <75485474+stokes-unibas@users.noreply.github.com>
2024-04-03 08:09:40 +0200

    Create chocolateyinstall.ps1

---
+++ automatic/snagit/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# Old Techsmith software versions can be found at https://www.techsmith.com/download/oldversions
+$url64 = 'https://download.techsmith.com/snagit/releases/2402/snagit.msi'
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'MSI'
+    url64bit       = $Url64
+
+    checksum64     = '4abe1d873c5fa16e37b9ee0f2adb533f9a05a7d12e12a81207c6c03131fd2cbd'
+    checksumType64 = 'sha256'
+
+    silentArgs     = "/quiet /passive /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+    validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
