130ac682647df79d626ed832d71ba37e8b229e9c
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-04-09 05:32:22 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-cyberduck/tools/chocolateyinstall.ps1
+++ automatic/unibas-cyberduck/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://update.cyberduck.io/windows/Cyberduck-Installer-8.8.1.41263.exe'
+  url            = 'https://update.cyberduck.io/windows/Cyberduck-Installer-8.8.2.41344.exe'
   #file         = $fileLocation
 
   #MSI
@@ -32,7 +32,7 @@ $packageArgs = @{
 
   # optional, highly recommended
   softwareName   = 'Cyberduck' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = '00080c32d4c20f846623befb0b4190a635e1fc9dbe2a9c6941af7b019de0c88c'
+  checksum       = 'd615d7446e67c358059c5bda6c207d13aa1d7e04d4541256040eac72a66f9989'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
