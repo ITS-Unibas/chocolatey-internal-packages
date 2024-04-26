dda47c6251abf0597ae86e6078f1cbdb2582a931
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-04-26 13:34:55 +0000

    updated chocolatey-internal-packages

--- automatic/zoom/tools/chocolateyinstall.ps1
+++ automatic/zoom/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
-$checksum = '47ad56e363ccc09a18bac285252371ba3d9de952d4631bb5a9c70d67c31c7b3e'
-$checksum64 = 'b1d720860657b7f15a8511db4ea8b028ca7d48a1178a1b6c2a712f085d7c187d'
+$checksum = '2497dafcfc6e2cd64d44f346feff9b44abaeafa0843e4ae6a919fbb7b5409443'
+$checksum64 = '62b261bf521aad81ff8e204b56423feeab5d325854eb269dd140defb6ce32ebe'
 
-$url = 'https://cdn.zoom.us/prod/6.0.3.37634/ZoomInstallerFull.msi'
-$url64 = 'https://cdn.zoom.us/prod/6.0.3.37634/x64/ZoomInstallerFull.msi'
+$url = 'https://cdn.zoom.us/prod/6.0.4.38135/ZoomInstallerFull.msi'
+$url64 = 'https://cdn.zoom.us/prod/6.0.4.38135/x64/ZoomInstallerFull.msi'
 
 $packageArgs = @{
   packageName    = 'zoom'
