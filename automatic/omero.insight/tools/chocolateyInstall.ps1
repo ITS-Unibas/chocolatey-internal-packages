c2b0e019c41472b4c733c7c7cf13a9a8dadb739e
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-05-13 15:38:07 +0000

    updated chocolatey-internal-packages

--- automatic/omero.insight/tools/chocolateyInstall.ps1
+++ automatic/omero.insight/tools/chocolateyInstall.ps1
@@ -9,11 +9,11 @@ $LogPath = "$env:TEMP\"
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'MSI'
-  url            = 'https://github.com/ome/omero-insight/releases/download/v5.8.3/OMERO.insight-5.8.3.msi'
+  url            = 'https://github.com/ome/omero-insight/releases/download/v5.8.4/OMERO.insight-5.8.4.msi'
   silentArgs     = "/qn /norestart /l*v `"$($env:SWP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'OMERO.Insight' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = 'db8fea24f478106a5141c454de63cf47496fd2975f6cfa4081f765da6d268a95'
+  checksum       = '754c19d6edcac270fdced4be06e378bd378cb66de1191ab72c2ad8309bca0871'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
