21a5f606aa8e70ea6a80f1962a7aab20df936955
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-05-21 16:45:19 +0000

    updated chocolatey-internal-packages

--- automatic/vcredist140-x86/tools/chocolateyinstall.ps1
+++ automatic/vcredist140-x86/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'vcredist140-x86'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
-  url            = 'https://download.visualstudio.microsoft.com/download/pr/71c6392f-8df5-4b61-8d50-dba6a525fb9d/510FC8C2112E2BC544FB29A72191EABCC68D3A5A7468D35D7694493BC8593A79/VC_redist.x86.exe'
+  url            = 'https://download.visualstudio.microsoft.com/download/pr/9c69db26-cda4-472d-bdae-f2b87f4a0177/A32DD41EAAB0C5E1EAA78BE3C0BB73B48593DE8D97A7510B97DE3FD993538600/VC_redist.x86.exe'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0, 1638, 3010)
   softwareName   = 'Microsoft Visual C++ 2015-2019 Redistributable (x86)*'
-  checksum       = '510fc8c2112e2bc544fb29a72191eabcc68d3a5a7468d35d7694493bc8593a79'
+  checksum       = 'a32dd41eaab0c5e1eaa78be3c0bb73b48593de8d97a7510b97de3fd993538600'
   checksumType   = 'sha256'
 }
 
