8dad2114a3826421d281d077a6714ae40555f5b1
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-06-27 04:53:46 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-anaconda3/tools/chocolateyinstall.ps1
+++ automatic/unibas-anaconda3/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Anaconda3'
   fileType       = 'EXE'
-  url            = 'https://repo.continuum.io/archive/Anaconda3-2024.02-1-Windows-x86_64.exe'
-  checksum       = '087c633e355bc709222ad9e0dbad77abeed84f7e06dbdbecae84ba8b3660429e'
+  url            = 'https://repo.continuum.io/archive/Anaconda3-2024.06-1-Windows-x86_64.exe'
+  checksum       = '45eef9132b70e3d5a493c26429ddb4e2c9278a32097fd4bc729f860685633a3b'
   checksumType   = 'sha256'
   silentArgs     = "/S /InstallationType=AllUsers /RegisterPython=1 /AddToPath=1 /D=C:\Program Files\Anaconda3"
   validExitCodes = @(0)
