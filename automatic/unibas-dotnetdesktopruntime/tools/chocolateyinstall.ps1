939f12c141ba59b1cc2568d52d72f61e9c3c1f4c
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2024-07-11 15:58:55 +0200

    Update chocolateyinstall.ps1

--- automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
+++ automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
@@ -1,18 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
  
 $packageName = 'unibas-dotnetdesktopruntime'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$LogPath = "$env:SWP\"
  
 $packageArgs = @{
   packageName    = $packageName
-  unzipLocation  = $toolsDir
   fileType       = 'exe'
   url            = 'https://download.visualstudio.microsoft.com/download/pr/bb581716-4cca-466e-9857-512e2371734b/5fe261422a7305171866fd7812d0976f/windowsdesktop-runtime-8.0.7-win-x64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
  
-  # optional, highly recommended
   softwareName   = 'unibas-dotnetdesktopruntime' 
   checksum       = '622e3d046852caae4c28a71be174020dabbc6b21f2a1a320ac0c41c701a6a52c'
   checksumType   = 'sha256'
