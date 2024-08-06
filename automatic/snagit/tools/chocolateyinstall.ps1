16f4ab9c0334fcacc2ef089e7785a63dbb57e7e7
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-08-06 15:39:59 +0000

    updated chocolatey-internal-packages

--- automatic/snagit/tools/chocolateyinstall.ps1
+++ automatic/snagit/tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Old Techsmith software versions can be found at https://www.techsmith.com/download/oldversions
-$url64 = 'https://download.techsmith.com/snagit/releases/2414/snagit.msi'
+$url64 = 'https://download.techsmith.com/snagit/releases/2420/snagit.msi'
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'MSI'
     url64bit       = $Url64
 
-    checksum64     = '7c1d67fefb165cb325dfbb0e66637035e252126ffb695c9fe6f17c5b0c719b4e'
+    checksum64     = 'f2c5a13426ba2eeca7296a7aa24ada4752c3c213ac67b3b924fb7c6459389f5d'
     checksumType64 = 'sha256'
 
     silentArgs     = "/quiet /passive /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
