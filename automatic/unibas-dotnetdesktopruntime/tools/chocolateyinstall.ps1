d1e478754bfa3d653820a5995c30ef19be9c5fba
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-08-13 17:41:25 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
+++ automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
@@ -5,12 +5,12 @@ $packageName = 'unibas-dotnetdesktopruntime'
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://download.visualstudio.microsoft.com/download/pr/bb581716-4cca-466e-9857-512e2371734b/5fe261422a7305171866fd7812d0976f/windowsdesktop-runtime-8.0.7-win-x64.exe'
+  url            = 'https://download.visualstudio.microsoft.com/download/pr/907765b0-2bf8-494e-93aa-5ef9553c5d68/a9308dc010617e6716c0e6abd53b05ce/windowsdesktop-runtime-8.0.8-win-x64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
  
   softwareName   = 'unibas-dotnetdesktopruntime' 
-  checksum       = '622e3d046852caae4c28a71be174020dabbc6b21f2a1a320ac0c41c701a6a52c'
+  checksum       = '1d8fc3eaad1285d701a77216e923c8be404dc2254d51779503828a3b3e1cdeb1'
   checksumType   = 'sha256'
 }
  
