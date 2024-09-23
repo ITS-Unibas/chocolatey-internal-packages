c833f4c15dcaf315fd0e110d2e6997ed2d41bbe3
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-09-23 12:02:02 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-maxqda/tools/chocolateyInstall.ps1
+++ automatic/unibas-maxqda/tools/chocolateyInstall.ps1
@@ -8,12 +8,12 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url            = ''
+  url            = 'https://updates.maxqda.de/24/MAXQDA24_Setup.msi'
   silentArgs     = '/qn LIC_SRV="131.152.22.50" LIC_NAME="MAXQDA" DISABLE_AUTO_UPDATES=1 DISABLE_MANUAL_UPDATES=1 /norestart' # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0,129, 3010, 1605, 1614, 1641)
   softwareName   = 'MAXQDA*'
-  checksum     = ''
-  checksumType = ''
+  checksum     = '2b2db98385335dd0b63a6aa4fdef2ae13be4fecfd1444a5365aeb0ead6427305'
+  checksumType = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
