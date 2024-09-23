900defedddaa70009f49ffd2045a0b6518a2ab55
Tim Keller <tim.keller@unibas.ch>
2024-09-23 13:47:27 +0200

    unibas-maxqda update script

---
+++ automatic/unibas-maxqda/tools/FinalScript.ps1
@@ -0,0 +1,1 @@
+Remove-Item -Path "C:\Users\Public\Desktop\MAXQDA 2024.lnk" -Force -ErrorAction SilentlyContinue
---
+++ automatic/unibas-maxqda/update.ps1
@@ -0,0 +1,30 @@
+Import-Module chocolatey-AU
+
+$releases = 'https://www.maxqda.com/de/produkte/maxqda-release-notes'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url64 = Get-RedirectedUrl 'https://www.maxqda.com/download/maxqdademo'
+
+    $re = "Release (\d+\.\d+\.\d+)"
+
+    $version = ([regex]::Match($download_page.RawContent, $re)).Captures.Groups[1].value
+
+    return @{
+        URL64 = $url64
+        Version = $version
+    }
+}
+
+update -ChecksumFor 64
