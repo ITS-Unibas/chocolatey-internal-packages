d4436b864c3c4a69a53b45503315e7fcc9bf8033
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2024-09-27 17:43:43 +0200

    Update chocolateyinstall.ps1

--- automatic/firefoxesr/tools/chocolateyinstall.ps1
+++ automatic/firefoxesr/tools/chocolateyinstall.ps1
@@ -1,13 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-
-$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
-
 $packageArgs = @{
   packageName   = 'firefoxesr'
   softwareName  = 'Mozilla Firefox*'
   fileType      = 'MSI'
-  unzipLocation = $toolsDir
   url           = 'https://ftp.mozilla.org/pub/firefox/releases/128.1.0esr/win64/en-US/Firefox%20Setup%20128.1.0esr.msi'
   checksum      = 'ece306bfc2de3dcbb9bcf7487ce53499d5e7bc5b334b0e6426a95fb314829f7b3b371082b80742bf96ad547875d16047a429fca27e5fc1ebd6e397505983413a'
   checksumType  = 'sha512'
