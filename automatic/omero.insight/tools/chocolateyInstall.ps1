1a494f85ad44a6e19cd405b2daa9b1b7d47b2b40
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-09-30 13:05:18 +0000

    updated chocolatey-internal-packages

--- automatic/omero.insight/tools/chocolateyInstall.ps1
+++ automatic/omero.insight/tools/chocolateyInstall.ps1
@@ -9,11 +9,11 @@ $LogPath = "$env:TEMP\"
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'MSI'
-  url            = 'https://github.com/ome/omero-insight/releases/download/v5.8.4/OMERO.insight-5.8.4.msi'
+  url            = 'https://github.com/ome/omero-insight/releases/download/v5.8.6/OMERO.insight-5.8.6.msi'
   silentArgs     = "/qn /norestart /l*v `"$($env:SWP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'OMERO.Insight' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = '754c19d6edcac270fdced4be06e378bd378cb66de1191ab72c2ad8309bca0871'
+  checksum       = '8dcd12038aec67d1ac32869518ee9386ef8f363a0c9e7d2884635e8ec00dcbdd'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
--- automatic/unibas-rdm/tools/chocolateyinstall.ps1
+++ automatic/unibas-rdm/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   fileType      = 'EXE'
   silentArgs    = '/S'
   validExitCodes= @(0)
-  url           = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.2024.3.10.0.exe'
-  checksum      = 'd0e8833ee46589ffd74a6e7fa3cb951c737ea3adb3dcdfab8d5bf35f580b97df'
+  url           = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.2024.3.12.0.exe'
+  checksum      = '9a5dfa548c90d86bc081245dd8cdb57c6d40f6f60c025e9ad9e35e4b946f9e4c'
   checksumType  = 'sha256'
 }
  
