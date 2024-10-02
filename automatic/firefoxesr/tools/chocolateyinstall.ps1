4c745ed0a0bc908d78c2f43811f5915dc5fffec2
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-10-02 14:28:11 +0000

    updated chocolatey-internal-packages

--- automatic/firefoxesr/tools/chocolateyinstall.ps1
+++ automatic/firefoxesr/tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ $packageArgs = @{
   packageName   = 'firefoxesr'
   softwareName  = 'Mozilla Firefox*'
   fileType      = 'MSI'
-  url           = 'https://ftp.mozilla.org/pub/firefox/releases/128.1.0esr/win64/en-US/Firefox%20Setup%20128.1.0esr.msi'
-  checksum      = 'ece306bfc2de3dcbb9bcf7487ce53499d5e7bc5b334b0e6426a95fb314829f7b3b371082b80742bf96ad547875d16047a429fca27e5fc1ebd6e397505983413a'
-  checksumType  = 'sha512'
+  url           = 'https://ftp.mozilla.org/pub/firefox/releases/128.3.0esr/win64/en-US/Firefox%20Setup%20128.3.0esr.msi'
+  checksum      = 'e00c994f5146825e672936dfcb9f74ef7d901978903fdf041d6b8bfb3ca546ce'
+  checksumType  = 'sha256'
   silentArgs    = '/quiet /norestart'
   validExitCodes = @(0, 3010)
 }
