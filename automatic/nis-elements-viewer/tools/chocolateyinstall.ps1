b83f2024f0e51dbcaf535126c88f2380af78e5d9
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-10-07 09:49:44 +0000

    updated chocolatey-internal-packages

--- automatic/nis-elements-viewer/tools/chocolateyinstall.ps1
+++ automatic/nis-elements-viewer/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://d33b8x22mym97j.cloudfront.net/phase5/files/downloads/NIS_Viewer_5.21.00_b1483_64bit.zip'
+  url            = 'https://www.software-dl.microscope.healthcare.nikon.com/en/imgsfw/data/NIS_Viewer_Ver52200_E.zip'
   silentArgs     = "/s /sms /f1C:\temp\setup.iss /f2c:\temp\install.txt" # s: silent, sms: wait for process, f1: import setup.iss-file for silent install, f2: install-log
   softwareName   = 'NIS-Elements viewer*' 
-  checksum       = '82dfc2dd6419c4c4ffc9eb81ea4b58c2296528fa3a0c5ec5c3505402afbcf989'
+  checksum       = 'd80b02e6f7bc96a81621a665c3a2ef693c188aaa562e21505e486c6dd4dcde8b'
   checksumType   = 'sha256'
 }
 
