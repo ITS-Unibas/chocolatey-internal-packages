e68c2c5b33ade673665922f823cb9903ce7aab0f
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-11-15 04:40:38 +0000

    updated chocolatey-internal-packages

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/91c9f3ef-cbc0-4380-a98d-8662fbca35f6/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/48092499-6d65-4b3d-b75c-01aa52a8d08c/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = '84BA997B57BB7B4EE4222816D2202D090F8002874A3FBA6EF54F11A74CFF1BD4'
+  checksum       = 'BE8B36B449A454D0D24C164B041A2DB2B689A8C3647CFB9FE104977E365677D0'
   checksumType   = 'sha256'
 }
 
--- automatic/unibas-msedge-webview2/tools/chocolateyinstall.ps1
+++ automatic/unibas-msedge-webview2/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageName = 'unibas-msedge-webview2'
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/0a9218ab-7dca-40e4-941f-d24d1d8e3970/MicrosoftEdgeWebView2RuntimeInstallerX64.exe'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/93aa6c36-7437-4c82-b77a-8f78ade90455/MicrosoftEdgeWebView2RuntimeInstallerX64.exe'
   silentArgs     = "/silent /install"
   validExitCodes = @(0)
   softwareName   = 'unibas-msedge-webview2*'
-  checksum       = 'fa07bc67c2a68763e127f6aabf5622b40a0220228899e78daf02ce2cc3971a50'
+  checksum       = 'ce09273fa2c0be6273ba151d51013aa74ae609b79d865682539758869f391075'
   checksumType   = 'sha256'
 }
 
