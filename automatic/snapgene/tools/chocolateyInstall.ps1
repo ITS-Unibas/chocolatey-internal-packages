c7275fa4a79a06783386c55896328cac4331267e
Philip Stokes - stokes-adm <stokes-adm@unibas.ch>
2024-11-19 10:18:05 +0100

    snap

---
+++ automatic/snapgene/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'snapgene'
+  fileType       = 'EXE'
+  url            = ''
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Snapgene*' 
+  checksum       = ''
+  checksumType   = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/snapgene/update.ps1
@@ -0,0 +1,28 @@
+Import-Module chocolatey-au -Force
+
+$releases = 'https://www.snapgene.com/updates'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm sha256
+ }
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+    $regex_latest_version = "The latest SnapGene version is<!-- --> <!-- -->([\d+\.]+)."
+    $download_page.Content -match $regex_latest_version
+    [version]$version = $matches[1]
+    $reURL = "https:\/\/cdn.snapgene.com\/downloads\/SnapGene\/([\d+\.]+).x\/([\d+\.]+)\/([\d+\.]+)\/snapgene_([\d+\.]+)_win.exe"
+    $url = ([regex]::Match($download_page.Content, $reURL)).Captures.value
+    return @{ Version = $version; URL = $url }
+}
+
+update -ChecksumFor none -NoCheckUrl
