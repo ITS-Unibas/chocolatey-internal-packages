5fcebdafa7ff4281b1c2fbf6c798bd3a70a14aae
ITS-Unibas <ITS-Unibas+github@gmail.com>
2024-12-08 14:37:19 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-mobaxterm/tools/chocolateyinstall.ps1
+++ automatic/unibas-mobaxterm/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = 'unibas-mobaxterm'
-  url            = 'https://download.mobatek.net/2432024101610907/MobaXterm_Installer_v24.3.zip'
+  url            = 'https://download.mobatek.net/2442024120112207/MobaXterm_Installer_v24.4.zip'
   unzipLocation  = $toolsDir
   softwareName   = 'unibas-mobaxterm*'
-  checksum       = 'f406a07f5f42f029cb21e5d30fb3f0f25a9f5df62467df3775dff8f56cbd4254'
+  checksum       = 'f66fd78e0e004e9017580ed5d5e915e209fd9da1c47616dbd0cc1cdb7f923b19'
   checksumType   = 'sha256' 
 }
 
