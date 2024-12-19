7aaee21927552dcb11ac71038be1481486dd9011
Max Mendes <max.mendesc@gmail.com>
2024-12-19 10:14:19 +0100

    Possible fix for unibas-maxqda

--- automatic/unibas-maxqda/tools/chocolateyInstall.ps1
+++ automatic/unibas-maxqda/tools/chocolateyInstall.ps1
@@ -12,13 +12,8 @@ $packageArgs = @{
   silentArgs     = '/qn LIC_SRV="131.152.22.50" LIC_NAME="MAXQDA" DISABLE_AUTO_UPDATES=1 DISABLE_MANUAL_UPDATES=1 /norestart' # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0,129, 3010, 1605, 1614, 1641)
   softwareName   = 'MAXQDA*'
-<<<<<<< HEAD
   checksum     = ''
   checksumType = ''
-=======
-  checksum     = ''
-  checksumType = ''
->>>>>>> 3a281d320b89097d0e065df1ec4561b572a50628
 }
 
 Install-ChocolateyPackage @packageArgs
