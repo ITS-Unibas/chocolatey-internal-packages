de5aac3826e1324740fdc07e4b13cf8f3c40f26f
Max Mendes <135263966+verifizieren@users.noreply.github.com>
2025-02-13 13:30:22 +0100

    possible Mindmanager fix

---
+++ automatic/unibas-mindmanager/tools/chocolateyinstall_old.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:ChocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$LogPath = "$env:TEMP\"
+$fileLocation = Join-Path $toolsDir 'MindManager 23.msi'
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+    file = (Join-Path $toolsDir 'MindManager_64bit_24.1.150_Admin.zip')
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  silentArgs     = "/quiet /norestart CAPTUREENABLED=`"0`" /l*v `"$($LogPath)Install_$($env:chocolateyPackageName)_$($env:chocolateyPackageVersion).log`""
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+  softwareName   = 'Mindmanager*'
+}
+&(Join-Path $PSScriptRoot InitialScript.ps1)
+Install-ChocolateyPackage @packageArgs
+&(Join-Path $PSScriptRoot FinalScript.ps1)
+
+
