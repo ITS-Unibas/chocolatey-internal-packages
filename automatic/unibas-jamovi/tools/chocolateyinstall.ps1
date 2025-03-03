604b901cec116ca7666e0841a545e77be63994e1
ITS-Unibas <ITS-Unibas+github@gmail.com>
2025-03-03 04:46:52 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-jamovi/tools/chocolateyinstall.ps1
+++ automatic/unibas-jamovi/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = 'https://jamovi.org//downloads/jamovi-2.6.25.0-win-x64.exe'
+  url64bit       = 'https://jamovi.org//downloads/jamovi-2.6.26.0-win-x64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'unibas-jamovi*'
-  checksum64     = '75a2a2d6cf1c91e7f4d67ff7cbb0e6839b3b7bcbfe130f8aeb8762a278a3687a'
+  checksum64     = '6d1879db60ac03c8aeeeb25d64268cec3cee91e9233dd527e7b934b6c4a2a422'
   checksumType64 = 'sha256' #default is checksumType
 }
 
