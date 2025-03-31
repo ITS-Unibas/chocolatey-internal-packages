3c511b4c8657b4c85bf7088dc54adeb1dc22ebd2
ITS-Unibas <ITS-Unibas+github@gmail.com>
2025-03-31 07:35:14 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-advanced-renamer/tools/chocolateyinstall.ps1
+++ automatic/unibas-advanced-renamer/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
-  url            = 'https://www.advancedrenamer.com/down/win/advanced_renamer_setup_4_10.exe'
+  url            = 'https://www.advancedrenamer.com/down/win/advanced_renamer_setup_4_11.exe'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART"
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'unibas-advanced-renamer*'
-  checksum       = '8bee61747cb46fe23a5365dc01cb2b89f93a4aca0ebee545f2d9355b74e2b42a'
+  checksum       = 'c0d3895f789ea89faca60766d3ed7cf4cb8d15bd57547e4a69c7eaf86370bb98'
   checksumType   = 'sha256'
 }
 
