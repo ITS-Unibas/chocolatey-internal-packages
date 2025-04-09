a1f3156a4c3aa13078f06792504e9e93adff2f0d
ITS-Unibas <ITS-Unibas+github@gmail.com>
2025-04-09 04:41:02 +0000

    updated chocolatey-internal-packages

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/f514a0c9-7e26-40a7-8bdf-3a50bd5982ec/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/44b00339-4b5d-434b-8267-8f5bcfad0286/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = '6166443C3C230E96C81C6D33759B6530E6B5F4780782BE35C161EEA19428C3C5'
+  checksum       = 'DFA93B4E36FCADFD5FA219A9CED80444D236E4690AFE5BAB52DBC44BED6BAD93'
   checksumType   = 'sha256'
 }
 
--- automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
+++ automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
@@ -5,12 +5,12 @@ $packageName = 'unibas-dotnetdesktopruntime'
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://download.visualstudio.microsoft.com/download/pr/63f0335a-6012-4017-845f-5d655d56a44f/f8d5150469889387a1de578d45415201/windowsdesktop-runtime-9.0.3-win-x64.exe'
+  url            = 'https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/9.0.4/windowsdesktop-runtime-9.0.4-win-x64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
  
   softwareName   = 'unibas-dotnetdesktopruntime' 
-  checksum       = 'e477efa08fd14d6d543b71dbfca0c5e2f2a1b18dda244d3e6bc0ef548c528c38'
+  checksum       = '526dd9b55389fb76816bbac42c97c17b802ebcd2107e73ffb2c77a74efd1fed1'
   checksumType   = 'sha256'
 }
  
