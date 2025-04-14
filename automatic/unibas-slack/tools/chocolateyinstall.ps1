9cd8a318492a87ad6ca4d0b687cee16708dd860e
ITS-Unibas <ITS-Unibas+github@gmail.com>
2025-04-14 17:36:05 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-slack/tools/chocolateyinstall.ps1
+++ automatic/unibas-slack/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32 = 'https://downloads.slack-edge.com/releases/windows/4.33.84/prod/ia32/slack-standalone-4.33.84.0.msi'
 $checksum32 = '2dc813f90c9726931556515e4d06226af8dbf43708f3e427c56b924ef74bd66b'
 $checksumType32 = 'sha256'
-$url64 = 'https://downloads.slack-edge.com/desktop-releases/windows/x64/4.43.51/slack-standalone-4.43.51.0.msi'
-$checksum64 = 'b7a019d65773b1d1f0907350db7d972f29e89ede9f4c41bc4d9eb8de4bc2af5c'
+$url64 = 'https://downloads.slack-edge.com/desktop-releases/windows/x64/4.43.52/slack-standalone-4.43.52.0.msi'
+$checksum64 = '21b2039b609f53219ca0a4f7de358fd0774e588e7e997e1e844b43db46225b8d'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
