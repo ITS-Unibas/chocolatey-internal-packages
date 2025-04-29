783e24b4296493c1db479718e48d231e55eb11d9
Max Mendes <135263966+verifizieren@users.noreply.github.com>
2025-04-29 14:03:47 +0200

    updates nuspec with correct version scraped for dependency (rdm)

--- automatic/unibas-rdm/update.ps1
+++ automatic/unibas-rdm/update.ps1
@@ -17,19 +17,38 @@ function global:au_SearchReplace {
   }
 }
 
-
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
   $urltext = $download_page.ToString()
-  $regExResult = $urltext | Select-String -Pattern 'Version:\s*(\d+\.\d+\.\d+\.\d+)'
+  $regExResult = $urltext | Select-String -Pattern 'Version:\s*(\d+(?:\.\d+)+)' -CaseSensitive:$false
     $version = $regExResult.Matches[0].Groups[1].Value
   $url = "https://cdn.devolutions.net/download/Setup.RemoteDesktopManager."
   $url += $version
   $url += ".exe"
+
+  $deversionurl = 'https://community.chocolatey.org/packages/rdm#dependencies'
+  $deversiontext = Invoke-WebRequest -Uri $deversionurl -UseBasicParsing | Select-Object -ExpandProperty Content
+  $deversiontext = $deversiontext -replace '<.*?>', ''
+  if ($deversiontext -cmatch '(?i)dotnet-9.0-desktopruntime\s*\(\s*\u2265\s*(\d+(?:\.\d+)+)\s*\)') {
+    $depversion1 = $matches[1]
+  }
+  if ($deversiontext -cmatch '(?i)webview2-runtime\s*\(\s*\u2265\s*(\d+(?:\.\d+)+)\s*\)') {
+    $depversion2 = $matches[1]
+  }
   return @{
     URL     = $url
     Version = $version
+    DependencyVersion1 = $depversion1 # dotnet
+    DependencyVersion2 = $depversion2 # webview2
   }
+}
+
+function global:au_AfterUpdate {
+  $NuspecFilePath = Join-Path -Path $PSScriptRoot -ChildPath 'unibas-rdm.nuspec'
+  [xml]$xml = Get-Content -Path $NuspecFilePath
+  $xml.package.metadata.dependencies.dependency | Where-Object { $_.id -eq "unibas-msedge-webview2" } | ForEach-Object { $_.version = "[$($Latest.DependencyVersion2),)" }
+  $xml.package.metadata.dependencies.dependency | Where-Object { $_.id -eq "unibas-dotnetdesktopruntime" } | ForEach-Object { $_.version = "[$($Latest.DependencyVersion1),)" }
+  $xml.Save($NuspecFilePath)
 }
 
 update -ChecksumFor none
