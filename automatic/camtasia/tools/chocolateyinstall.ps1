0aa4c5cc9d957b04b6b3778b0dbe8f695df30291
ITS-Unibas <ITS-Unibas+github@gmail.com>
2025-05-27 13:51:25 +0000

    updated chocolatey-internal-packages

--- automatic/camtasia/tools/chocolateyinstall.ps1
+++ automatic/camtasia/tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Old Camtasia versions can be found at https://www.techsmith.com/download/oldversions
-$url64      = 'https://download.techsmith.com/camtasiastudio/releases/2511/camtasia.msi'
+$url64      = 'https://download.techsmith.com/camtasiastudio/releases/2512/camtasia.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
   url64bit      = $url64
 
-  checksum64    = '215923298fad804f296d3f036544acadc8a221aad3ed58c3243769a70930b489'
+  checksum64    = '0ad55e74d3f026e5f5bc66a7971f9e90b0c64c01b9a26b00e47114216672573f'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
