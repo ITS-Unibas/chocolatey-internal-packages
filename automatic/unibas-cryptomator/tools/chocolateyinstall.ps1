ca6ffad2ae71381c4f77235d916539614fbc3c8e
ITS-Unibas <ITS-Unibas+github@gmail.com>
2025-07-08 12:52:28 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-cryptomator/tools/chocolateyinstall.ps1
+++ automatic/unibas-cryptomator/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'MSI'
-  url            = 'https://github.com/cryptomator/cryptomator/releases/download/1.17.0/Cryptomator-1.17.0-x64.msi'
+  url            = 'https://github.com/cryptomator/cryptomator/releases/download/1.17.1/Cryptomator-1.17.1-x64.msi'
   silentArgs     = '/qn /norestart'
   validExitCodes = @(0) 
   softwareName   = 'Cryptomator'
-  checksum       = '3bae9567d445edb930c7f32f5fa2f288cfde459163ab3681c648161a34059295'
+  checksum       = '794435f090232a8af2d0e9fb770e02ce2d99326323433a46f4125cefb3e6713b'
   checksumType   = 'sha256'
 }
 
--- automatic/unibas-logitech-options-plus/tools/chocolateyinstall.ps1
+++ automatic/unibas-logitech-options-plus/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $packageName = $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $LogPath = "$env:temp\"
 
-$url = ''
-$checksum = ''
+$url = 'https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.exe'
+$checksum = 'ced5fe8f9cae1df4a49b4a15301ec047accd7763ecafa8265ca7e5035fd34dea'
 $checksumType = 'sha256'
 
 $packageArgs = @{
