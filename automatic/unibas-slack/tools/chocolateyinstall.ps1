7957efa7ad15b94163fb6692e16401fe7ef9af48
ITS-Unibas <ITS-Unibas+github@gmail.com>
2025-07-15 04:57:39 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-slack/tools/chocolateyinstall.ps1
+++ automatic/unibas-slack/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32 = 'https://downloads.slack-edge.com/releases/windows/4.33.84/prod/ia32/slack-standalone-4.33.84.0.msi'
 $checksum32 = '2dc813f90c9726931556515e4d06226af8dbf43708f3e427c56b924ef74bd66b'
 $checksumType32 = 'sha256'
-$url64 = 'https://downloads.slack-edge.com/desktop-releases/windows/x64/4.44.65/slack-standalone-4.44.65.0.msi'
-$checksum64 = '7906c6ded34a105e9d011b3e7405965d56a20dca48de314d592a0c68c4ae21de'
+$url64 = 'https://downloads.slack-edge.com/desktop-releases/windows/x64/4.45.60/slack-standalone-4.45.60.0.msi'
+$checksum64 = '455671c3ff16de271ce86c1dd1502b3df8f084c15ae44623cc4098c765e0d10d'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
--- automatic/unibas-zotero/tools/chocolateyInstall.ps1
+++ automatic/unibas-zotero/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0, 3010, 1605, 1614, 1641)
   softwareName   = 'Zotero'
-  url64bit       = 'https://download.zotero.org/client/release/7.0.20/Zotero-7.0.20_x64_setup.exe'
-  checksum64     = '673d5d4d0750aa6612e897272c392efe14379d5039492419e69150f687e5f64d'
+  url64bit       = 'https://download.zotero.org/client/release/7.0.21/Zotero-7.0.21_x64_setup.exe'
+  checksum64     = '74b685354ff23b9b8261de3f8b741cee6937f3dfa0f687a99ea3fbf656729659'
   checksumType64 = 'sha256'
 }
 
