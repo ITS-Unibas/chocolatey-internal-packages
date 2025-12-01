fe91ac31d282fc4a9d68b0b6fee5ad2e26da50c0
Max Mendes <135263966+verifizieren@users.noreply.github.com>
2025-12-01 16:20:15 +0100

    reflected change audacity 2

--- automatic/unibas-audacity/tools/chocolateyinstall.ps1
+++ automatic/unibas-audacity/tools/chocolateyinstall.ps1
@@ -1,16 +1,34 @@
+
 $ErrorActionPreference = 'Stop';
-$packageName = $env:ChocolateyPackageName
+
+
+$packageName = 'unibas-audacity'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
 $packageArgs = @{
-  file = (Join-Path $toolsDir 'audacity-win-3.7.5-64bit.exe')
-  packageName = $env:ChocolateyPackageName
+  packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  silentArgs     = '/VERYSILENT /NORESTART'
-  validExitCodes = @(0, 3010, 1605, 1614, 1641)
-  softwareName   = 'Audacity'
-}
-&(Join-Path $PSScriptRoot InitialScript.ps1)
-Install-ChocolateyPackage @packageArgs
-&(Join-Path $PSScriptRoot FinalScript.ps1)
+  url            = 'https://github.com/audacity/audacity/releases/download/Audacity-3.7.5/audacity-win-3.7.5-64bit.exe'
+  #OTHERS
+  # Uncomment matching EXE type (sorted by most to least common)
+  #silentArgs   = '/S'           # NSIS
+  silentArgs     = '/VERYSILENT' # Inno Setup
+  #silentArgs   = '/s'           # InstallShield
+  #silentArgs   = '/s /v"/qn"' # InstallShield with MSI
+  #silentArgs   = '/s'           # Wise InstallMaster
+  #silentArgs   = '-s'           # Squirrel
+  #silentArgs   = '-q'           # Install4j
+  #silentArgs   = '-s -u'        # Ghost
+  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
+  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
+  #       https://chocolatey.org/packages/autohotkey.portable
+  validExitCodes = @(0) #please insert other valid exit codes here
 
+  # optional, highly recommended
+  softwareName   = 'Audacity' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum       = 'd618903c6368785e35a0d6933a741f7efb75d869ca0897cb690d323c720d27c9'
+  checksumType   = 'sha256' #default is md5, can also be sha1
+}
+
+Install-ChocolateyPackage @packageArgs
