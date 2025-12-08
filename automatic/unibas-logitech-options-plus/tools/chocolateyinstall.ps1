61d0e6ebbd7e47e6feb11d6bf130b68d5d6eafdc
Max Mendes <135263966+verifizieren@users.noreply.github.com>
2025-12-08 11:11:03 +0100

    possible fix for logitech options plus

--- automatic/unibas-logitech-options-plus/tools/chocolateyinstall.ps1
+++ automatic/unibas-logitech-options-plus/tools/chocolateyinstall.ps1
@@ -3,19 +3,19 @@ $packageName = $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $LogPath = "$env:temp\"
 
-$url64 = 'https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.exe'
-$checksum64 = ''
-$checksumType64 = 'sha256'
+$url = 'https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.exe'
+$checksum = '4c1b1a9ee013d4319aaabbe7130e8875d0d5fecedb26e1c2ba3e1c724c8049f8'
+$checksumType = 'sha256'
 
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
-  url64bit       = $url64
+  url            = $url
   silentArgs     = "/quiet /analytics No /update No /dfu Yes"
   validExitCodes = @(0, 3010, 1605, 1614, 1641)
   softwareName   = 'Logi Options+*'
-  checksum64     = $checksum64
-  checksumType64 = $checksumType64
+  checksum       = $checksum
+  checksumType   = $checksumType
 }
 
 &(Join-Path $PSScriptRoot InitialScript.ps1)
