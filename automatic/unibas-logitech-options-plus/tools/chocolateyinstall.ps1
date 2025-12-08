dd19c14a3b1fc0ec024d04f5447efe87600e8e3c
Max Mendes <135263966+verifizieren@users.noreply.github.com>
2025-12-08 11:02:09 +0100

    applid 64bit fix for logitech options plus

--- automatic/unibas-logitech-options-plus/tools/chocolateyinstall.ps1
+++ automatic/unibas-logitech-options-plus/tools/chocolateyinstall.ps1
@@ -3,21 +3,21 @@ $packageName = $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $LogPath = "$env:temp\"
 
-$url = 'https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.exe'
-$checksum = '4c1b1a9ee013d4319aaabbe7130e8875d0d5fecedb26e1c2ba3e1c724c8049f8'
-$checksumType = 'sha256'
+$url64 = 'https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.exe'
+$checksum64 = '4c1b1a9ee013d4319aaabbe7130e8875d0d5fecedb26e1c2ba3e1c724c8049f8'
+$checksumType64 = 'sha256'
 
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
-  url            = $url
+  url64bit       = $url64
   silentArgs     = "/quiet /analytics No /update No /dfu Yes"
   validExitCodes = @(0, 3010, 1605, 1614, 1641)
   softwareName   = 'Logi Options+*'
-  checksum       = $checksum
-  checksumType   = $checksumType
+  checksum64     = $checksum64
+  checksumType64 = $checksumType64
 }
 
 &(Join-Path $PSScriptRoot InitialScript.ps1)
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
 &(Join-Path $PSScriptRoot FinalScript.ps1) 
--- automatic/unibas-logitech-options-plus/update.ps1
+++ automatic/unibas-logitech-options-plus/update.ps1
@@ -6,9 +6,9 @@ $releases = 'https://community.chocolatey.org/packages/logioptionsplus'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*\`$url\s*=\s*)('.*')"           = "`$1'$($Latest.URL)'"
-            "(?i)(^\s*\`$checksum\s*=\s*)('.*')"      = "`$1'$($Latest.Checksum)'"
-            "(?i)(^\s*\`$checksumType\s*=\s*)('.*')"  = "`$1'$($Latest.ChecksumType)'"
+            "(?i)(^\s*\`$url64\s*=\s*)('.*')"           = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*\`$checksum64\s*=\s*)('.*')"      = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*\`$checksumType64\s*=\s*)('.*')"  = "`$1'$($Latest.ChecksumType64)'"
         }
     }
 }
@@ -29,9 +29,9 @@ function global:au_GetLatest {
         
         return @{
             Version = $version
-            URL = $url
-            Checksum = $checksum
-            ChecksumType = 'sha256'
+            URL64 = $url
+            Checksum64 = $checksum
+            ChecksumType64 = 'sha256'
         }
     } else {
         throw "Could not find version information"
