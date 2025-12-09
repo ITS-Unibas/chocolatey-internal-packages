055ad6c3f0c9d923d8009a1f6dcfaf1c96f5456f
ITS-Unibas <ITS-Unibas+github@gmail.com>
2025-12-09 12:13:37 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-keepass/tools/chocolateyInstall.ps1
+++ automatic/unibas-keepass/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = ''
-    checksum       = ''
+    url            = 'https://keepass.info/filepool/KeePass-2.60-1.msi'
+    checksum       = 'afb3fab5210fdfe36e5644351c94f336f53fcc4ddeedc0486b511d97ef06d109'
     checksumType   = 'sha256'
     fileType       = 'MSI'
     silentArgs     = '/qn'
