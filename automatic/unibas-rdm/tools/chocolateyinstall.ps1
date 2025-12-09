8dc505b32f7a82ee83421f606872c00bc29af01f
ITS-Unibas <ITS-Unibas+github@gmail.com>
2025-12-09 15:52:44 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-rdm/tools/chocolateyinstall.ps1
+++ automatic/unibas-rdm/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   fileType      = 'EXE'
   silentArgs    = '/S'
   validExitCodes= @(0)
-  url           = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.2025.3.25.0.exe'
-  checksum      = '67f5281a8ebc1662e61c216f5887115a8b68f56c46fee58a32e24d0ce82eb1b2'
+  url           = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.2025.3.26.0.exe'
+  checksum      = 'ff6b68622cb151fce6e7afa2d8780a8a626f65a86730c9f68caebc7b6c580b9d'
   checksumType  = 'sha256'
 }
  
