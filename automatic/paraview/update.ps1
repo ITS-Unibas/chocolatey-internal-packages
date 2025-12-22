a77d8a56a1daf28e4f9d8810eab4e864802303a1
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2025-12-22 12:31:11 +0100

    Update update.ps1

--- automatic/paraview/update.ps1
+++ automatic/paraview/update.ps1
@@ -14,8 +14,8 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$folder = ((Invoke-WebRequest -Uri $releases).Links | Where-Object {$_ -match 'v[0-9].'} | Select-Object -Last 1).href
-	$file = (((Invoke-WebRequest -Uri "$releases$folder" ).Links | Where-Object {$_ -match ".msi"} | Select-Object -Last 1).href).replace('.0&','&')
+	$folder = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'v[0-9].'} | Select-Object -Last 1).href
+	$file = (((Invoke-WebRequest -Uri "$releases$folder" -UseBasicParsing).Links | Where-Object {$_ -match ".msi"} | Select-Object -Last 1).href).replace('.0&','&')
 	$url = "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=$folder&type=binary&os=Windows&downloadFile=$file"
 	$version=$folder.replace('v','').replace('/','')
 	$checksumType = 'sha256'
@@ -27,5 +27,6 @@ function global:au_GetLatest {
 	}
 	return $Latest
 }
+
 
 update -NoCheckChocoVersion
