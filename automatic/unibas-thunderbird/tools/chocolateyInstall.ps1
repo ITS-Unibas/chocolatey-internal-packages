26ca97ab2562b10008b0006b88e946df4d2b362b
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-01-14 05:03:09 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-thunderbird/tools/chocolateyInstall.ps1
+++ automatic/unibas-thunderbird/tools/chocolateyInstall.ps1
@@ -10,9 +10,9 @@ $packageArgs = @{
   fileType = 'exe'
   softwareName = "$softwareName*"
 
-  Checksum       = '63a7035b1d1114791055ae0d138cd34411650383bb72766b6ce4d846fc66cae2'
+  Checksum       = 'c800cc6b3b0cfc6eec929b08e78d41f86be760431c5b821242aea995f9b1461a'
   ChecksumType = 'sha256'
-  Url = 'https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/140.6.0esr/win64/en-US/Thunderbird%20Setup%20140.6.0esr.exe'
+  Url = 'https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/140.7.0esr/win64/en-US/Thunderbird%20Setup%20140.7.0esr.exe'
   silentArgs     = '-ms'
   validExitCodes = @(0)
 }
