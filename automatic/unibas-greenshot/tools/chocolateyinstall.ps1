d02e8cceb3acd66ce1fd56fbbe802ee39ecc3732
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-01-23 08:31:22 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-greenshot/tools/chocolateyinstall.ps1
+++ automatic/unibas-greenshot/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.10.6/Greenshot-INSTALLER-1.2.10.6-RELEASE.exe'
+  url           = 'https://github.com/greenshot/greenshot/releases/download/v1.3.312/Greenshot-INSTALLER-1.3.312-RELEASE.exe'
   #OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
   silentArgs    = '/S'           # NSIS
@@ -25,7 +25,7 @@ $packageArgs = @{
 
   # optional, highly recommended
   softwareName  = 'Greenshot*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '1687311b4e7a3720be20490e8ed6cc772a32336a7bed8896e475b8ec616c6b81'
+  checksum      = '64fd6d75ca1ec23b0aa1c48fbe071baea1c5e82303801a53b7d7b1c2456f8562'
   checksumType  = 'sha256' #default is md5, can also be sha1
 }
 
