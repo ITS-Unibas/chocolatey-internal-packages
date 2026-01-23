586431956e4e65c185ee5415e602053d78469e3f
Marius Lengwiler <marius.lengwiler@unibas.ch>
2026-01-23 11:34:09 +0100

    Update chocolateyinstall.ps1

--- automatic/unibas-freecad/tools/chocolateyinstall.ps1
+++ automatic/unibas-freecad/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://github.com/FreeCAD/FreeCAD/releases/download/1.0.0/FreeCAD_1.0.0-conda-Windows-x86_64-installer-1.exe'
+  url64            = 'https://github.com/FreeCAD/FreeCAD/releases/download/1.0.0/FreeCAD_1.0.0-conda-Windows-x86_64-installer-1.exe'
   silentArgs     = '/S'
   softwareName   = 'FreeCAD*'
-  checksum       = '2a173c91fb61b4eb4a086b7da5db9fa51536412db2452559c02a91f39ca55d7d'
+  checksum64       = '2a173c91fb61b4eb4a086b7da5db9fa51536412db2452559c02a91f39ca55d7d'
   checksumType   = 'sha256'
   validExitCodes = @(0) 
 }
