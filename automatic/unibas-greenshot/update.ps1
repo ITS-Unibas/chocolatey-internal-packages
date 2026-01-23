4116d7167bd485dffcb152ce7d10ae0c534a3a6a
Marius Lengwiler <marius.lengwiler@unibas.ch>
2026-01-23 09:19:29 +0100

    changed version parsing because it failed

--- automatic/unibas-greenshot/update.ps1
+++ automatic/unibas-greenshot/update.ps1
@@ -19,8 +19,7 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
   $regex = '.exe$'
   $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
-  $arr = $url -split '-|.exe'
-  $version = $arr[4]
+  $version = ($url -split "-")[-2]
   return @{ Version = $version; URL = $url }
 }
 
