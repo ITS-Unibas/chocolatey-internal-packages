159e41cd3a86e0f51fbd4321af430d7d681033ad
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-01-30 09:34:47 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-eclipse/tools/chocolateyinstall.ps1
+++ automatic/unibas-eclipse/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'unibas-eclipse'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
@@ -7,9 +7,9 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64       = ''
+  url64       = 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2025-12/R/eclipse-jee-2025-12-R-win32-x86_64.zip&r=1'
   softwareName   = 'Eclipse*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum64     = ''
+  checksum64     = '01ca2e43f14231e94647515024ce967121fdb637f758d87899032d0baefacb45'
   checksumType64 = 'sha256' #default is checksumType
 }
 
