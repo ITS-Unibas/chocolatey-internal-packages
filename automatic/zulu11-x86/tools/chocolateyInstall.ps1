3ec95205c5ad02094cabcc485a6e973285e245a5
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-02-03 18:14:43 +0000

    updated chocolatey-internal-packages

--- automatic/zulu11-x86/tools/chocolateyInstall.ps1
+++ automatic/zulu11-x86/tools/chocolateyInstall.ps1
@@ -2,13 +2,13 @@
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-$urljre = 'https://cdn.azul.com/zulu/bin/zulu11.86.19-ca-jre11.0.30-win_i686.zip'
-$urljdkfx = 'https://cdn.azul.com/zulu/bin/zulu11.84.17-ca-fx-jdk11.0.29-win_i686.zip'
-$urljrefx = 'https://cdn.azul.com/zulu/bin/zulu11.84.17-ca-fx-jre11.0.29-win_i686.zip'
+$urljre = 'https://cdn.azul.com/zulu/bin/zulu11.86.21-ca-jre11.0.30-win_i686.zip'
+$urljdkfx = 'https://cdn.azul.com/zulu/bin/zulu11.86.21-ca-fx-jdk11.0.30-win_i686.zip'
+$urljrefx = 'https://cdn.azul.com/zulu/bin/zulu11.86.21-ca-fx-jre11.0.30-win_i686.zip'
 
-$checksumurljre = 'e932b58758cabeca606e2895ddfe931b68083f936b21720d10d4412eceaa4d76'
-$checksumurljdkfx = '55b3dbcea5e0c5a20f8082c58edc455755ebf3ef7d207e0096bba089db99b034'
-$checksumurljrefx = '11a6f74074de88dab7b248d269520ed02a88a1ed8599e38e374050a9e64099c3'
+$checksumurljre = '766198d1753857d2d0a9b753b97db6eefa6ead8c1578b5a94d4374c175c01f60'
+$checksumurljdkfx = '3b50bdb01bb08c1d5c8af9f71baa9fb1fada02baf6d6aaf8a2d39da2fb8dd6fc'
+$checksumurljrefx = 'efa567bc924ff91fbf0ab533069a36a2af6aeaaca311687a2da2aca561d228a5'
 
 
 Get-ChocolateyWebFile -PackageName 'zulu11-x32' -FileFullPath "$toolsDir\JRE\zulu11-x32-jre.zip" -Url $urljre -Checksum $checksumurljre -ChecksumType 'sha256'
@@ -18,8 +18,8 @@ Get-ChocolateyWebFile -PackageName 'zulu11-x32' -FileFullPath "$toolsDir\FX\JRE\
 $packageArgs = @{
     packageName    = 'zulu11-x86'
     fileType       = 'msi'
-    url64bit       = 'https://cdn.azul.com/zulu/bin/zulu11.86.19-ca-jdk11.0.30-win_i686.msi'
-    checksum64     = '0a22e7e437b780dfc16b1a974ef98bbb402d96763d183088edc31dc4924a96f9'
+    url64bit       = 'https://cdn.azul.com/zulu/bin/zulu11.86.21-ca-jdk11.0.30-win_i686.msi'
+    checksum64     = '929af7a52dfb10335be6bc24218b020a5b08db17fcd0f0dab513648aed786215'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
     validExitCodes = @(0)
--- automatic/zulu8-x86/tools/chocolateyInstall.ps1
+++ automatic/zulu8-x86/tools/chocolateyInstall.ps1
@@ -2,13 +2,13 @@
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-$urljre = 'https://cdn.azul.com/zulu/bin/zulu8.92.0.19-ca-jre8.0.482-win_i686.zip'
-$urljdkfx = 'https://cdn.azul.com/zulu/bin/zulu8.90.0.19-ca-fx-jdk8.0.472-win_i686.zip'
-$urljrefx = 'https://cdn.azul.com/zulu/bin/zulu8.90.0.19-ca-fx-jre8.0.472-win_i686.zip'
+$urljre = 'https://cdn.azul.com/zulu/bin/zulu8.92.0.21-ca-jre8.0.482-win_i686.zip'
+$urljdkfx = 'https://cdn.azul.com/zulu/bin/zulu8.92.0.21-ca-fx-jdk8.0.482-win_i686.zip'
+$urljrefx = 'https://cdn.azul.com/zulu/bin/zulu8.92.0.21-ca-fx-jre8.0.482-win_i686.zip'
 
-$checksumurljre = '118ad2e29ae0f86d81b74d55e55af3dc5f696ccc051316e88a7e9113fa931bf8'
-$checksumurljdkfx = '6a425207aa710a89628afeb99b44e9eb80fb9bc2deb9f23ba01790f661368f9b'
-$checksumurljrefx = '47325ecae75110e17de6a8dd2b8de4e00c51291ce9b2cde24f8b3b8d57b62cec'
+$checksumurljre = '4ec67c300c4c1924a57e0ccd6dbf118ef6c64d16ab6f871cab256cd465aaae22'
+$checksumurljdkfx = '4da56fcb10bf1f6be5574bf57c28b1cbed7d0db03a2f6b05c378c0ccf9cb8d58'
+$checksumurljrefx = '10081f9d104c6c3fb3a7a1d30173e65f9c97ff4338094ca6badece60548ec080'
 
 
 Get-ChocolateyWebFile -PackageName 'zulu8-x32' -FileFullPath "$toolsDir\JRE\zulu8-x32-jre.zip" -Url $urljre -Checksum $checksumurljre -ChecksumType 'sha256'
@@ -18,8 +18,8 @@ Get-ChocolateyWebFile -PackageName 'zulu8-x32' -FileFullPath "$toolsDir\FX\JRE\z
 $packageArgs = @{
     packageName    = 'zulu8-x86'
     fileType       = 'msi'
-    url64bit       = 'https://cdn.azul.com/zulu/bin/zulu8.92.0.19-ca-jdk8.0.482-win_i686.msi'
-    checksum64     = 'c06f05b2bd89138ee8c849c4c02d43b6e072f261c73f5789669f9479cb97794f'
+    url64bit       = 'https://cdn.azul.com/zulu/bin/zulu8.92.0.21-ca-jdk8.0.482-win_i686.msi'
+    checksum64     = 'ebb54c8d04bc1f79531326c6e3167fe97ca6bb42975554a548f719cd37ea8cac'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
     validExitCodes = @(0)
