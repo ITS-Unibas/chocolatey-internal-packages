0eb4eb87ea4ac6ed9e0543ee07ec5df4a3c612af
stokes-unibas <75485474+stokes-unibas@users.noreply.github.com>
2026-02-05 11:49:17 +0100

    Update update.ps1

--- automatic/unibas-msedge-webview2/update.ps1
+++ automatic/unibas-msedge-webview2/update.ps1
@@ -1,11 +1,11 @@
 import-module chocolatey-au
 
-$urlEvergreenManifestMicrosoftEdgeWebView2Runtime = "https://raw.githubusercontent.com/aaronparker/evergreen/main/Evergreen/Manifests/MicrosoftEdgeWebView2Runtime.json"
+$urlEvergreenManifestMicrosoftEdgeWebView2Runtime = "https://raw.githubusercontent.com/aaronparker/apptracker/refs/heads/main/json/MicrosoftEdgeWebView2Runtime.json"
 
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            '(^\s*url\s*=\s*)(''.*'')'            = "`$1'$($Latest.url)'"
+            '(^\s*url\s*=\s*)(''.*'')'            = "`$1'$($Latest.url64)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
             "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
         }
@@ -16,38 +16,12 @@ function global:au_GetLatest {
     # Get the version for MicrosoftEdgeWebView2Runtime
     $content = Invoke-WebRequest -Uri $urlEvergreenManifestMicrosoftEdgeWebView2Runtime -UseBasicParsing
     $contentJSON = $content.Content | ConvertFrom-Json
-    $uri = $contentJSON.Get.Update.Uri
-    $ContentType = "application/json; charset=utf-8"
-    $Method = "Default"
-
-    $params = @{
-        Uri                = $Uri
-        ContentType        = $ContentType
-        DisableKeepAlive   = $true
-        MaximumRedirection = 2
-        Method             = $Method
-        UseBasicParsing    = $true
-    }
-
-    $products = Invoke-RestMethod @params
-    $releases = ($products | Where-Object {$_.Product -eq 'Stable'}).Releases
-    $winx64release = $releases | Where-Object {$_.Platform -eq 'Windows' -and $_.Architecture -eq 'x64'}
-
-    $version = $winx64release.ProductVersion
-
-    # Get the download URI for MicrosoftEdgeWebView2Runtime
-    $downloadURIWebView = $contentJSON.Get.Download.Uri.x64
-
-    $httpWebRequest = [System.Net.WebRequest]::Create($downloadURIWebView)
-    $httpWebRequest.MaximumAutomaticRedirections = 3
-    $httpWebRequest.AllowAutoRedirect = $true
-    $webResponse = $httpWebRequest.GetResponse()
-
-    $url = $webResponse.ResponseUri
+    $url = $contentJSON.URI | Where-Object {$_ -like '*X64*'}
+    $version = ($contentJSON | Where-Object {$_.Architecture -eq 'x64'} | Select-Object -Property Version).Version
 
     # return everything
     return @{
-        url			= $url
+        url64		= $url
         version		= $version
     }
 }
