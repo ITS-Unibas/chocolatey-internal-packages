43630a1a57069201b99dd6676b70d1f0d9899f21
stokes-unibas <75485474+stokes-unibas@users.noreply.github.com>
2026-02-06 09:49:13 +0100

    Update update.ps1

--- automatic/unibas-dotnetdesktopruntime/update.ps1
+++ automatic/unibas-dotnetdesktopruntime/update.ps1
@@ -1,7 +1,7 @@
 Import-Module chocolatey-au
 
-$sdk_download_url  = 'https://dotnet.microsoft.com/en-us/download'
-$ddrt_download_url_main = 'https://dotnet.microsoft.com/en-us/download/dotnet/' #ddrt = dotnet desctop runtime
+$sdk_download_url  = 'https://dotnet.microsoft.com/en-us/download/dotnet/10.0/'
+$ddrt_download_url_main = 'https://dotnet.microsoft.com/en-us/download/dotnet/10.0/' #ddrt = dotnet desctop runtime
 
 function global:au_SearchReplace {
     @{
@@ -17,11 +17,9 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $sdk_download_page = Invoke-WebRequest -Uri $sdk_download_url -UseBasicParsing
 
-    [version]$sdk_version = ((($sdk_download_page.Links.href | ? {$_ -match "windows-x64-installer"}) | select -First 1) -split("/"))[-1] -replace "sdk-", "" -replace "-windows-x64-installer", ""
-    $sdk_major_version_clean = "$($sdk_version.Major)" + ".0"
-    $ddrt_download_url =$ddrt_download_url_main + $sdk_major_version_clean
+    [version]$sdk_version = ((($sdk_download_page.Links.href | ? {$_ -match "runtime-desktop"}) | select-object -First 1) -split("/"))[-1] -replace "runtime-desktop-", "" -replace "-windows-x64-installer", ""
 
-    $ddrt_download_page = Invoke-WebRequest -Uri $ddrt_download_url -UseBasicParsing
+    $ddrt_download_page = Invoke-WebRequest -Uri $sdk_download_url -UseBasicParsing
     $ddrt_url_part = $ddrt_download_page.Links.href | ? {$_ -match "(runtime-desktop-)(.*)(-windows-x64-installer)$"} | ? {$_ -notmatch "preview|rc."} | Sort-Object -Descending | Select -First 1
     $dotnet_main_url = $sdk_download_url -split '(?<=//.+)/' | select -First 1
     $ddrt_download_url_latest = $dotnet_main_url + $ddrt_url_part
