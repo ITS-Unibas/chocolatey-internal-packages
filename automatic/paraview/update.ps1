4b49d9320ae0f6192577fee4fc8fedc90c883f59
Marius Lengwiler <marius.lengwiler@unibas.ch>
2026-02-09 17:57:29 +0100

    paraview: catch empty folders and minor releases
    added while loop to catch empty folders
    changed version determination from folder name to file name (to catch minor releases in same folder)

--- automatic/paraview/update.ps1
+++ automatic/paraview/update.ps1
@@ -14,10 +14,16 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$folder = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'v[0-9].'} | Select-Object -Last 1).href
-	$file = (((Invoke-WebRequest -Uri "$releases$folder" -UseBasicParsing).Links | Where-Object {$_ -match ".msi"} | Select-Object -Last 1).href).replace('.0&','&')
+    # check last 3 releases, in case folder has already been created but no installers uploaded yet
+    $i = 1
+    while ($file -eq $null -and $i -le 3){
+	    $folder = (((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'v[0-9].'}).href)[-$i]
+	    $file = (((Invoke-WebRequest -Uri $releases$folder -UseBasicParsing).Links | Where-Object {$_ -match ".msi"} | Select-Object -Last 1).href)
+        $i++
+    }
+    $file = $file.replace('.0&','&')
 	$url = "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=$folder&type=binary&os=Windows&downloadFile=$file"
-	$version=$folder.replace('v','').replace('/','')
+	$version=($file).Split("-")[1]
 	$checksumType = 'sha256'
 	$checksum = Get-RemoteChecksum($url,$checksumType)
 
