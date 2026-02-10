e89bfc13404837c429a3457cea7d5f6bdfb24177
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-02-10 10:20:35 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-maxqda/tools/chocolateyInstall.ps1
+++ automatic/unibas-maxqda/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
   silentArgs     = '/qn LIC_SRV="131.152.22.50" LIC_NAME="MAXQDA" DISABLE_AUTO_UPDATES=1 DISABLE_MANUAL_UPDATES=1 /norestart' # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0,129, 3010, 1605, 1614, 1641)
   softwareName   = 'MAXQDA*'
-  checksum     = 'f582d6dce3a58a06fd86b79b7debc80d4a0e911361450f52c8e5a539a35c88af'
+  checksum     = '95191f0b08c000b88bbceeda4b4e2a0f42482e69b92c9f40887a834b5eee3225'
   checksumType = 'sha256'
 }
 
