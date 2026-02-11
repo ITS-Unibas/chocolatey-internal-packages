866673548ca41febeb32113a0e019e42e043badf
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-02-11 07:27:03 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
+++ automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
@@ -5,12 +5,12 @@ $packageName = 'unibas-dotnetdesktopruntime'
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/10.0.2/windowsdesktop-runtime-10.0.2-win-x64.exe'
+  url            = 'https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/10.0.3/windowsdesktop-runtime-10.0.3-win-x64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
  
   softwareName   = 'unibas-dotnetdesktopruntime' 
-  checksum       = '0996f612dcd56507278911fb4bb194df2c9eba0ea1b88bdb78cc7359ffffe6f7'
+  checksum       = '48bac209f52eb48652b349ba0347ca51dcf6f3d1f53d9126880ee650692b8f11'
   checksumType   = 'sha256'
 }
  
