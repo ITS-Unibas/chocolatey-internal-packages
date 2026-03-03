a4c9cf2162ada4de1ddcc1f9b6f80a7a65457dcb
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-03-03 05:30:57 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-googleearth/tools/chocolateyinstall.ps1
+++ automatic/unibas-googleearth/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://dl.google.com/dl/earth/client/advanced/current/googleearthprowin-7.3.6-x64.exe'
+  url            = 'https://dl.google.com/dl/earth/client/advanced/current/googleearthprowin-7.3.7-x64.exe'
   silentArgs     = '/S OMAHA=1'
   validExitCodes = @(0)
   softwareName   = 'Google Earth*'
-  checksum       = '48a8f9b18002d415e49cbd455f0ed1b0c130b1f102ea8adbfbe44c59e728e002'
+  checksum       = 'e677726d5bf6969eb498d1052039d2b21e7a0658d22f1a76df36120e737783a1'
   checksumType   = 'sha256'
 }
 
