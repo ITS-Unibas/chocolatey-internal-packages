aa47b7b6d2fd70faa703cceccb65e3ec4b0ad968
Marius Lengwiler <marius.lengwiler@unibas.ch>
2026-03-06 15:33:13 +0100

    Update update.ps1

--- automatic/unibas-keepass/update.ps1
+++ automatic/unibas-keepass/update.ps1
@@ -1,26 +1,39 @@
 Import-Module chocolatey-au
 
 $releases = 'https://keepass.info/download.html'
+$checksums = 'https://keepass.info/integrity.html'
+
+function global:au_BeforeUpdate {
+  if ($Latest.Url) {
+    (Invoke-WebRequest -Uri $checksums -UseBasicParsing).content -match "KeePass-($version)\.msi<[\s\S]+?SHA-256.*<code>([\w\s]*)"
+    $Latest.ChecksumType = 'sha256'
+    $Latest.Checksum = $matches[2] -replace " ",""
+  }
+}
+
 
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1`'$($Latest.URL)`'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum64)`'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
         }
     }
 }
+
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $base_url = $download_page.Links | Where-Object href -match '2\.x/[\.\d]+/KeePass-[\d\.]+\.msi/download' | Select-Object -ExpandProperty href
     $version = ($matches[0] -split "/")[1] 
     $final_url = $base_url -replace '(.*)/download','$1'
+#    (Invoke-WebRequest -Uri $checksums -UseBasicParsing).content -match "KeePass-($version)\.msi<[\s\S]+?SHA-256.*<code>([\w\s]*)"
+#    $checksum = $matches[2] -replace " ",""
 
     return @{
         URL     = $final_url
         Version = $version
     }
 }
+
 Update-Package -NoCheckChocoVersion -nocheckurl -checksumfor none
-#nocheckurl and checksumfor none needed, because github runner is blocked from downloading the file by sourceforge
