464e35894753ee2251200f9f85c34ca32c516a78
Marius Lengwiler <marius.lengwiler@unibas.ch>
2026-03-06 16:23:29 +0100

    Update update.ps1

--- automatic/unibas-keepass/update.ps1
+++ automatic/unibas-keepass/update.ps1
@@ -1,13 +1,13 @@
 Import-Module chocolatey-au
 
 $releases = 'https://keepass.info/download.html'
-$checksums = 'https://keepass.info/integrity.html'
 
 function global:au_BeforeUpdate {
   if ($Latest.Url) {
+    $checksums = 'https://keepass.info/integrity.html'
     (Invoke-WebRequest -Uri $checksums -UseBasicParsing).content -match "KeePass-($version)\.msi<[\s\S]+?SHA-256.*<code>([\w\s]*)"
     $Latest.ChecksumType = 'sha256'
-    $Latest.Checksum = $matches[2] -replace " ",""
+    $Latest.Checksum = ($matches[2] -replace " ","")
   }
 }
 
@@ -24,11 +24,9 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $base_url = $download_page.Links | Where-Object href -match '2\.x/[\.\d]+/KeePass-[\d\.]+\.msi/download' | Select-Object -ExpandProperty href
-    $version = ($matches[0] -split "/")[1] 
+    $base_url = $download_page.Links | Where-Object href -match '2\.x/[\d\.]+/KeePass-[\d\.]+\.msi/download' | Select-Object -ExpandProperty href
+    $version = ($base_url -split "/")[-3]
     $final_url = $base_url -replace '(.*)/download','$1'
-#    (Invoke-WebRequest -Uri $checksums -UseBasicParsing).content -match "KeePass-($version)\.msi<[\s\S]+?SHA-256.*<code>([\w\s]*)"
-#    $checksum = $matches[2] -replace " ",""
 
     return @{
         URL     = $final_url
