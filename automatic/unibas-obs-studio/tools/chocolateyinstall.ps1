f773c76891c43b44c8a36d0b3549e67caafe5c3d
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-03-12 07:24:07 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-obs-studio/tools/chocolateyinstall.ps1
+++ automatic/unibas-obs-studio/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   url64bit          = 'https://github.com/obsproject/obs-studio/releases/download/32.1.0/OBS-Studio-32.1.0-Windows-x64-Installer.exe'
   silentArgs     = '/S'
   softwareName   = 'OBS Studio*'
-  checksum64     = ''
+  checksum64     = '0E3D75EB83951D3FE100CDC7A97FD870F16781BBDCF544FBE101D9B89F9C6231'
   checksumType   = 'sha256'
   validExitCodes = @(0) 
 }
