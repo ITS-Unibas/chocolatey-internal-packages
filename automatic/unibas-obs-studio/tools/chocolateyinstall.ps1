b7d931174c5f182264f3f09307bf2b75d7278fbc
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-03-12 05:35:34 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-obs-studio/tools/chocolateyinstall.ps1
+++ automatic/unibas-obs-studio/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit          = 'https://github.com/obsproject/obs-studio/releases/download/32.0.4/OBS-Studio-32.0.4-Windows-x64-Installer.exe'
+  url64bit          = 'https://github.com/obsproject/obs-studio/releases/download/32.1.0/OBS-Studio-32.1.0-Windows-x64-Installer.exe'
   silentArgs     = '/S'
   softwareName   = 'OBS Studio*'
-  checksum64     = '46A18BCE8E2FF662B700C91D340A519376E712FE0AF0D335536E4F9FD253F10A'
+  checksum64     = ''
   checksumType   = 'sha256'
   validExitCodes = @(0) 
 }
--- automatic/unibas-rdm/tools/chocolateyinstall.ps1
+++ automatic/unibas-rdm/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   fileType      = 'EXE'
   silentArgs    = '/S'
   validExitCodes= @(0)
-  url           = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.2026.1.10.0.exe'
-  checksum      = '595df9ce30b0a43741e527ff5e063e1063aa441c14033ab32772a32115367bd1'
+  url           = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.2026.1.11.0.exe'
+  checksum      = 'd50d6870785ac0129707217d11495951a2dee86c4bdac5b1c6e3ce797a35f5a6'
   checksumType  = 'sha256'
 }
  
