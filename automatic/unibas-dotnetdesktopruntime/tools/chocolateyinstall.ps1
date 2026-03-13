37d2ee0d40840c53bdfed848799e02989802617b
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-03-13 05:33:31 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
+++ automatic/unibas-dotnetdesktopruntime/tools/chocolateyinstall.ps1
@@ -5,12 +5,12 @@ $packageName = 'unibas-dotnetdesktopruntime'
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/10.0.4/windowsdesktop-runtime-10.0.4-win-x64.exe'
+  url            = 'https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/10.0.5/windowsdesktop-runtime-10.0.5-win-x64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
  
   softwareName   = 'unibas-dotnetdesktopruntime' 
-  checksum       = 'dbb961ed3abe6aae81683f1568443245704fb5b334f31973cbeebcaabd1a67d3'
+  checksum       = '1cbe57a8b78944b499d058d3c1af2a54eddd896456865207bf4a6b494bafc997'
   checksumType   = 'sha256'
 }
  
--- automatic/unibas-rdm/tools/chocolateyinstall.ps1
+++ automatic/unibas-rdm/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   fileType      = 'EXE'
   silentArgs    = '/S'
   validExitCodes= @(0)
-  url           = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.2026.1.11.0.exe'
-  checksum      = 'd50d6870785ac0129707217d11495951a2dee86c4bdac5b1c6e3ce797a35f5a6'
+  url           = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.2026.1.12.0.exe'
+  checksum      = '1d4a1f52364d7366787ed060d43a15615bc04f8c417d34f1f37b1120556d0f91'
   checksumType  = 'sha256'
 }
  
