e0cbf0ef7215c5916d85c87f789155e7ba67ccbd
Max <135263966+verifizieren@users.noreply.github.com>
2026-03-13 08:08:01 +0100

    FileZilla new apprach (enabled update.ps1 again)

--- automatic/unibas-filezilla/tools/chocolateyInstall.ps1
+++ automatic/unibas-filezilla/tools/chocolateyInstall.ps1
@@ -1,14 +1,15 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'FileZilla*'
-  url            = 'https://dl4.cdn.filezilla-project.org/client/FileZilla_3.69.1_win64-setup.exe?h=Dhd7YVCdeIhh6BESWeyEiA&x=1745419433'
-  checksum       = '6cbd5fdb157a7851cdd32b9a61d6136a31a5fa05fb36fd9403b920b708c9125e'
-  checksumType   = 'sha256'
+  file           = Join-Path $toolsDir "FileZilla_$($env:ChocolateyPackageVersion)_win64-setup.exe"
   fileType       = 'EXE'
   silentArgs     = '/S'
   validExitCodes = @(0, 1223)
 }
 
-Install-ChocolateyPackage @packageArgs
+&(Join-Path $PSScriptRoot InitialScript.ps1)
+Install-ChocolateyInstallPackage @packageArgs
