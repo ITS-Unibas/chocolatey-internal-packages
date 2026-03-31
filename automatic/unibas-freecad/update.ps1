8778fc587a3cf76bc61a11e7f455c8f3a1f08dba
Marius Lengwiler <marius.lengwiler@unibas.ch>
2026-03-31 12:43:58 +0200

    Update update.ps1 - changed regex for url and version to match new naming on website

--- automatic/unibas-freecad/update.ps1
+++ automatic/unibas-freecad/update.ps1
@@ -18,9 +18,9 @@ function global:au_SearchReplace {
 }
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $PreUrl -UseBasicParsing
-  $regex = 'FreeCAD_.*-Windows-x86_64-installer-.*.exe$'
+  $regex = 'FreeCAD_.*-Windows-x86_64-.*installer\.exe$'
   $url = "$($download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href)"
-  $arr = $url -split 'FreeCAD_|-conda-Windows-x86_64-installer-.*$' 
+  $arr = $url -split 'FreeCAD_|-Windows-x86_64-.*$' 
   $version = $arr[1]
   return @{ Version = $version; URL = $url }
 }
