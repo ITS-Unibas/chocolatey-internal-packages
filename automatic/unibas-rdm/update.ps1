fa29a0368a4b6fb77f0ec9ee401e5e613d055479
Marius Lengwiler <marius.lengwiler@unibas.ch>
2026-04-07 10:53:31 +0200

    unibas-rdm: removed webview2 dependency, updated dotnet dependency for v10, updated version matching for website changes

--- automatic/unibas-rdm/update.ps1
+++ automatic/unibas-rdm/update.ps1
@@ -20,7 +20,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
   $urltext = $download_page.ToString()
-  $regExResult = $urltext | Select-String -Pattern 'Version:\s*(\d+(?:\.\d+)+)' -CaseSensitive:$false
+  $regExResult = $urltext | Select-String -Pattern 'Version:(?:<\/span>)?\s*(\d+(?:\.\d+)+)' -CaseSensitive:$false
     $version = $regExResult.Matches[0].Groups[1].Value
   $url = "https://cdn.devolutions.net/download/Setup.RemoteDesktopManager."
   $url += $version
@@ -29,11 +29,8 @@ function global:au_GetLatest {
   $deversionurl = 'https://community.chocolatey.org/packages/rdm#dependencies'
   $deversiontext = Invoke-WebRequest -Uri $deversionurl -UseBasicParsing | Select-Object -ExpandProperty Content
   $deversiontext = $deversiontext -replace '<.*?>', ''
-  if ($deversiontext -cmatch '(?i)dotnet-9.0-desktopruntime\s*\(\s*\u2265\s*(\d+(?:\.\d+)+)\s*\)') {
+  if ($deversiontext -cmatch '(?i)dotnet-\d+\.0-desktopruntime\s*\(\s*\u2265\s*(\d+(?:\.\d+)+)\s*\)') {
     $depversion1 = $matches[1]
-  }
-  if ($deversiontext -cmatch '(?i)webview2-runtime\s*\(\s*\u2265\s*(\d+(?:\.\d+)+)\s*\)') {
-    $depversion2 = $matches[1]
   }
   return @{
     URL     = $url
@@ -46,7 +43,6 @@ function global:au_GetLatest {
 function global:au_AfterUpdate {
   $NuspecFilePath = Join-Path -Path $PSScriptRoot -ChildPath 'unibas-rdm.nuspec'
   [xml]$xml = Get-Content -Path $NuspecFilePath
-  $xml.package.metadata.dependencies.dependency | Where-Object { $_.id -eq "unibas-msedge-webview2" } | ForEach-Object { $_.version = "[$($Latest.DependencyVersion2),)" }
   $xml.package.metadata.dependencies.dependency | Where-Object { $_.id -eq "unibas-dotnetdesktopruntime" } | ForEach-Object { $_.version = "[$($Latest.DependencyVersion1),)" }
   $xml.Save($NuspecFilePath)
 }
