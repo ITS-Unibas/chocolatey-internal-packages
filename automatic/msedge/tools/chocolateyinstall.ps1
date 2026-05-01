a7ffa8496839f8049a91dba688f8056edb4d0d8a
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-05-01 06:52:11 +0000

    updated chocolatey-internal-packages

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/2d48f894-08d6-4212-99bc-319200435457/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/33a7f26a-babe-4cfd-aa52-de77616e2850/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = 'B1DCC651AC4F168EE658285293119E47C95A79391C5AE560A977D63BF8518EF5'
+  checksum       = '6CA8F057CAEFEB78116E58D96CC0DC2547BC226C3FD23BE48A9D6E452864A40F'
   checksumType   = 'sha256'
 }
 
--- automatic/unibas-zotero/tools/chocolateyInstall.ps1
+++ automatic/unibas-zotero/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0, 3010, 1605, 1614, 1641)
   softwareName   = 'Zotero'
-  url64bit       = 'https://download.zotero.org/client/release/9.0.1/Zotero-9.0.1_x64_setup.exe'
-  checksum64     = '531ce0b3ebdb4e25266d149bd88b835ce1bc0c801fc58385845c29c13a4d9314'
+  url64bit       = 'https://download.zotero.org/client/release/9.0.2/Zotero-9.0.2_x64_setup.exe'
+  checksum64     = 'cf311861ff1af3b42e0750b48090492c034a8ee614a77f9e9f12bf05e0de51a9'
   checksumType64 = 'sha256'
 }
 
