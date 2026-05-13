8079e6cfde6a7c2cb433e898c4afcf20a4657718
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-05-13 15:31:18 +0000

    updated chocolatey-internal-packages

--- automatic/vcredist140-x64/tools/chocolateyinstall.ps1
+++ automatic/vcredist140-x64/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'vcredist140-x64'
   fileType       = 'exe'
-  url            = ''
+  url            = 'https://download.visualstudio.microsoft.com/download/pr/c1bd4f2c-3672-468e-8480-7ed419dbb641/90E48ADE404E4576D023ABFA374F323555F233982A8805EA9AC63DCA9491A16B/VC_redist.x64.exe'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0, 1638, 3010)
   softwareName   = 'Microsoft Visual C++ v14 Redistributable (x64)*'
-  checksum       = ''
+  checksum       = '90e48ade404e4576d023abfa374f323555f233982a8805ea9ac63dca9491a16b'
   checksumType   = 'sha256'
 }
 
--- automatic/vcredist140-x86/tools/chocolateyinstall.ps1
+++ automatic/vcredist140-x86/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'vcredist140-x86'
   fileType       = 'exe'
-  url            = ''
+  url            = 'https://download.visualstudio.microsoft.com/download/pr/0dd156af-82aa-4812-b524-49c2f894359a/B6AB675F0A27E6600F9726E75DEA08D99C15F8EA4B842A2A1D988FA9529D39B9/VC_redist.x86.exe'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0, 1638, 3010)
   softwareName   = 'Microsoft Visual C++ v14 Redistributable (x86)*'
-  checksum       = ''
+  checksum       = 'b6ab675f0a27e6600f9726e75dea08d99c15f8ea4b842a2a1d988fa9529d39b9'
   checksumType   = 'sha256'
 }
 
