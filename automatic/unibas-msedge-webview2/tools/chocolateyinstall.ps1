2e474f85c538483fd5c2f6ea222d5b990fd66572
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-05-16 12:14:12 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-msedge-webview2/tools/chocolateyinstall.ps1
+++ automatic/unibas-msedge-webview2/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageName = 'unibas-msedge-webview2'
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/a9d9edbb-41ad-412e-8a0c-e9737c9b4630/MicrosoftEdgeWebView2RuntimeInstallerX64.exe'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/b989c718-2e60-46bb-956d-605d24cf11a6/MicrosoftEdgeWebView2RuntimeInstallerX64.exe'
   silentArgs     = "/silent /install"
   validExitCodes = @(0)
   softwareName   = 'unibas-msedge-webview2*'
-  checksum       = '75e59b2246b4c8edfd393ba91e4fa7dcaee7910e89eaca9c00d9fb7fa9c014f0'
+  checksum       = 'e8464b185b4786f43e9c7357eea6a0e64f25b1e3bf841e1db0f7a0e9e8a9d090'
   checksumType   = 'sha256'
 }
 
