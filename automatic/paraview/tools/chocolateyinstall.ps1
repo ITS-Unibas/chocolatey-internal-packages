53b8ed948122e11561f3c329a8b4f38ef1fcdbf3
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-05-25 08:50:05 +0000

    updated chocolatey-internal-packages

--- automatic/paraview/tools/chocolateyinstall.ps1
+++ automatic/paraview/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName 	= 'paraview'
-$url            = 'https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v6.1/&type=binary&os=Windows&downloadFile=ParaView-6.1.0-Windows-Python3.12-msvc2017-AMD64.msi'
-$checksum       = 'ccd582f55079413659d67ac0c76cb9a0778dc5dca8fbd365e9a42775f48c420e'
+$url            = 'https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v6.1/&type=binary&os=Windows&downloadFile=ParaView-6.1.1-Windows-Python3.12-msvc2017-AMD64.msi'
+$checksum       = '9cdc653d839ebdd14903dd11a473018d722185eef091569454170c5bab876d6d'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
