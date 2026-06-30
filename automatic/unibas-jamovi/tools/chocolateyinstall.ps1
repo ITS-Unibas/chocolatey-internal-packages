42e0892961d33c0585b222ff95e2936bcf25b994
Max <135263966+verifizieren@users.noreply.github.com>
2026-06-30 12:15:00 +0200

    updated chocolatey-internal-packages

--- automatic/unibas-jamovi/tools/chocolateyinstall.ps1
+++ automatic/unibas-jamovi/tools/chocolateyinstall.ps1
@@ -7,12 +7,15 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = 'https://www.jamovi.org/downloads/jamovi-2.7.31.0-win-x64.exe'
+  url64bit       = 'https://dl-cdn.jamovi.org/jamovi-2.7.31.0-win-x64.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'unibas-jamovi*'
   checksum64     = '1fb79a52017d3e02ddc3288cf1ec5ea7d90ea8b39edf7dc5b9e4f7afd01ca14d'
   checksumType64 = 'sha256' #default is checksumType
+  options        = @{
+    Headers = @{ Referer = 'https://www.jamovi.org/download.html' }
+  }
 }
 
 Install-ChocolateyPackage @packageArgs
--- automatic/unibas-jamovi/update.ps1
+++ automatic/unibas-jamovi/update.ps1
@@ -5,7 +5,7 @@ $releases = 'https://www.jamovi.org/download.html'
 
 function global:au_BeforeUpdate() {
   Get-RemoteFiles -Purge -FileNameBase 'unibas-jamovi'
-  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'sha256'
+  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'sha256' -Headers $Latest.Options.Headers
 }
 function global:au_SearchReplace {
   @{
@@ -19,12 +19,12 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing -Headers @{"Referer" = "https://www.jamovi.org/"}
   # needs referer, otherwise will be redirected to the selection page for cloud or desktop version
   $regex = '.exe$'
-  $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
-  $version = $url -replace 'downloads/', ''
-  $arr = $version -split '-|.exe'
-  $version = $arr[1]
-  $url = "https://www.jamovi.org$url"
-  return @{ Version = $version; URL = $url }
+  $href = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = ($href -replace 'downloads/', '' -split '-|.exe')[1]
+  $filename = ($href -split '/')[-1]
+  $url = "https://dl-cdn.jamovi.org/$filename"
+  $options = @{ Headers = @{ Referer = $releases } }
+  return @{ Version = $version; URL = $url; Options = $options }
 }
 
 update -ChecksumFor none -NoCheckChocoVersion
