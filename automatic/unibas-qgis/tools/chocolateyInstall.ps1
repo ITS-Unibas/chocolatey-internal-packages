3789780f770daf2bb4dcdf39d068356b45052388
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-07-08 12:02:13 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-qgis/tools/chocolateyInstall.ps1
+++ automatic/unibas-qgis/tools/chocolateyInstall.ps1
@@ -8,12 +8,12 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url            = 'https://download.qgis.org/downloads/QGIS-OSGeo4W-3.44.11-1.msi'
+  url            = 'https://download.qgis.org/downloads/QGIS-OSGeo4W-3.44.12-1.msi'
   silentArgs     = "/qn /norestart /l*v `"$($LogPath)Install_$($env:chocolateyPackageName)_$($env:chocolateyPackageVersion).log`"" # Inno Setup
   validExitCodes = @(0, 3010) #please insert other valid exit codes here
 
   softwareName   = 'qgis*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = '062f58fd0f45fe94be2d9eda3316fc1d9109f2149f33bd1b15837d8dee0909c1'
+  checksum       = '9138740d3dc691c20afd2151fa780afe36496df30fd9d2fa724d5a4efa46f270'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
