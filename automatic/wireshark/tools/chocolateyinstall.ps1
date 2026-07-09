8f169edf1638974434d930e5fd826920e446c845
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-07-09 08:12:52 +0000

    updated chocolatey-internal-packages

--- automatic/wireshark/tools/chocolateyinstall.ps1
+++ automatic/wireshark/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Wireshark*'
   fileType       = 'exe'
-  url            = 'https://2.na.dl.wireshark.org/win64/Wireshark-4.6.6-x64.exe'
-  checksum       = 'ab28d13695ace992307fb1aaea7144f977d5b6562acec1f465c9c0a9fa04190c'
+  url            = 'https://2.na.dl.wireshark.org/win64/Wireshark-4.6.7-x64.exe'
+  checksum       = 'e67cfc2b4462cce93aecb67972d5079e85b8fe575fb18858f9260b6ec7b37258'
   checksumType   = 'sha256'
   silentArgs     = '/S /quicklaunchicon=no'
   validExitCodes = @(0)
