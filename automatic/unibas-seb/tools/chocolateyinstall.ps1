ff2bc51edf07441e4d485cdc6c3104eb1870b5cc
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-07-20 18:32:01 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-seb/tools/chocolateyinstall.ps1
+++ automatic/unibas-seb/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   fileType      = 'EXE'
   silentArgs    = '/quiet /norestart /log $($logFile)'
   validExitCodes= @(0)
-  url           = 'https://github.com/SafeExamBrowser/seb-win-refactoring/releases/download/v3.10.2/SEB_3.10.2.904_SetupBundle.exe'
-  checksum      = '9180566e407555fd8be9f7a022aa35a595e629ed9c9f81daf2cdd65ddb9646b6'
+  url           = 'https://github.com/SafeExamBrowser/seb-win-refactoring/releases/download/v3.10.2/SEB_3.10.2.920_SetupBundle.exe'
+  checksum      = '45e463ff49dcc39d6bb48cde3749ab7fa31d502f7dd271e9a86b04a8948893f9'
   checksumType  = 'sha256'
 }
  
