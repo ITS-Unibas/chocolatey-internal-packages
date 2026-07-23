481382dd97846de00d2671df07ba9afa1a26303f
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2026-07-23 17:09:50 +0200

    Update chocolateyinstall.ps1

--- automatic/unibas-jamovi/tools/chocolateyinstall.ps1
+++ automatic/unibas-jamovi/tools/chocolateyinstall.ps1
@@ -13,7 +13,7 @@ $packageArgs = @{
   checksum64     = '58150b9dcb4f2f4eb239fa684635968ea441adf05d8b1d57f67ac3132e487645'
   checksumType64 = 'sha256' #default is checksumType
   options        = @{
-    Headers = @{ Referer = 'https://www.jamovi.org/download.html'; User-Agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' }
+    Headers = @{ Referer = 'https://www.jamovi.org/download.html'; 'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' }
   }
 }
 
