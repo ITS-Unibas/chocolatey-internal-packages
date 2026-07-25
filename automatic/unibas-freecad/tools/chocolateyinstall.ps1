7c6934fc4beec629e488c0d0fc2f300ebcb41bde
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-07-25 17:34:25 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-freecad/tools/chocolateyinstall.ps1
+++ automatic/unibas-freecad/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64            = 'https://github.com/FreeCAD/FreeCAD/releases/download/1.1.2/FreeCAD_1.1.2-Windows-x86_64-py311-installer.exe'
+  url64            = 'https://github.com/FreeCAD/FreeCAD/releases/download/1.1.3/FreeCAD_1.1.3-Windows-x86_64-py311-installer.exe'
   silentArgs     = '/S'
   softwareName   = 'FreeCAD*'
-  checksum64       = 'a5d9d829c10a6074620dc4ca52ca13e71d87d866e0caf4b737172ffb4f94bec9'
+  checksum64       = '3de56676dedb7c68f4da9734c79abeaff9bbbf09f6a2c01df72a82beeee81c11'
   checksumType   = 'sha256'
   validExitCodes = @(0) 
 }
