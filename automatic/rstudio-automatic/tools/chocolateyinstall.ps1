c3495742534a2c269363585e7a35dbed7bb4cde5
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-08-14 05:52:47 +0000

    updated chocolatey-internal-packages

--- automatic/rstudio-automatic/tools/chocolateyinstall.ps1
+++ automatic/rstudio-automatic/tools/chocolateyinstall.ps1
@@ -7,12 +7,12 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://download1.rstudio.org/electron/windows/RStudio-2026.07.1-147.exe'
+  url           = 'https://download1.rstudio.org/electron/windows/RStudio-2026.08.0-187.exe'
   silentArgs   = '/S'
   validExitCodes= @(0)
  
   softwareName  = 'rstudio*'
-  checksum      = '6c22bd2cf2af0af4365e74dd0f84573d30385a3b6fe0afa8c112fa34ff408d1b'
+  checksum      = '9f8461a1ec285d89fbaed7f601bc7ea95505f06d58b868395771a18bf33b11b1'
   checksumType  = 'sha256'
 }
 
