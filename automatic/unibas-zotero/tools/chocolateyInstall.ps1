567846e0a30ab9ccf46e058fe85547e686ec21f2
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-08-17 17:48:49 +0000

    updated chocolatey-internal-packages

--- automatic/unibas-zotero/tools/chocolateyInstall.ps1
+++ automatic/unibas-zotero/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0, 3010, 1605, 1614, 1641)
   softwareName   = 'Zotero'
-  url64bit       = 'https://download.zotero.org/client/release/9.0.6/Zotero-9.0.6_x64_setup.exe'
-  checksum64     = '973bfc96a7af8b2949bbfdc46b220366b2b204942ac30d50aef9835eb152d31c'
+  url64bit       = 'https://download.zotero.org/client/release/10.0/Zotero-10.0_x64_setup.exe'
+  checksum64     = 'efa961da61b2c179ccb9ce75b490a8ba9b92bba8592f90d78822bb503de52c71'
   checksumType64 = 'sha256'
 }
 
