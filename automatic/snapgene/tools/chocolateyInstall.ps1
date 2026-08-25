bf8b9386da86f6807792d4969b5969230d9d9593
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-08-25 17:04:50 +0000

    updated chocolatey-internal-packages

--- automatic/snapgene/tools/chocolateyInstall.ps1
+++ automatic/snapgene/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'snapgene'
   fileType       = 'EXE'
-  url            = 'https://cdn.snapgene.com/downloads/SnapGene/8.x/8.2/8.2.2/snapgene_8.2.2_win.exe'
+  url            = 'https://cdn.snapgene.com/downloads/SnapGene/8.x/8.2/8.2.3/snapgene_8.2.3_win.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Snapgene*' 
-  checksum       = 'db68e20dc8512770cbb18d71ed865e50d2346e74debccc97038080e82e4eb8a8'
+  checksum       = '8a0ec2075706dd911596d088070b45977a1d1ff57aecaa11d351a04f461e8c0c'
   checksumType   = 'sha256' 
 }
 
