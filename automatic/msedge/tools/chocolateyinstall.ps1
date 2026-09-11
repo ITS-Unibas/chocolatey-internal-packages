5790f948e90647c7a36b7455662cde3e0e2534e6
ITS-Unibas <ITS-Unibas+github@gmail.com>
2026-09-11 09:03:28 +0000

    updated chocolatey-internal-packages

--- automatic/msedge/tools/chocolateyinstall.ps1
+++ automatic/msedge/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = 'msedge'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/2757513f-2ec6-4bb1-b23a-5738a92d7a56/MicrosoftEdgeEnterpriseX64.msi'
+  url            = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/4d637967-3bb7-4c92-8b67-6d91ff264d45/MicrosoftEdgeEnterpriseX64.msi'
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'edge*'
-  checksum       = '5BCF8CD57351DDAA94419F800F8D90B8CBAF2B485B49D9DDB07D055B76A518DF'
+  checksum       = '4388DC0C46B5EC98AEBDF9B78A07F2C70076164F322E20228DC7DE366D60A385'
   checksumType   = 'sha256'
 }
 
--- automatic/rstudio-automatic/tools/chocolateyinstall.ps1
+++ automatic/rstudio-automatic/tools/chocolateyinstall.ps1
@@ -7,12 +7,12 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://download1.rstudio.org/electron/windows/RStudio-2026.08.2-200.exe'
+  url           = 'https://download1.rstudio.org/electron/windows/RStudio-2026.09.0-174.exe'
   silentArgs   = '/S'
   validExitCodes= @(0)
  
   softwareName  = 'rstudio*'
-  checksum      = 'b246a1cc3c61b7c56555d99393dd19fa377be9a6c6b874fc54836671cebf5f0c'
+  checksum      = 'e6b5a3b1028b7cc8d2abe7375aae4813a791898899d81d64468185349e41491c'
   checksumType  = 'sha256'
 }
 
